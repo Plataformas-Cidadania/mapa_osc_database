774d5d5fa419bb550cd2c0dc2512235b4cfe3155
Vagner Praia da Silva <vagnerpraia@gmail.com>
2017-06-19 17:21:38 -0300

    Inclusão do tipo de parceria em projetos

--- database/2.2.0/2_alter_osc.tb_fonte_recursos_projeto.sql
+++ database/2.2.0/2_alter_osc.tb_fonte_recursos_projeto.sql
@@ -1,5 +1,5 @@
-﻿ALTER TABLE osc.tb_osc_parceira_projeto ADD cd_tipo_parceria INTEGER;
+﻿ALTER TABLE osc.tb_fonte_recursos_projeto ADD cd_tipo_parceria INTEGER;
 
-ALTER TABLE osc.tb_osc_parceira_projeto 
+ALTER TABLE osc.tb_fonte_recursos_projeto 
 ADD CONSTRAINT fk_cd_parceira_projeto FOREIGN KEY (cd_tipo_parceria) 
 REFERENCES syst.dc_tipo_parceria (cd_tipo_parceria);
