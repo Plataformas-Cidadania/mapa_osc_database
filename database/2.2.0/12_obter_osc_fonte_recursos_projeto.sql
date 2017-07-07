2b09cccb1a7c5683af7ca65ef4f08b367cdca59b
Denise <Denise@DESKTOP-J7J6R3E>
2017-07-07 15:56:27 -0300

    Issue: MOSC-1228 Subject: Criação do serviço para a obtenção dos tipos de parcerias Detail:

---
+++ database/2.2.0/12_obter_osc_fonte_recursos_projeto.sql
@@ -0,0 +1,28 @@
+﻿-- Function: portal.obter_osc_fonte_recursos_projeto(integer)
+
+DROP FUNCTION portal.obter_osc_fonte_recursos_projeto(integer);
+
+CREATE OR REPLACE FUNCTION portal.obter_osc_fonte_recursos_projeto(IN param integer)
+  RETURNS TABLE(id_fonte_recursos_projeto integer, cd_origem_fonte_recursos_projeto integer, tx_nome_origem_fonte_recursos_projeto text, ft_fonte_recursos_projeto text, cd_tipo_parceria integer, tx_nome_tipo_parceria text) AS
+$BODY$
+BEGIN 
+	RETURN QUERY 
+		SELECT 
+			vw_osc_fonte_recursos_projeto.id_fonte_recursos_projeto, 
+			vw_osc_fonte_recursos_projeto.cd_origem_fonte_recursos_projeto, 
+			vw_osc_fonte_recursos_projeto.tx_nome_origem_fonte_recursos_projeto, 
+			vw_osc_fonte_recursos_projeto.ft_fonte_recursos_projeto,
+			vw_osc_fonte_recursos_projeto.cd_tipo_parceria,
+			vw_osc_fonte_recursos_projeto.tx_nome_tipo_parceria
+		FROM 
+			portal.vw_osc_fonte_recursos_projeto 
+		WHERE 
+			vw_osc_fonte_recursos_projeto.id_projeto = param;
+	RETURN;
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100
+  ROWS 1000;
+ALTER FUNCTION portal.obter_osc_fonte_recursos_projeto(integer)
+  OWNER TO postgres;
