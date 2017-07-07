9315a43a812b33f5a7a89af4f83f946b3bd45f78
Denise <Denise@DESKTOP-J7J6R3E>
2017-07-07 20:18:25 -0300

    Issue: MOSC-1284 Subject: Criação de serviço para ativação de representantes de estados e municípios Detail:

---
+++ database/2.2.0/13_ativar_representante_governo.sql
@@ -0,0 +1,28 @@
+﻿-- DROP FUNCTION portal.ativar_representante_governo(integer);
+
+CREATE OR REPLACE FUNCTION portal.ativar_representante_governo(id integer)
+  RETURNS boolean AS
+$BODY$ 
+
+BEGIN 
+
+	UPDATE 
+		portal.tb_usuario 
+	SET 
+		bo_ativo = true, 
+		dt_atualizacao = NOW() 
+	WHERE 
+		id_usuario = id;
+
+	IF FOUND THEN
+		RETURN TRUE;
+	ELSE
+		RETURN FALSE;
+	END IF;
+
+END; 
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+ALTER FUNCTION portal.ativar_representante_governo(integer)
+  OWNER TO postgres;
---
+++ database/2.2.0/14_ativar_representante.sql
@@ -0,0 +1,29 @@
+﻿DROP FUNCTION portal.ativar_representante(integer);
+
+CREATE OR REPLACE FUNCTION portal.ativar_representante(id integer)
+  RETURNS boolean AS
+$BODY$ 
+
+BEGIN 
+
+	UPDATE 
+		portal.tb_usuario 
+	SET 
+		bo_ativo = true, 
+		dt_atualizacao = NOW() 
+	WHERE 
+		id_usuario = id AND 
+		cd_tipo_usuario = 2;
+
+	IF FOUND THEN
+		RETURN TRUE;
+	ELSE
+		RETURN FALSE;
+	END IF;
+
+END; 
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+ALTER FUNCTION portal.ativar_representante(integer)
+  OWNER TO postgres;
