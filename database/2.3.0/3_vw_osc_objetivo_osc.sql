1bc032d0c9f006c4de91bea9337805540a8fa24a
vagnerpraia <vagnerpraia@gmail.com>
2017-07-19 19:26:08 -0300

    Issue: MOSC-1305 Subject: Adicionar informação de ODS de OSC no serviço de Dados Gerais Detail: Criação de tabela e view para os objetivos de OSC

---
+++ database/2.3.0/3_vw_osc_objetivo_osc.sql
@@ -0,0 +1,19 @@
+﻿-- object: portal.vw_osc_objetivo_osc | type: MATERIALIZED VIEW --
+DROP MATERIALIZED VIEW IF EXISTS portal.vw_osc_objetivo_osc CASCADE;
+CREATE MATERIALIZED VIEW portal.vw_osc_objetivo_osc
+AS
+
+SELECT
+	tb_objetivo_osc.id_osc,
+	tb_objetivo_osc.id_objetivo_osc,
+	(SELECT cd_objetivo_projeto FROM syst.dc_objetivo_projeto WHERE cd_objetivo_projeto = (SELECT cd_objetivo_projeto FROM syst.dc_meta_projeto WHERE cd_meta_projeto = tb_objetivo_osc.cd_meta_osc)) AS cd_objetivo_osc,
+	(SELECT tx_codigo_objetivo_projeto || '. ' || tx_nome_objetivo_projeto FROM syst.dc_objetivo_projeto WHERE cd_objetivo_projeto = (SELECT cd_objetivo_projeto FROM syst.dc_meta_projeto WHERE cd_meta_projeto = tb_objetivo_osc.cd_meta_osc)) AS tx_nome_objetivo_osc,
+	tb_objetivo_osc.cd_meta_osc,
+	(SELECT tx_codigo_meta_projeto || ' ' || tx_nome_meta_projeto FROM syst.dc_meta_projeto WHERE cd_meta_projeto = tb_objetivo_osc.cd_meta_osc) AS tx_nome_meta_osc,
+	tb_objetivo_osc.ft_objetivo_osc 
+FROM osc.tb_osc 
+INNER JOIN osc.tb_objetivo_osc ON tb_objetivo_osc.id_osc = osc.tb_osc.id_osc 
+WHERE tb_osc.bo_osc_ativa;
+-- ddl-end --
+ALTER MATERIALIZED VIEW portal.vw_osc_objetivo_osc OWNER TO postgres;
+-- ddl-end --
