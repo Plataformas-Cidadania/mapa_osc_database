77bb2ca42e98f2d09fdfb987002d49b57211fbed
vagnerpraia <vagnerpraia@gmail.com>
2017-08-09 01:44:04 -0300

    Atualização dos funções de ativação de usuário

---
+++ database/2.3.0/19_ativar_representante_osc.sql
@@ -0,0 +1,29 @@
+﻿DROP FUNCTION IF EXISTS portal.ativar_representante_osc(idusuario integer);
+
+CREATE OR REPLACE FUNCTION portal.ativar_representante_osc(idusuario integer) RETURNS TABLE(
+	flag BOOLEAN,
+	mensagem TEXT
+)AS $$
+
+BEGIN 
+	UPDATE 
+		portal.tb_usuario 
+	SET 
+		bo_ativo = true, 
+		dt_atualizacao = NOW() 
+	WHERE 
+		id_usuario = idusuario AND 
+		cd_tipo_usuario = 2;
+	
+	flag := true;
+	mensagem := 'Usuário ativado.';
+	RETURN NEXT;
+
+EXCEPTION 
+	WHEN others THEN 
+		flag := false;
+		mensagem := 'Ocorreu um erro na ativação do usuário no banco de dados.';
+		RETURN NEXT;
+
+END;
+$$ LANGUAGE 'plpgsql';
