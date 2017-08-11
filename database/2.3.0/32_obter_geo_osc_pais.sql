d066560aa48d7bb34fbf52f6487993576b9b11b1
vagnerpraia <vagnerpraia@gmail.com>
2017-08-11 18:01:00 -0300

    Atualizações de funções e views

---
+++ database/2.3.0/32_obter_geo_osc_pais.sql
@@ -0,0 +1,22 @@
+﻿DROP FUNCTION IF EXISTS portal.obter_geolocalizacao_oscs();
+
+CREATE OR REPLACE FUNCTION portal.obter_geolocalizacao_oscs() RETURNS TABLE (
+	id_osc INTEGER,
+	geo_lat DOUBLE PRECISION,
+	geo_lng DOUBLE PRECISION
+) AS $$
+
+BEGIN
+	RETURN QUERY
+		SELECT
+			vw_geo_osc.id_osc,
+			vw_geo_osc.geo_lat,
+			vw_geo_osc.geo_lng
+		FROM 
+			osc.vw_geo_osc
+		WHERE 
+			vw_geo_osc.geo_lat IS NOT NULL AND 
+			vw_geo_osc.geo_lng IS NOT NULL;
+	
+END;
+$$ LANGUAGE 'plpgsql';
---
+++ database/2.3.0/33_obter_geo_osc.sql
@@ -0,0 +1,19 @@
+﻿DROP FUNCTION IF EXISTS portal.obter_geolocalizacao_osc(idosc INTEGER);
+
+CREATE OR REPLACE FUNCTION portal.obter_geolocalizacao_osc(idosc INTEGER) RETURNS TABLE (
+	geo_lat DOUBLE PRECISION,
+	geo_lng DOUBLE PRECISION
+) AS $$
+
+BEGIN
+	RETURN QUERY
+		SELECT
+			vw_geo_osc.geo_lat,
+			vw_geo_osc.geo_lng
+		FROM 
+			osc.vw_geo_osc
+		WHERE 
+			vw_geo_osc.id_osc = idosc;
+	
+END;
+$$ LANGUAGE 'plpgsql'
