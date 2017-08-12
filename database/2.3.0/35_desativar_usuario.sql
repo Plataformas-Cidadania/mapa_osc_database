de5250a3cc95bafd7149288bda61293d788f19ff
vagnerpraia <vagnerpraia@gmail.com>
2017-08-12 15:09:24 -0300

    Função para fazer a desativação de usuário

---
+++ database/2.3.0/35_desativar_usuario.sql
@@ -0,0 +1,28 @@
+﻿DROP FUNCTION IF EXISTS portal.desativar_usuario(idusuario integer);
+
+CREATE OR REPLACE FUNCTION portal.desativar_usuario(idusuario integer) RETURNS TABLE(
+	flag BOOLEAN,
+	mensagem TEXT
+)AS $$
+
+BEGIN 
+	UPDATE 
+		portal.tb_usuario 
+	SET 
+		bo_ativo = false, 
+		dt_atualizacao = NOW() 
+	WHERE 
+		id_usuario = idusuario;
+	
+	flag := true;
+	mensagem := 'Usuário desativado.';
+	RETURN NEXT;
+
+EXCEPTION 
+	WHEN others THEN 
+		flag := false;
+		mensagem := 'Ocorreu um erro na desativação do usuário no banco de dados.';
+		RETURN NEXT;
+
+END;
+$$ LANGUAGE 'plpgsql';
