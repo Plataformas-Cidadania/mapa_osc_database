a5c729a146df02ed3d496e1b8df3a5ce88598511
Denise <Denise@DESKTOP-J7J6R3E>
2017-08-21 12:54:46 -0300

    MOSC-1338 Criar adaptação no back-end para a opção "não possui"

---
+++ database/2.3.0/46_inserir_participacao_social_outra.sql
@@ -0,0 +1,29 @@
+﻿-- Function: portal.inserir_participacao_social_outra(integer, text, text, boolean)
+
+DROP FUNCTION portal.inserir_participacao_social_outra(integer, text, text, boolean);
+
+CREATE OR REPLACE FUNCTION portal.inserir_participacao_social_outra(id integer, nomeparticipacaosocialoutra text, ftparticipacaosocialoutra text, oficial boolean, naopossui boolean)
+  RETURNS boolean AS
+$BODY$
+
+DECLARE
+	status BOOLEAN;	
+
+BEGIN
+	INSERT INTO osc.tb_participacao_social_outra (id_osc, tx_nome_participacao_social_outra, ft_participacao_social_outra, bo_oficial, bo_nao_possui) 
+	VALUES (id, nomeparticipacaosocialoutra, ftparticipacaosocialoutra, oficial, naopossui);
+
+	status := true;
+	RETURN status;
+
+EXCEPTION 
+	WHEN others THEN 
+		status := false;
+		RETURN status;
+
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+ALTER FUNCTION portal.inserir_participacao_social_outra(integer, text, text, boolean, boolean)
+  OWNER TO postgres;
