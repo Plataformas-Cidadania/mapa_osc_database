7225c05cccee1b37b3a72a01b8f1395ad685c864
vagnerpraia <vagnerpraia@gmail.com>
2017-09-24 18:55:10 -0300

    Atualização função de histórico

--- database/2.4.0/8_obter_osc_atualizadas_recentemente.sql
+++ database/2.4.0/8_obter_osc_atualizadas_recentemente.sql
@@ -1,12 +1,23 @@
 ﻿DROP FUNCTION IF EXISTS portal.obter_osc_atualizadas_recentemente();
 
 CREATE OR REPLACE FUNCTION portal.obter_osc_atualizadas_recentemente() RETURNS TABLE(
-	id_osc INTEGER
+	id_osc INTEGER, 
+	tx_nome_osc TEXT
 )AS $$
 
 BEGIN 
 	RETURN QUERY 
-		SELECT id_osc FROM log.tb_log_alteracao ORDER BY dt_alteracao DESC LIMIT 10;
+		SELECT 
+			tb_log_alteracao.id_osc, 
+			vw_busca_resultado.tx_nome_osc 
+		FROM 
+			log.tb_log_alteracao INNER JOIN 
+			osc.vw_busca_resultado ON 
+			tb_log_alteracao.id_osc = vw_busca_resultado.id_osc 
+			ORDER BY dt_alteracao DESC 
+			LIMIT 10;
 	
 END;
 $$ LANGUAGE 'plpgsql';
+
+SELECT * FROM portal.obter_osc_atualizadas_recentemente();
