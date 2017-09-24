b65789cc56907be25c0e940384ca10dcd009ce71
vagnerpraia <vagnerpraia@gmail.com>
2017-09-24 04:19:16 -0300

    Atualizações do histórico de atualizações

---
+++ database/2.4.0/7_alter_tb_log.sql
@@ -0,0 +1,1 @@
+ALTER TABLE log.tb_log_alteracao RENAME COLUMN id_tabela TO id_osc;
---
+++ database/2.4.0/8_obter_osc_atualizadas_recentemente.sql
@@ -0,0 +1,12 @@
+﻿DROP FUNCTION IF EXISTS portal.obter_osc_atualizadas_recentemente();
+
+CREATE OR REPLACE FUNCTION portal.obter_osc_atualizadas_recentemente() RETURNS TABLE(
+	id_osc INTEGER
+)AS $$
+
+BEGIN 
+	RETURN QUERY 
+		SELECT id_osc FROM log.tb_log_alteracao ORDER BY dt_alteracao DESC LIMIT 10;
+	
+END;
+$$ LANGUAGE 'plpgsql';
