ad551e4f7990a6fe6f00a7dcc15f439b4e25db90
vagnerpraia <vagnerpraia@gmail.com>
2017-09-25 18:54:38 -0300

    Inclusão de geolocalização função de busca de OSCs por área de atuação

--- database/2.4.0/9_obter_osc_por_area_atuacao.sql
+++ database/2.4.0/9_obter_osc_por_area_atuacao.sql
@@ -1,16 +1,32 @@
-﻿DROP FUNCTION IF EXISTS portal.obter_osc_por_area_atuacao(area_atuacao INTEGER, municipio INTEGER, quantidade_oscs INTEGER);
+﻿DROP FUNCTION IF EXISTS portal.obter_osc_por_area_atuacao(area_atuacao INTEGER, geolocalizacao DOUBLE PRECISION[], municipio INTEGER, quantidade_oscs INTEGER);
 
-CREATE OR REPLACE FUNCTION portal.obter_osc_por_area_atuacao(area_atuacao INTEGER, municipio INTEGER, quantidade_oscs INTEGER) RETURNS TABLE(
+CREATE OR REPLACE FUNCTION portal.obter_osc_por_area_atuacao(area_atuacao INTEGER, geolocalizacao DOUBLE PRECISION[], municipio INTEGER, quantidade_oscs INTEGER) RETURNS TABLE(
 	id_osc INTEGER, 
 	tx_nome_osc TEXT
 )AS $$
 
 DECLARE 
-	query_limit TEXT;
 	join_municipio TEXT;
+	query_order TEXT;
 	where_municipio TEXT;
+	query_limit TEXT;
 	
-BEGIN 
+BEGIN
+	RAISE NOTICE 'Geolocalização: %', geolocalizacao[1];
+	RAISE NOTICE 'Município: %', municipio;
+	RAISE NOTICE 'Quantidade de OSCs: %', quantidade_oscs;
+	
+	IF array_length(geolocalizacao, 1) = 2 THEN 
+		join_municipio := ' INNER JOIN osc.vw_geo_osc ON vw_geo_osc.id_osc = vw_osc_area_atuacao.id_osc ';
+		query_order := ' ST_Distance(
+			ST_GeomFromText(''POINT(vw_geo_osc.geo_lng vw_geo_osc.geo_lat)'', 4326),
+			ST_GeomFromText(''POINT(' || geolocalizacao[2] || ' ' || geolocalizacao[1] || ')'', 4326)
+		) '; 
+	ELSE 
+		join_municipio := ' '; 
+		query_order := ' RANDOM() '; 
+	END IF; 
+	
 	IF municipio > 0 THEN 
 		join_municipio := ' INNER JOIN osc.vw_geo_osc ON vw_geo_osc.id_osc = vw_osc_area_atuacao.id_osc ';
 		where_municipio := ' AND vw_geo_osc.cd_municipio = ' || municipio || ' '; 
@@ -33,7 +49,9 @@ BEGIN
 			join_municipio || '
 			WHERE vw_osc_area_atuacao.cd_area_atuacao = ' || area_atuacao || where_municipio || '
 			GROUP BY vw_osc_area_atuacao.id_osc, vw_busca_resultado.tx_nome_osc 
-			ORDER BY RANDOM()' || query_limit;
+			ORDER BY' || query_order || query_limit;
 	
 END;
 $$ LANGUAGE 'plpgsql';
+
+SELECT * FROM portal.obter_osc_por_area_atuacao(2, '{123456.5, 987654.1}', 0, 5);
