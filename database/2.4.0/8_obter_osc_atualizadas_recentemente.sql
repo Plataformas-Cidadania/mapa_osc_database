7836c5176f1dc8460e804e8bed578e96c8c5e25c
vagnerpraia <vagnerpraia@gmail.com>
2017-09-26 18:06:54 -0300

    Inclusão do parâmetro de limit na função de atualizadas

--- database/2.4.0/8_obter_osc_atualizadas_recentemente.sql
+++ database/2.4.0/8_obter_osc_atualizadas_recentemente.sql
@@ -1,13 +1,14 @@
-﻿DROP FUNCTION IF EXISTS portal.obter_osc_atualizadas_recentemente();
+﻿DROP FUNCTION IF EXISTS portal.obter_osc_atualizadas_recentemente(quantidade_oscs INTEGER);
 
-CREATE OR REPLACE FUNCTION portal.obter_osc_atualizadas_recentemente() RETURNS TABLE(
+CREATE OR REPLACE FUNCTION portal.obter_osc_atualizadas_recentemente(quantidade_oscs INTEGER) RETURNS TABLE(
 	id_osc INTEGER, 
 	tx_nome_osc TEXT
 )AS $$
 
 BEGIN 
 	RETURN QUERY 
-		SELECT tb_log_alteracao.id_osc, vw_busca_resultado.tx_nome_osc 
+	EXECUTE 
+		'SELECT tb_log_alteracao.id_osc, vw_busca_resultado.tx_nome_osc 
 		FROM 
 			(
 				SELECT tb_log_alteracao.id_osc, MAX(dt_alteracao) AS dt_alteracao 
@@ -16,7 +17,7 @@ BEGIN
 			) AS tb_log_alteracao INNER JOIN 
 			osc.vw_busca_resultado ON tb_log_alteracao.id_osc = vw_busca_resultado.id_osc 
 			ORDER BY tb_log_alteracao.dt_alteracao DESC 
-			LIMIT 10;
+			LIMIT ' || quantidade_oscs;
 	
 END;
 $$ LANGUAGE 'plpgsql';
