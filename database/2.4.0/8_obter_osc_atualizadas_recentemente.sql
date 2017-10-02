482383c9a2ddf98347a71919be504dbbdd356b20
vagnerpraia <vagnerpraia@gmail.com>
2017-10-02 19:31:41 -0300

    Atualização ETL estados e municípios

--- database/2.4.0/8_obter_osc_atualizadas_recentemente.sql
+++ database/2.4.0/8_obter_osc_atualizadas_recentemente.sql
@@ -7,17 +7,17 @@ CREATE OR REPLACE FUNCTION portal.obter_osc_atualizadas_recentemente(quantidade_
 
 BEGIN 
 	RETURN QUERY 
-	EXECUTE 
-		'SELECT tb_log_alteracao.id_osc, vw_busca_resultado.tx_nome_osc 
-		FROM 
-			(
-				SELECT tb_log_alteracao.id_osc, MAX(dt_alteracao) AS dt_alteracao 
-				FROM log.tb_log_alteracao 
-				GROUP BY tb_log_alteracao.id_osc
-			) AS tb_log_alteracao INNER JOIN 
-			osc.vw_busca_resultado ON tb_log_alteracao.id_osc = vw_busca_resultado.id_osc 
-			ORDER BY tb_log_alteracao.dt_alteracao DESC 
-			LIMIT ' || quantidade_oscs;
+		EXECUTE 
+			'SELECT tb_log_alteracao.id_osc, vw_busca_resultado.tx_nome_osc 
+			FROM 
+				(
+					SELECT tb_log_alteracao.id_osc, MAX(dt_alteracao) AS dt_alteracao 
+					FROM log.tb_log_alteracao 
+					GROUP BY tb_log_alteracao.id_osc
+				) AS tb_log_alteracao INNER JOIN 
+				osc.vw_busca_resultado ON tb_log_alteracao.id_osc = vw_busca_resultado.id_osc 
+				ORDER BY tb_log_alteracao.dt_alteracao DESC 
+				LIMIT ' || quantidade_oscs;
 	
 END;
 $$ LANGUAGE 'plpgsql';
