d4ffa2cd1434b07ff86929130129463d0c29c263
vagnerpraia <vagnerpraia@gmail.com>
2017-11-01 15:16:58 -0200

    Atualização da busca por regiões

---
+++ database/2.4.1/03_buscar_osc_regiao_lista.sql
@@ -0,0 +1,36 @@
+﻿DROP FUNCTION IF EXISTS portal.buscar_osc_regiao_lista(param NUMERIC, limit_result INTEGER, offset_result INTEGER);
+
+CREATE OR REPLACE FUNCTION portal.buscar_osc_regiao_lista(param NUMERIC, limit_result INTEGER, offset_result INTEGER) RETURNS TABLE(
+	id_osc INTEGER,
+	tx_nome_osc TEXT,
+	cd_identificador_osc TEXT,
+	tx_natureza_juridica_osc TEXT,
+	tx_endereco_osc TEXT,
+	geo_lat DOUBLE PRECISION,
+	geo_lng DOUBLE PRECISION,
+	tx_nome_atividade_economica TEXT,
+	im_logo TEXT
+) AS $$
+
+DECLARE
+	id_osc_search INTEGER;
+
+BEGIN
+	RETURN QUERY
+		SELECT
+			vw_busca_resultado.id_osc,
+			vw_busca_resultado.tx_nome_osc,
+			vw_busca_resultado.cd_identificador_osc,
+			vw_busca_resultado.tx_natureza_juridica_osc,
+			vw_busca_resultado.tx_endereco_osc,
+			vw_busca_resultado.geo_lat,
+			vw_busca_resultado.geo_lng,
+			vw_busca_resultado.tx_nome_atividade_economica,
+			vw_busca_resultado.im_logo
+		FROM 
+			osc.vw_busca_resultado
+		WHERE vw_busca_resultado.id_osc IN (
+			SELECT a.id_osc FROM portal.buscar_osc_regiao(param, limit_result, offset_result) a
+		);
+END;
+$$ LANGUAGE 'plpgsql';
