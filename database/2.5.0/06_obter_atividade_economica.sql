7ced04570fdfd8d830411b12d47d741317f9b82e
Denise <Denise@DESKTOP-J7J6R3E>
2017-11-10 13:51:56 -0200

    MOSC-1431 - Criar Rota para autocomplete de atividade econômica

---
+++ database/2.5.0/06_obter_atividade_economica.sql
@@ -0,0 +1,40 @@
+﻿-- Function: portal.obter_atividade_economica(text, integer, integer)
+
+DROP FUNCTION portal.obter_atividade_economica(text, integer, integer);
+
+CREATE OR REPLACE FUNCTION portal.obter_atividade_economica(IN param text, IN limit_result integer, IN offset_result integer)
+  RETURNS TABLE(tx_atividade_economica text ) AS
+$BODY$
+
+DECLARE 
+	query_limit TEXT; 
+
+BEGIN	
+	IF offset_result > 0 THEN 
+		query_limit := 'LIMIT ' || limit_result || ' OFFSET ' || offset_result || ';'; 
+	ELSIF limit_result > 0 THEN 
+		query_limit := 'LIMIT ' || limit_result || ';'; 
+	ELSE 
+		query_limit := ';'; 
+	END IF; 
+	
+	param := TRANSLATE(UNACCENT(param::TEXT), ' /_-', '');
+	
+	RETURN QUERY
+		EXECUTE 
+			'SELECT
+				tx_nome_subclasse_atividade_economica
+			FROM 
+				syst.dc_subclasse_atividade_economica
+			WHERE 
+				UNACCENT(tx_nome_subclasse_atividade_economica) ILIKE ''' || param::TEXT || '%'' 
+			ORDER BY 
+				similarity(tx_nome_subclasse_atividade_economica, ''' || param::TEXT || ''') DESC ' || query_limit;
+	
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100
+  ROWS 1000;
+ALTER FUNCTION portal.obter_atividade_economica(text, integer, integer)
+  OWNER TO postgres;
