6df45153c6cebf8da8b968c8ea56dca7bb27569b
vagnerpraia <vagnerpraia@gmail.com>
2017-11-27 04:17:07 -0200

    Atualização ETL CEBAS Saúde

--- database/2.5.0/01_atualizar_area_atuacao_osc.sql
+++ database/2.5.0/01_atualizar_area_atuacao_osc.sql
@@ -14,17 +14,17 @@ DECLARE
 	dado_nao_delete INTEGER[];
 	flag_update BOOLEAN;
 	osc INTEGER;
-
+	
 BEGIN
 	nome_tabela := 'osc.tb_area_atuacao';
 	tipo_identificador := lower(tipo_identificador);
-
+	
 	SELECT INTO fonte_dados * FROM portal.verificar_fonte(fonte);
-
+	
 	IF fonte_dados IS null THEN
 		RAISE EXCEPTION 'fonte_invalida';
 	END IF;
-
+	
 	IF tipo_identificador = 'cnpj' THEN
 		SELECT id_osc INTO osc FROM osc.tb_osc WHERE cd_identificador_osc = identificador;
 	ELSIF tipo_identificador = 'id_osc' THEN
@@ -32,40 +32,40 @@ BEGIN
 	ELSE
 		RAISE EXCEPTION 'tipo_identificador_invalido';
 	END IF;
-
+	
 	IF osc IS null THEN
 		RAISE EXCEPTION 'osc_nao_encontrada';
 	ELSIF osc != ALL(fonte_dados.representacao) THEN
 		RAISE EXCEPTION 'permissao_negada_usuario';
 	END IF;
-
+	
     IF jsonb_typeof(json) = 'object' THEN
  		json := jsonb_build_array(json);
  	END IF;
-
+	
 	FOR objeto IN (SELECT * FROM jsonb_populate_recordset(null::osc.tb_area_atuacao, json))
 	LOOP
 		dado_anterior := null;
-
+		
 		IF tipo_busca = 1 THEN
 			SELECT INTO dado_anterior *
 			FROM osc.tb_area_atuacao
 			WHERE id_area_atuacao = objeto.id_area_atuacao
 			AND id_osc = osc;
-
+			
 		ELSIF tipo_busca = 2 THEN
 			SELECT INTO dado_anterior *
 			FROM osc.tb_area_atuacao
 			WHERE cd_area_atuacao = objeto.cd_area_atuacao
 			AND cd_subarea_atuacao = objeto.cd_subarea_atuacao
 			AND id_osc = osc;
-
+			
 		ELSE
 			RAISE EXCEPTION 'tipo_busca_invalido';
-
+			
 		END IF;
-
-	IF COUNT(dado_anterior.id_osc) = 0 THEN
+		
+		IF dado_anterior.id_area_atuacao IS null THEN 
 			INSERT INTO osc.tb_area_atuacao (
 				id_osc,
 				cd_area_atuacao,
@@ -79,34 +79,34 @@ BEGIN
 				objeto.tx_nome_outra,
 				fonte_dados.nome_fonte
 			) RETURNING * INTO dado_posterior;
-
+			
 			dado_nao_delete := array_append(dado_nao_delete, dado_posterior.id_area_atuacao);
-
+			
 			PERFORM * FROM portal.inserir_log_atualizacao(nome_tabela, osc, fonte, data_atualizacao, null, row_to_json(dado_posterior),id_carga);
-
+			
 		ELSE
 			dado_posterior := dado_anterior;
 			dado_nao_delete := array_append(dado_nao_delete, dado_posterior.id_area_atuacao);
 			flag_update := false;
-
+			
 			IF (SELECT a.flag FROM portal.verificar_dado(dado_anterior.cd_area_atuacao::TEXT, dado_anterior.ft_area_atuacao, objeto.cd_area_atuacao::TEXT, fonte_dados.prioridade, null_valido) AS a) THEN
 				dado_posterior.cd_area_atuacao := objeto.cd_area_atuacao;
 				dado_posterior.ft_area_atuacao := fonte_dados.nome_fonte;
 				flag_update := true;
 			END IF;
-
+			
 			IF (SELECT a.flag FROM portal.verificar_dado(dado_anterior.cd_subarea_atuacao::TEXT, dado_anterior.ft_area_atuacao, objeto.cd_subarea_atuacao::TEXT, fonte_dados.prioridade, null_valido) AS a) THEN
 				dado_posterior.cd_subarea_atuacao := objeto.cd_subarea_atuacao;
 				dado_posterior.ft_area_atuacao := fonte_dados.nome_fonte;
 				flag_update := true;
 			END IF;
-
+			
 			IF (SELECT a.flag FROM portal.verificar_dado(dado_anterior.tx_nome_outra::TEXT, dado_anterior.ft_area_atuacao, objeto.tx_nome_outra::TEXT, fonte_dados.prioridade, null_valido) AS a) THEN
 				dado_posterior.tx_nome_outra := objeto.tx_nome_outra;
 				dado_posterior.ft_area_atuacao := fonte_dados.nome_fonte;
 				flag_update := true;
 			END IF;
-
+			
 			IF flag_update THEN
 				UPDATE osc.tb_area_atuacao
 				SET	cd_area_atuacao = dado_posterior.cd_area_atuacao,
@@ -114,14 +114,14 @@ BEGIN
 					tx_nome_outra = dado_posterior.tx_nome_outra,
 					ft_area_atuacao = dado_posterior.ft_area_atuacao
 				WHERE id_area_atuacao = dado_posterior.id_area_atuacao;
-
+				
 				PERFORM * FROM portal.inserir_log_atualizacao(nome_tabela, osc, fonte, data_atualizacao, row_to_json(dado_anterior), row_to_json(dado_posterior),id_carga);
 			END IF;
-
+			
 		END IF;
-
+		
 	END LOOP;
-
+	
 	IF delete_valido THEN
 		FOR objeto IN (SELECT * FROM osc.tb_area_atuacao WHERE id_osc = osc AND id_area_atuacao != ALL(dado_nao_delete))
 		LOOP
@@ -131,17 +131,17 @@ BEGIN
 			END IF;
 		END LOOP;
 	END IF;
-
+	
 	flag := true;
 	mensagem := 'Área de atuação de OSC atualizado.';
-
+	
 	RETURN NEXT;
-
+	
 EXCEPTION
 	WHEN others THEN
 		flag := false;
 		SELECT INTO mensagem a.mensagem FROM portal.verificar_erro(SQLSTATE, SQLERRM, fonte, identificador, data_atualizacao::TIMESTAMP, erro_log, id_carga) AS a;
 		RETURN NEXT;
-
+		
 END;
 $$ LANGUAGE 'plpgsql';
