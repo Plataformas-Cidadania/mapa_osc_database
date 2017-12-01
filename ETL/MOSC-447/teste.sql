51001fe1ce783937ead3bbf3df3049dac3094893
vagnerpraia <vagnerpraia@gmail.com>
2017-11-30 22:48:14 -0200

    Atualização Finep

--- ETL/MOSC-447/teste.sql
+++
@@ -1,9 +0,0 @@
-﻿/*
-cd_identificador_osc: 5880992000102
-tx_razao_social_osc: "Centro De Trad Gauchas Sent Da Front Ctg"
-
-Fonte: "MTE/RAIS"
-*/
-
-DELETE FROM osc.tb_fonte_recursos_projeto WHERE id_projeto > 24035;
-DELETE FROM osc.tb_projeto WHERE id_projeto > 24035;
