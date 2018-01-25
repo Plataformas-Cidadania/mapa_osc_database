ed584a201e860ad7e4cf68a98ede295341e481ac
tmnascimento <tmnascimento@gmail.com>
2018-01-25 17:42:41 -0200

    Update 31_trigger_view.sql
    Correção da view portal.vw_busca_resultado para
    osc.vw_busca_resultado

--- database/2.4.1/31_trigger_view.sql
+++ database/2.4.1/31_trigger_view.sql
@@ -1,12 +1,12 @@
 CREATE UNIQUE INDEX ix_vw_busca_resultado
-    ON portal.vw_busca_resultado USING btree
+    ON osc.vw_busca_resultado USING btree
     (id_osc ASC NULLS LAST)
     TABLESPACE pg_default;
 
 CREATE OR REPLACE FUNCTION vw_busca_resultado()
 RETURNS TRIGGER AS $$
 BEGIN
-  REFRESH MATERIALIZED VIEW CONCURRENTLY portal.vw_busca_resultado;
+  REFRESH MATERIALIZED VIEW CONCURRENTLY osc.vw_busca_resultado;
   RETURN NULL;
 END;
 $$ LANGUAGE plpgsql;
