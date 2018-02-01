249fa6a7a701517eb3191dd4054d0322c5787115
EricFerreiraS <eric_santos6@hotmail.com>
2018-02-01 15:28:53 -0200

    Atualizacao ELT

--- database/2.5.3/01_atualizacao_situacao_projeto.sql
+++ database/2.5.3/01_atualizacao_situacao_projeto.sql
@@ -1,14 +1,14 @@
-UPDATE syst.dc_status_projeto 
-SET tx_nome_status_projeto = 'Proposta' 
+UPDATE syst.dc_status_projeto
+SET tx_nome_status_projeto = 'Proposta'
 WHERE tx_nome_status_projeto = 'Planejado';
 
-UPDATE syst.dc_status_projeto 
-SET tx_nome_status_projeto = 'Projeto em andamento' 
-WHERE tx_nome_status_projeto = 'Em Execução';
+UPDATE syst.dc_status_projeto
+SET tx_nome_status_projeto = 'Projeto em andamento'
+WHERE tx_nome_status_projeto = 'Em ExecuÃ§Ã£o';
 
 INSERT INTO syst.dc_status_projeto (cd_status_projeto, tx_nome_status_projeto)
 VALUES (5, 'Arquivado, cancelado ou indeferido');
 
-UPDATE osc.tb_projeto 
-SET cd_status_projeto = (SELECT cd_status_projeto FROM syst.dc_status_projeto WHERE tx_nome_status_projeto = 'Projeto em andamento') 
+UPDATE osc.tb_projeto
+SET cd_status_projeto = (SELECT cd_status_projeto FROM syst.dc_status_projeto WHERE tx_nome_status_projeto = 'Projeto em andamento')
 WHERE cd_status_projeto = (SELECT cd_status_projeto FROM syst.dc_status_projeto WHERE tx_nome_status_projeto = 'Proposta');
