b4aee57f1e8f0f6ad36a540bd4be305bf5d8e3d8
vagnerpraia <vagnerpraia@gmail.com>
2018-02-21 19:03:47 -0300

    Issue: MOSC-1199
    Subject: Alteração no serviço de Títulos e Certificações
    Detail: Criação do script de adição dos campos de estados e municípios na tabela de certificados.

---
+++ database/2.6.0/03_alter_tb_certificado.sql.sql
@@ -0,0 +1,13 @@
+ALTER TABLE osc.tb_certificado ADD cd_municipio NUMERIC DEFAULT null;
+
+ALTER TABLE osc.tb_certificado 
+ADD CONSTRAINT fk_cd_municipio 
+FOREIGN KEY (cd_municipio) 
+REFERENCES spat.ed_municipio(edmu_cd_municipio);
+
+ALTER TABLE osc.tb_certificado ADD cd_uf NUMERIC DEFAULT null;
+
+ALTER TABLE osc.tb_certificado 
+ADD CONSTRAINT fk_cd_uf 
+FOREIGN KEY (cd_uf) 
+REFERENCES spat.ed_uf(eduf_cd_uf);
