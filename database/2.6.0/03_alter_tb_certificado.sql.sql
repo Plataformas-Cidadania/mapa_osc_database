c2a369276f2ea858e7b78c653273f0bd6f46df04
vagnerpraia <vagnerpraia@gmail.com>
2018-02-22 10:32:44 -0300

    Issue: MOSC-1199
    Subject: Alteração no serviço de Títulos e Certificações
    Detail: Atualização da view de certificados.

--- database/2.6.0/03_alter_tb_certificado.sql.sql
+++ database/2.6.0/03_alter_tb_certificado.sql.sql
@@ -1,13 +1,15 @@
 ALTER TABLE osc.tb_certificado ADD cd_municipio NUMERIC DEFAULT null;
+ALTER TABLE osc.tb_certificado ADD cd_uf NUMERIC DEFAULT null;
 
 ALTER TABLE osc.tb_certificado 
 ADD CONSTRAINT fk_cd_municipio 
 FOREIGN KEY (cd_municipio) 
 REFERENCES spat.ed_municipio(edmu_cd_municipio);
 
-ALTER TABLE osc.tb_certificado ADD cd_uf NUMERIC DEFAULT null;
-
 ALTER TABLE osc.tb_certificado 
 ADD CONSTRAINT fk_cd_uf 
 FOREIGN KEY (cd_uf) 
 REFERENCES spat.ed_uf(eduf_cd_uf);
+
+ALTER TABLE osc.tb_certificado ADD ft_municipio TEXT DEFAULT null;
+ALTER TABLE osc.tb_certificado ADD ft_estado TEXT DEFAULT null;
