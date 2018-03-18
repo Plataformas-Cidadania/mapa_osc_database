17e6d06e6d0f86bd20a6e79402cb03c76fadbd8b
vagnerpraia <vagnerpraia@gmail.com>
2018-03-18 04:43:50 -0300

    Issue: MOSC-1504 Subject: Adição de campos "não possui" para projetos da OSC Detail: Acerto na formatação da função de obter projetos.

--- database/2.6.0/41_obter_osc_projetos.sql
+++ database/2.6.0/41_obter_osc_projetos.sql
@@ -15,75 +15,81 @@ BEGIN
 	IF tb_osc.bo_nao_possui_projeto IS false THEN 
 		IF tipo = 1 THEN 
 			resultado := (
-				SELECT 
-					jsonb_agg(
-						jsonb_build_object(
-							'bo_nao_possui_projeto', false, 
-							'ft_nao_possui_projeto', tb_osc.ft_nao_possui_projeto, 
-							'projetos', jsonb_build_object(
-								'id_projeto', id_projeto, 
-								'tx_identificador_projeto_externo', tx_identificador_projeto_externo, 
-								'ft_identificador_projeto_externo', ft_identificador_projeto_externo, 
-								'tx_nome_projeto', tx_nome_projeto, 
-								'ft_nome_projeto', ft_nome_projeto, 
-								'cd_status_projeto', cd_status_projeto, 
-								'tx_nome_status_projeto', tx_nome_status_projeto, 
-								'tx_status_projeto_outro', tx_status_projeto_outro, 
-								'ft_status_projeto', ft_status_projeto, 
-								'dt_data_inicio_projeto', dt_data_inicio_projeto, 
-								'ft_data_inicio_projeto', ft_data_inicio_projeto, 
-								'dt_data_fim_projeto', dt_data_fim_projeto, 
-								'ft_data_fim_projeto', ft_data_fim_projeto, 
-								'tx_link_projeto', tx_link_projeto, 
-								'ft_link_projeto', ft_link_projeto, 
-								'nr_total_beneficiarios', nr_total_beneficiarios, 
-								'ft_total_beneficiarios', ft_total_beneficiarios, 
-								'nr_valor_total_projeto', nr_valor_total_projeto, 
-								'ft_valor_total_projeto', ft_valor_total_projeto, 
-								'nr_valor_captado_projeto', nr_valor_captado_projeto, 
-								'ft_valor_captado_projeto', ft_valor_captado_projeto, 
-								'tx_metodologia_monitoramento', tx_metodologia_monitoramento, 
-								'ft_metodologia_monitoramento', ft_metodologia_monitoramento, 
-								'tx_descricao_projeto', tx_descricao_projeto, 
-								'ft_descricao_projeto', ft_descricao_projeto, 
-								'cd_abrangencia_projeto', cd_abrangencia_projeto, 
-								'tx_nome_abrangencia_projeto', tx_nome_abrangencia_projeto, 
-								'ft_abrangencia_projeto', ft_abrangencia_projeto, 
-								'cd_zona_atuacao_projeto', cd_zona_atuacao_projeto, 
-								'tx_nome_zona_atuacao', tx_nome_zona_atuacao, 
-								'ft_zona_atuacao_projeto', ft_zona_atuacao_projeto, 
-								'cd_municipio', cd_municipio, 
-								'ft_municipio', ft_municipio, 
-								'cd_uf', cd_uf, 
-								'ft_uf', ft_uf
-							)
-						)
+				jsonb_build_object(
+					'bo_nao_possui_projeto', false, 
+					'ft_nao_possui_projeto', tb_osc.ft_nao_possui_projeto, 
+					'projetos', (
+							SELECT 
+								jsonb_agg(
+									jsonb_build_object(
+										'id_projeto', id_projeto, 
+										'tx_identificador_projeto_externo', tx_identificador_projeto_externo, 
+										'ft_identificador_projeto_externo', ft_identificador_projeto_externo, 
+										'tx_nome_projeto', tx_nome_projeto, 
+										'ft_nome_projeto', ft_nome_projeto, 
+										'cd_status_projeto', cd_status_projeto, 
+										'tx_nome_status_projeto', tx_nome_status_projeto, 
+										'tx_status_projeto_outro', tx_status_projeto_outro, 
+										'ft_status_projeto', ft_status_projeto, 
+										'dt_data_inicio_projeto', dt_data_inicio_projeto, 
+										'ft_data_inicio_projeto', ft_data_inicio_projeto, 
+										'dt_data_fim_projeto', dt_data_fim_projeto, 
+										'ft_data_fim_projeto', ft_data_fim_projeto, 
+										'tx_link_projeto', tx_link_projeto, 
+										'ft_link_projeto', ft_link_projeto, 
+										'nr_total_beneficiarios', nr_total_beneficiarios, 
+										'ft_total_beneficiarios', ft_total_beneficiarios, 
+										'nr_valor_total_projeto', nr_valor_total_projeto, 
+										'ft_valor_total_projeto', ft_valor_total_projeto, 
+										'nr_valor_captado_projeto', nr_valor_captado_projeto, 
+										'ft_valor_captado_projeto', ft_valor_captado_projeto, 
+										'tx_metodologia_monitoramento', tx_metodologia_monitoramento, 
+										'ft_metodologia_monitoramento', ft_metodologia_monitoramento, 
+										'tx_descricao_projeto', tx_descricao_projeto, 
+										'ft_descricao_projeto', ft_descricao_projeto, 
+										'cd_abrangencia_projeto', cd_abrangencia_projeto, 
+										'tx_nome_abrangencia_projeto', tx_nome_abrangencia_projeto, 
+										'ft_abrangencia_projeto', ft_abrangencia_projeto, 
+										'cd_zona_atuacao_projeto', cd_zona_atuacao_projeto, 
+										'tx_nome_zona_atuacao', tx_nome_zona_atuacao, 
+										'ft_zona_atuacao_projeto', ft_zona_atuacao_projeto, 
+										'cd_municipio', cd_municipio, 
+										'ft_municipio', ft_municipio, 
+										'cd_uf', cd_uf, 
+										'ft_uf', ft_uf
+									)
+								)
+							FROM 
+								portal.vw_osc_projeto 
+							WHERE 
+								id_osc::TEXT = param 
+							OR 
+								tx_apelido_osc = param
 					)
-				FROM 
-					portal.vw_osc_projeto 
-				WHERE 
-					id_osc::TEXT = param OR 
-					tx_apelido_osc = param
+				)
 			);
 			
 		ELSIF tipo = 2 THEN 
 			resultado := (
-				SELECT 
-					jsonb_agg(
-						jsonb_build_object(
-							'bo_nao_possui_projeto', false, 
-							'ft_nao_possui_projeto', tb_osc.ft_nao_possui_projeto, 
-							'projetos', jsonb_build_object(
-								'id_projeto', id_projeto, 
-								'tx_nome_projeto', tx_nome_projeto
-							)
-						)
+				jsonb_build_object(
+					'bo_nao_possui_projeto', false, 
+					'ft_nao_possui_projeto', tb_osc.ft_nao_possui_projeto, 
+					'projetos', (
+							SELECT 
+								jsonb_agg(
+									jsonb_build_object(
+										'id_projeto', id_projeto, 
+										'tx_nome_projeto', tx_nome_projeto
+									)
+								)
+							FROM 
+								portal.vw_osc_projeto 
+							WHERE 
+								id_osc::TEXT = param 
+							OR 
+								tx_apelido_osc = param
 					)
-				FROM 
-					portal.vw_osc_projeto 
-				WHERE 
-					id_osc::TEXT = param OR 
-					tx_apelido_osc = param
+				)
 			);
 			
 		ELSE 
