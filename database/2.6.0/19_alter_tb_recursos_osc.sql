7f355cdb959bf159ca38d1584ce61d03f295309d
vagnerpraia <vagnerpraia@gmail.com>
2018-03-21 16:31:40 -0300

    Acertos funções da 2.6

--- database/2.6.0/19_alter_tb_recursos_osc.sql
+++ database/2.6.0/19_alter_tb_recursos_osc.sql
@@ -1,4 +1,5 @@
 ALTER TABLE osc.tb_recursos_osc ALTER COLUMN cd_fonte_recursos_osc DROP NOT NULL;
+ALTER TABLE osc.tb_recursos_osc ADD cd_origem_fonte_recursos_osc INTEGER DEFAULT null;
 
 ALTER TABLE osc.tb_recursos_osc DROP CONSTRAINT IF EXISTS un_recursos_osc;
 ALTER TABLE osc.tb_recursos_osc ADD CONSTRAINT un_recursos_osc UNIQUE (id_osc, cd_origem_fonte_recursos_osc, cd_fonte_recursos_osc, dt_ano_recursos_osc);
--- database/2.6.0/33_alter_tb_recursos_osc.sql
+++ database/2.6.0/33_alter_tb_recursos_osc.sql
@@ -1,4 +1,2 @@
-ALTER TABLE osc.tb_recursos_osc ADD cd_origem_fonte_recursos_osc INTEGER DEFAULT null;
-
 UPDATE osc.tb_recursos_osc 
 SET cd_origem_fonte_recursos_osc = (SELECT cd_origem_fonte_recursos_osc FROM syst.dc_fonte_recursos_osc WHERE cd_fonte_recursos_osc = tb_recursos_osc.cd_fonte_recursos_osc);
