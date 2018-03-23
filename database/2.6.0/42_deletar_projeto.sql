ef683150b6cdfc364c47353619e0c751bc6f1c48
vagnerpraia <vagnerpraia@gmail.com>
2018-03-23 02:42:47 -0300

    Atualização da função de delete projeto

--- database/2.6.0/42_deletar_projeto.sql
+++ database/2.6.0/42_deletar_projeto.sql
@@ -38,8 +38,9 @@ BEGIN
 	IF osc <> (SELECT id_osc FROM osc.tb_projeto WHERE id_projeto = id) THEN
 		RAISE EXCEPTION 'id_osc_nao_confere';
 	END IF;
-
-	objeto := (SELECT * FROM osc.tb_tipo_parceria_projeto WHERE id_projeto = id);
+	
+	SELECT INTO objeto * FROM osc.tb_tipo_parceria_projeto WHERE id_projeto = id;
+	
 	IF (SELECT a.flag FROM portal.verificar_delete(fonte_dados.prioridade, ARRAY[objeto.ft_tipo_parceria_projeto]) AS a) THEN
 		DELETE FROM osc.tb_tipo_parceria_projeto WHERE id_projeto = id;
 		PERFORM portal.inserir_log_atualizacao('osc.tb_tipo_parceria_projeto', osc, fonte, data_atualizacao, row_to_json(objeto), null, id_carga);
@@ -47,15 +48,15 @@ BEGIN
 		RAISE EXCEPTION 'prioridade_invalida';
 	END IF;
 	
-	objeto := (SELECT * FROM osc.tb_fonte_recursos_projeto WHERE id_projeto = id);
+	SELECT INTO objeto * FROM osc.tb_fonte_recursos_projeto WHERE id_projeto = id;
 	IF (SELECT a.flag FROM portal.verificar_delete(fonte_dados.prioridade, ARRAY[objeto.ft_fonte_recursos_projeto, objeto.ft_orgao_concedente]) AS a) THEN
 		DELETE FROM osc.tb_fonte_recursos_projeto WHERE id_projeto = id;
 		PERFORM portal.inserir_log_atualizacao('osc.tb_fonte_recursos_projeto', osc, fonte, data_atualizacao, row_to_json(objeto), null, id_carga);
 	ELSE
 		RAISE EXCEPTION 'prioridade_invalida';
 	END IF;
-			
-	objeto := (SELECT * FROM osc.tb_area_atuacao_outra_projeto WHERE id_projeto = id);
+	
+	SELECT INTO objeto * FROM osc.tb_area_atuacao_outra_projeto WHERE id_projeto = id;
 	IF (SELECT a.flag FROM portal.verificar_delete(fonte_dados.prioridade, ARRAY[objeto.ft_area_atuacao_outra_projeto]) AS a) THEN
 		DELETE FROM osc.tb_area_atuacao_outra_projeto WHERE id_projeto = id;
 		PERFORM portal.inserir_log_atualizacao('osc.tb_area_atuacao_outra_projeto', osc, fonte, data_atualizacao, row_to_json(objeto), null, id_carga);
@@ -63,7 +64,7 @@ BEGIN
 		RAISE EXCEPTION 'prioridade_invalida';
 	END IF;
 	
-	objeto := (SELECT * FROM osc.tb_area_atuacao_projeto WHERE id_projeto = id);
+	SELECT INTO objeto * FROM osc.tb_area_atuacao_projeto WHERE id_projeto = id;
 	IF (SELECT a.flag FROM portal.verificar_delete(fonte_dados.prioridade, ARRAY[objeto.ft_area_atuacao_projeto]) AS a) THEN
 		DELETE FROM osc.tb_area_atuacao_projeto WHERE id_projeto = id;
 		PERFORM portal.inserir_log_atualizacao('osc.tb_area_atuacao_projeto', osc, fonte, data_atualizacao, row_to_json(objeto), null, id_carga);
@@ -71,7 +72,7 @@ BEGIN
 		RAISE EXCEPTION 'prioridade_invalida';
 	END IF;
 	
-	objeto := (SELECT * FROM osc.tb_financiador_projeto WHERE id_projeto = id);
+	SELECT INTO objeto * FROM osc.tb_financiador_projeto WHERE id_projeto = id;
 	IF (SELECT a.flag FROM portal.verificar_delete(fonte_dados.prioridade, ARRAY[objeto.ft_nome_financiador]) AS a) THEN
 		DELETE FROM osc.tb_financiador_projeto WHERE id_projeto = id;
 		PERFORM portal.inserir_log_atualizacao('osc.tb_financiador_projeto', osc, fonte, data_atualizacao, row_to_json(objeto), null, id_carga);
@@ -79,7 +80,7 @@ BEGIN
 		RAISE EXCEPTION 'prioridade_invalida';
 	END IF;
 	
-	objeto := (SELECT * FROM osc.tb_localizacao_projeto WHERE id_projeto = id);
+	SELECT INTO objeto * FROM osc.tb_localizacao_projeto WHERE id_projeto = id;
 	IF (SELECT a.flag FROM portal.verificar_delete(fonte_dados.prioridade, ARRAY[objeto.ft_regiao_localizacao_projeto, objeto.ft_nome_regiao_localizacao_projeto, objeto.ft_localizacao_prioritaria]) AS a) THEN
 		DELETE FROM osc.tb_localizacao_projeto WHERE id_projeto = id;
 		PERFORM portal.inserir_log_atualizacao('osc.tb_localizacao_projeto', osc, fonte, data_atualizacao, row_to_json(objeto), null, id_carga);
@@ -87,7 +88,7 @@ BEGIN
 		RAISE EXCEPTION 'prioridade_invalida';
 	END IF;
 	
-	objeto := (SELECT * FROM osc.tb_objetivo_projeto WHERE id_projeto = id);
+	SELECT INTO objeto * FROM osc.tb_objetivo_projeto WHERE id_projeto = id;
 	IF (SELECT a.flag FROM portal.verificar_delete(fonte_dados.prioridade, ARRAY[objeto.ft_objetivo_projeto]) AS a) THEN
 		DELETE FROM osc.tb_objetivo_projeto WHERE id_projeto = id;
 		PERFORM portal.inserir_log_atualizacao('osc.tb_objetivo_projeto', osc, fonte, data_atualizacao, row_to_json(objeto), null, id_carga);
@@ -95,7 +96,7 @@ BEGIN
 		RAISE EXCEPTION 'prioridade_invalida';
 	END IF;
 	
-	objeto := (SELECT * FROM osc.tb_osc_parceira_projeto WHERE id_projeto = id);
+	SELECT INTO objeto * FROM osc.tb_osc_parceira_projeto WHERE id_projeto = id;
 	IF (SELECT a.flag FROM portal.verificar_delete(fonte_dados.prioridade, ARRAY[objeto.ft_osc_parceira_projeto]) AS a) THEN
 		DELETE FROM osc.tb_osc_parceira_projeto WHERE id_projeto = id;
 		PERFORM portal.inserir_log_atualizacao('osc.tb_osc_parceira_projeto', osc, fonte, data_atualizacao, row_to_json(objeto), null, id_carga);
@@ -103,7 +104,7 @@ BEGIN
 		RAISE EXCEPTION 'prioridade_invalida';
 	END IF;
 	
-	objeto := (SELECT * FROM osc.tb_publico_beneficiado_projeto WHERE id_projeto = id);
+	SELECT INTO objeto * FROM osc.tb_publico_beneficiado_projeto WHERE id_projeto = id;
 	IF (SELECT a.flag FROM portal.verificar_delete(fonte_dados.prioridade, ARRAY[objeto.ft_publico_beneficiado_projeto]) AS a) THEN
 		DELETE FROM osc.tb_publico_beneficiado_projeto WHERE id_projeto = id;
 		PERFORM portal.inserir_log_atualizacao('osc.tb_publico_beneficiado_projeto', osc, fonte, data_atualizacao, row_to_json(objeto), null, id_carga);
@@ -111,7 +112,7 @@ BEGIN
 		RAISE EXCEPTION 'prioridade_invalida';
 	END IF;
 	
-	objeto := (SELECT * FROM osc.tb_publico_beneficiado_projeto WHERE id_projeto = id);
+	SELECT INTO objeto * FROM osc.tb_projeto WHERE id_projeto = id;
 	IF (SELECT a.flag FROM portal.verificar_delete(fonte_dados.prioridade, ARRAY[objeto.ft_identificador_projeto_externo, objeto.ft_municipio, objeto.ft_uf, objeto.ft_nome_projeto, objeto.ft_status_projeto, objeto.ft_data_inicio_projeto, objeto.ft_data_fim_projeto, objeto.ft_valor_total_projeto, objeto.ft_valor_captado_projeto, objeto.ft_total_beneficiarios, objeto.ft_abrangencia_projeto, objeto.ft_zona_atuacao_projeto, objeto.ft_descricao_projeto, objeto.ft_metodologia_monitoramento, objeto.ft_link_projeto]) AS a) THEN
 		DELETE FROM osc.tb_projeto WHERE id_projeto = id;
 		PERFORM portal.inserir_log_atualizacao(nome_tabela, osc, fonte, data_atualizacao, row_to_json(objeto), null, id_carga);
