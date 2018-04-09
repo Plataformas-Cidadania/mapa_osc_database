696626f751b6dfcfd92e206205c25c5d9fa2d8f7
vagnerpraia <vagnerpraia@gmail.com>
2018-04-09 19:38:19 -0300

    Atualização do cálculo da transparência da fonte de recursos

--- database/2.6.0/53_obter_barra_transparencia_fontes_recursos.sql
+++ database/2.6.0/53_obter_barra_transparencia_fontes_recursos.sql
@@ -13,10 +13,10 @@ BEGIN
 	RETURN QUERY 
         SELECT 
 			SUM(
-				(CASE WHEN EXISTS(SELECT * FROM osc.tb_recursos_osc WHERE id_osc = osc AND cd_fonte_recursos_osc = 1) THEN peso_campo ELSE 0 END) + 
-				(CASE WHEN EXISTS(SELECT * FROM osc.tb_recursos_osc WHERE id_osc = osc AND cd_fonte_recursos_osc = 2) THEN peso_campo ELSE 0 END) + 
-				(CASE WHEN EXISTS(SELECT * FROM osc.tb_recursos_osc WHERE id_osc = osc AND cd_fonte_recursos_osc = 3) THEN peso_campo ELSE 0 END) + 
-				(CASE WHEN EXISTS(SELECT * FROM osc.tb_recursos_osc WHERE id_osc = osc AND cd_fonte_recursos_osc = 4) THEN peso_campo ELSE 0 END)
+				(CASE WHEN EXISTS(SELECT * FROM osc.tb_recursos_osc JOIN syst.dc_fonte_recursos_osc ON tb_recursos_osc.cd_fonte_recursos_osc = dc_fonte_recursos_osc.cd_fonte_recursos_osc WHERE tb_recursos_osc.id_osc = osc AND tb_recursos_osc.cd_origem_fonte_recursos_osc = 1) THEN peso_campo ELSE 0 END) + 
+				(CASE WHEN EXISTS(SELECT * FROM osc.tb_recursos_osc JOIN syst.dc_fonte_recursos_osc ON tb_recursos_osc.cd_fonte_recursos_osc = dc_fonte_recursos_osc.cd_fonte_recursos_osc WHERE tb_recursos_osc.id_osc = osc AND tb_recursos_osc.cd_origem_fonte_recursos_osc = 2) THEN peso_campo ELSE 0 END) + 
+				(CASE WHEN EXISTS(SELECT * FROM osc.tb_recursos_osc JOIN syst.dc_fonte_recursos_osc ON tb_recursos_osc.cd_fonte_recursos_osc = dc_fonte_recursos_osc.cd_fonte_recursos_osc WHERE tb_recursos_osc.id_osc = osc AND tb_recursos_osc.cd_origem_fonte_recursos_osc = 3) THEN peso_campo ELSE 0 END) + 
+				(CASE WHEN EXISTS(SELECT * FROM osc.tb_recursos_osc JOIN syst.dc_fonte_recursos_osc ON tb_recursos_osc.cd_fonte_recursos_osc = dc_fonte_recursos_osc.cd_fonte_recursos_osc WHERE tb_recursos_osc.id_osc = osc AND tb_recursos_osc.cd_origem_fonte_recursos_osc = 4) THEN peso_campo ELSE 0 END)
 			);
 END;
 
