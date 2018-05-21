b77c9b8f18398fd5b018d3587a7dfc8a5df72add
tmnascimento <tmnascimento@gmail.com>
2018-05-21 19:20:45 -0300

    Update vw_shiny.sql

--- database/2.6.0/vw_shiny.sql
+++ database/2.6.0/vw_shiny.sql
@@ -1,44 +1,29 @@
-CREATE MATERIALIZED VIEW graph.vw_shiny
-TABLESPACE pg_default
-AS
- SELECT p.id_osc,
-    p.cd_identificador_osc,
+
+DROP MATERIALIZED VIEW IF EXISTS graph.vw_shiny;
+
+CREATE MATERIALIZED VIEW graph.vw_shiny AS
+SELECT p.id_osc,
+	-- p.cd_identificador_osc,
     p.tx_razao_social_osc,
-    p.tx_nome_osc,
+    p.tx_nome_fantasia_osc,
     p.cd_natureza_juridica_osc,
-    p.tx_nome_natureza_juridica_osc,
+    aa.tx_nome_natureza_juridica,
     p.dt_fundacao_osc,
-    "substring"(p.dt_fundacao_osc, 7, 10) AS ano_fundacao,
-    p.cd_municipio,
-    p.tx_nome_municipio,
-    p.cd_uf,
-    p.tx_sigla_uf,
-        CASE
-            WHEN p.tx_sigla_uf::text = ANY (ARRAY['RO'::character varying::text, 'AC'::character varying::text, 'AM'::character varying::text, 'RR'::character varying::text, 'PA'::character varying::text, 'AP'::character varying::text, 'TO'::character varying::text]) THEN 'Norte'::text
-            WHEN p.tx_sigla_uf::text = ANY (ARRAY['PR'::character varying::text, 'SC'::character varying::text, 'RS'::character varying::text]) THEN 'Sul'::text
-            WHEN p.tx_sigla_uf::text = ANY (ARRAY['MA'::character varying::text, 'PI'::character varying::text, 'CE'::character varying::text, 'RN'::character varying::text, 'PB'::character varying::text, 'PE'::character varying::text, 'AL'::character varying::text, 'SE'::character varying::text, 'BA'::character varying::text]) THEN 'Nordeste'::text
-            WHEN p.tx_sigla_uf::text = ANY (ARRAY['MS'::character varying::text, 'MT'::character varying::text, 'GO'::character varying::text, 'DF'::character varying::text]) THEN 'Centro-Oeste'::text
-            WHEN p.tx_sigla_uf::text = ANY (ARRAY['MG'::character varying::text, 'ES'::character varying::text, 'RJ'::character varying::text, 'SP'::character varying::text]) THEN 'Sudeste'::text
-            ELSE 'Nao informado'::text
-        END AS regiao,
-        CASE
-            WHEN p.tx_sigla_uf::text = ANY (ARRAY['RO'::character varying::text, 'AC'::character varying::text, 'AM'::character varying::text, 'RR'::character varying::text, 'PA'::character varying::text, 'AP'::character varying::text, 'TO'::character varying::text]) THEN 1
-            WHEN p.tx_sigla_uf::text = ANY (ARRAY['PR'::character varying::text, 'SC'::character varying::text, 'RS'::character varying::text]) THEN 4
-            WHEN p.tx_sigla_uf::text = ANY (ARRAY['MA'::character varying::text, 'PI'::character varying::text, 'CE'::character varying::text, 'RN'::character varying::text, 'PB'::character varying::text, 'PE'::character varying::text, 'AL'::character varying::text, 'SE'::character varying::text, 'BA'::character varying::text]) THEN 2
-            WHEN p.tx_sigla_uf::text = ANY (ARRAY['MS'::character varying::text, 'MT'::character varying::text, 'GO'::character varying::text, 'DF'::character varying::text]) THEN 5
-            WHEN p.tx_sigla_uf::text = ANY (ARRAY['MG'::character varying::text, 'ES'::character varying::text, 'RJ'::character varying::text, 'SP'::character varying::text]) THEN 3
-            ELSE 9
-        END AS cd_regiao,
-    p.cd_atividade_economica_osc,
-    p.tx_nome_atividade_economica_osc,
-    p.geo_lat,
-    p.geo_lng,
+	substring(p.dt_fundacao_osc::TEXT, 7, 10) AS ano_fundacao,
+    cc.edmu_nm_municipio,
+	cc.edmu_cd_municipio,
+    dd.eduf_sg_uf,
+    dd.eduf_cd_uf,
+    ee.edre_nm_regiao,
+    ee.edre_cd_regiao,
+    p.cd_classe_atividade_economica_osc,
+    ff.tx_nome_classe_atividade_economica,
     a.cd_area_atuacao,
     d.tx_nome_area_atuacao,
     a.cd_subarea_atuacao,
     y.tx_nome_subarea_atuacao,
     v.nr_trabalhadores_vinculo,
-        CASE
+    CASE
             WHEN v.nr_trabalhadores_vinculo = 0 THEN 'Sem pessoal ocupado'::text
             WHEN v.nr_trabalhadores_vinculo > 0 AND v.nr_trabalhadores_vinculo < 3 THEN '1-2'::text
             WHEN v.nr_trabalhadores_vinculo > 2 AND v.nr_trabalhadores_vinculo < 5 THEN '3-4'::text
@@ -66,9 +51,9 @@ AS
     ods.cd_meta_osc,
     q.id_projeto,
     q.tx_nome_projeto,
-    q.tx_nome_status_projeto,
+    gg.tx_nome_status_projeto,
     q.dt_data_inicio_projeto,
-    "substring"(q.dt_data_inicio_projeto, 7, 10) AS ano_parceria,
+    substring(q.dt_data_inicio_projeto::TEXT, 7, 10) AS ano_parceria,
         CASE
             WHEN q.dt_data_inicio_projeto IS NULL THEN 0
             ELSE 1
@@ -78,17 +63,18 @@ AS
     q.nr_valor_total_projeto,
     q.nr_valor_captado_projeto,
     q.tx_descricao_projeto,
-    q.tx_nome_abrangencia_projeto,
-    q.tx_nome_zona_atuacao,
+    hh.tx_nome_abrangencia_projeto,
+    ii.tx_nome_zona_atuacao,
     q.ft_identificador_projeto_externo,
     r.cd_certificado,
     s.tx_nome_certificado,
     t.tx_cargo_dirigente,
+    t.tx_nome_dirigente,
     c.tx_nome_conselho,
-        CASE
-            WHEN c.tx_nome_conselho IS NULL THEN 0
-            ELSE 1
-        END AS in_conselho,
+    	CASE
+    		WHEN c.tx_nome_conselho IS NULL THEN 0
+        	ELSE 1
+    	END AS in_conselho,
     e.tx_nome_tipo_participacao,
     m.dt_data_inicio_conselho,
     m.dt_data_fim_conselho,
@@ -100,10 +86,15 @@ AS
     z.cd_fonte_recursos_osc,
     z.dt_ano_recursos_osc,
     z.nr_valor_recursos_osc,
-    w.tx_nome_fonte_recursos_osc
-   FROM portal.vw_osc_dados_gerais p
-     LEFT JOIN osc.tb_localizacao ON p.id_osc = tb_localizacao.id_osc
-     LEFT JOIN portal.vw_osc_projeto q ON p.id_osc = q.id_osc
+    w.tx_nome_fonte_recursos_osc,
+    st_y(bb.geo_localizacao) AS lat,
+    st_x(bb.geo_localizacao) AS lon,
+    jj.tx_codigo_meta_projeto,
+    kk.tx_codigo_objetivo_projeto
+   FROM osc.tb_osc
+     JOIN osc.tb_dados_gerais p ON p.id_osc = tb_osc.id_osc
+     LEFT JOIN osc.tb_localizacao bb ON p.id_osc = bb.id_osc
+     LEFT JOIN osc.tb_projeto q ON p.id_osc = q.id_osc
      LEFT JOIN osc.tb_relacoes_trabalho v ON p.id_osc = v.id_osc
      LEFT JOIN osc.tb_certificado r ON p.id_osc = r.id_osc
      LEFT JOIN syst.dc_certificado s ON r.cd_certificado = s.cd_certificado
@@ -121,6 +112,15 @@ AS
      LEFT JOIN syst.dc_conferencia h ON l.cd_conferencia = h.cd_conferencia
      LEFT JOIN syst.dc_forma_participacao_conferencia j ON l.cd_forma_participacao_conferencia = j.cd_forma_participacao_conferencia
      LEFT JOIN osc.tb_participacao_social_outra k ON p.id_osc = k.id_osc
+     LEFT JOIN syst.dc_natureza_juridica aa ON p.cd_natureza_juridica_osc = aa.cd_natureza_juridica
+     LEFT JOIN spat.ed_municipio cc ON cc.edmu_cd_municipio = bb.cd_municipio
+     LEFT JOIN spat.ed_uf dd ON dd.eduf_cd_uf = cc.eduf_cd_uf::numeric
+     LEFT JOIN spat.ed_regiao ee ON dd.edre_cd_regiao = ee.edre_cd_regiao::numeric
+     LEFT JOIN syst.dc_classe_atividade_economica ff ON ff.cd_classe_atividade_economica::text = p.cd_classe_atividade_economica_osc::text
+     LEFT JOIN syst.dc_status_projeto gg ON gg.cd_status_projeto = q.cd_status_projeto
+     LEFT JOIN syst.dc_abrangencia_projeto hh ON hh.cd_abrangencia_projeto = q.cd_abrangencia_projeto
+     LEFT JOIN syst.dc_zona_atuacao_projeto ii ON ii.cd_zona_atuacao_projeto = q.cd_zona_atuacao_projeto
      LEFT JOIN osc.tb_objetivo_osc ods ON p.id_osc = ods.id_osc
-  WHERE p.id_osc <> 789809 AND p.id_osc <> 987654
-  
+     LEFT JOIN syst.dc_meta_projeto jj ON ods.cd_meta_osc = jj.cd_meta_projeto
+     LEFT JOIN syst.dc_objetivo_projeto kk ON jj.cd_objetivo_projeto = kk.cd_objetivo_projeto
+  WHERE p.id_osc <> 789809 AND p.id_osc <> 987654 AND tb_osc.bo_osc_ativa;
