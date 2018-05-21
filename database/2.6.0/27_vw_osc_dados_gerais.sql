94727e1b99ea30565ca176999feb539c095d39ef
tmnascimento <tmnascimento@gmail.com>
2018-05-21 14:48:31 -0300

    Update 27_vw_osc_dados_gerais.sql
    CREATE MATERIALIZED VIEW portal.vw_log_alteracao

--- database/2.6.0/27_vw_osc_dados_gerais.sql
+++ database/2.6.0/27_vw_osc_dados_gerais.sql
@@ -92,4 +92,35 @@ BEGIN
   REFRESH MATERIALIZED VIEW CONCURRENTLY portal.vw_osc_recursos_projeto;
   RETURN NULL;
 END;
+
+-- object: portal.vw_log_alteracao | type: MATERIALIZED VIEW --
+DROP MATERIALIZED VIEW IF EXISTS portal.vw_log_alteracao CASCADE;
+CREATE MATERIALIZED VIEW portal.vw_log_alteracao
+AS
+
+SELECT distinct y.dt_alteracao
+     , y.id_osc
+     , vw_osc_dados_gerais.tx_nome_osc
+FROM log.tb_log_alteracao y
+JOIN
+    ( SELECT id_osc
+           , MAX(dt_alteracao) AS currentTS
+      FROM log.tb_log_alteracao
+      WHERE id_osc <> 789809
+      GROUP BY id_osc
+    ) AS grp
+  ON grp.id_osc = y.id_osc
+    AND grp.currentTS = y.dt_alteracao
+JOIN portal.vw_osc_dados_gerais ON y.id_osc = vw_osc_dados_gerais.id_osc
+limit 10;
+
+-- ddl-end --
+ALTER MATERIALIZED VIEW portal.vw_log_alteracao OWNER TO postgres;
+-- ddl-end --
+
+CREATE UNIQUE INDEX ix_vw_log_alteracao
+    ON portal.vw_log_alteracao USING btree
+    (id_osc, dt_alteracao ASC NULLS LAST)
+TABLESPACE pg_default;
+
 $$ LANGUAGE plpgsql;
