cdb9998f8c3416bdfe67f74d567fa50f59f18653
vagnerpraia <vagnerpraia@gmail.com>
2018-05-24 04:13:21 -0300

    Issue: MOSC-1499 Subject: Criação da view para os dados de gráficos Detail: Atualização da função para o gráfico de empregos formais por região, adicionando as fontes

--- database/2.6.0/20_obter_grafico_osc_natureza_juridica_regiao.sql
+++ database/2.6.0/20_obter_grafico_osc_natureza_juridica_regiao.sql
@@ -3,7 +3,8 @@ DROP FUNCTION IF EXISTS portal.obter_grafico_osc_natureza_juridica_regiao() CASC
 CREATE OR REPLACE FUNCTION portal.obter_grafico_osc_natureza_juridica_regiao() RETURNS TABLE (
 	titulo TEXT, 
 	tipo TEXT, 
-	dados JSON
+	dados JSON, 
+	fontes TEXT[]	
 ) AS $$ 
 
 BEGIN 
@@ -11,16 +12,19 @@ BEGIN
 		SELECT 
 			'Número de OSCs por natureza jurídica e região, Brasil'::TEXT as titulo, 
 			'barras'::TEXT as tipo, 
-			array_to_json(array_agg(b)) AS dados 
+			b.dados::JSON AS dados, 
+			b.fontes AS fontes 
 		FROM (
 			SELECT 
-				rotulo_1 AS rotulo,
-				array_to_json(array_agg(('{"' || rotulo_2 || '": ' || valor || '}')::JSON)) AS valor 
+				a.regiao AS rotulo, 
+				ARRAY_TO_JSON(ARRAY_AGG('{' || a.natureza_juridica::TEXT || ', ' || a.valor::TEXT || '}')) AS dados, 
+				(SELECT ARRAY(SELECT DISTINCT UNNEST(('{' || BTRIM(REPLACE(REPLACE(RTRIM(LTRIM(TRANSLATE(ARRAY_AGG(a.fontes::TEXT)::TEXT, '"\', ''), '{'), '}'), '},{', ','), ',,', ','), ',') || '}')::TEXT[]))) AS fontes
 			FROM (
 				SELECT 
-					ed_regiao.edre_nm_regiao AS rotulo_1, 
-					dc_natureza_juridica.tx_nome_natureza_juridica AS rotulo_2, 
-					count(*) AS valor 
+					ed_regiao.edre_nm_regiao AS regiao, 
+					dc_natureza_juridica.tx_nome_natureza_juridica AS natureza_juridica, 
+					count(*) AS valor, 
+					ARRAY_AGG(DISTINCT(COALESCE(tb_dados_gerais.ft_classe_atividade_economica_osc, '') || ',' || COALESCE(tb_localizacao.ft_municipio, ''))) AS fontes 
 				FROM osc.tb_dados_gerais 
 				INNER JOIN osc.tb_localizacao 
 				ON tb_dados_gerais.id_osc = tb_localizacao.id_osc 
@@ -31,8 +35,10 @@ BEGIN
 				GROUP BY ed_regiao.edre_nm_regiao, dc_natureza_juridica.tx_nome_natureza_juridica 
 				ORDER BY ed_regiao.edre_nm_regiao, dc_natureza_juridica.tx_nome_natureza_juridica
 			) AS a 
-			GROUP BY rotulo
+			GROUP BY regiao
 		) AS b;
 END;
 
 $$ LANGUAGE 'plpgsql';
+
+SELECT * FROM portal.obter_grafico_osc_natureza_juridica_regiao();
--- database/2.6.0/67_obter_grafico_empregos_formais_oscs_regiao.sql
+++ database/2.6.0/67_obter_grafico_empregos_formais_oscs_regiao.sql
@@ -12,31 +12,29 @@ BEGIN
 		SELECT 
 			'Número de empregos formais nas OSCs por região'::TEXT AS titulo, 
 			'barras'::TEXT AS tipo, 
-			p.dados::JSONB AS dados, 
-			p.fontes AS fontes
+			b.dados::JSONB AS dados, 
+			b.fontes AS fontes 
 		FROM (
 			SELECT 
-				ARRAY_TO_JSON(ARRAY_AGG('{' || a.valor::TEXT || ', ' || a.rotulo::TEXT || '}')) AS dados, 
-				(SELECT ARRAY(SELECT DISTINCT UNNEST((
-					'{' || REPLACE(REPLACE(TRANSLATE(ARRAY_AGG(a.fontes::TEXT)::TEXT, '{\}', ''), '""', '"'), 'NULL"', 'NULL') || '}'
-				)::TEXT[]))) AS fontes
+				ARRAY_TO_JSON(ARRAY_AGG('{' || a.trabalhadores::TEXT || ', ' || a.regiao::TEXT || '}')) AS dados, 
+				(SELECT ARRAY(SELECT DISTINCT UNNEST(('{' || BTRIM(REPLACE(REPLACE(RTRIM(LTRIM(TRANSLATE(ARRAY_AGG(a.fontes::TEXT)::TEXT, '"\', ''), '{'), '}'), '},{', ','), ',,', ','), ',') || '}')::TEXT[]))) AS fontes
 			FROM (
 				SELECT 
-					SUM(tb_relacoes_trabalho.nr_trabalhadores_vinculo) AS valor, 
+					SUM(tb_relacoes_trabalho.nr_trabalhadores_vinculo) AS trabalhadores, 
 					(
 						SELECT edre_nm_regiao 
 						FROM spat.ed_regiao 
 						WHERE edre_cd_regiao = (SELECT SUBSTR(tb_localizacao.cd_municipio::TEXT, 1, 1))::NUMERIC(1, 0)
-					) AS rotulo,
-					ARRAY_AGG(DISTINCT(tb_relacoes_trabalho.ft_trabalhadores_vinculo)) AS fontes
+					) AS regiao,
+					ARRAY_AGG(DISTINCT(COALESCE(tb_relacoes_trabalho.ft_trabalhadores_vinculo, '') || ',' || COALESCE(tb_localizacao.ft_municipio, ''))) AS fontes
 				FROM osc.tb_dados_gerais 
 				INNER JOIN osc.tb_relacoes_trabalho 
 				ON tb_dados_gerais.id_osc = tb_relacoes_trabalho.id_osc 
 				INNER JOIN osc.tb_localizacao 
 				ON tb_dados_gerais.id_osc = tb_localizacao.id_osc 
-				GROUP BY rotulo
+				GROUP BY regiao
 			) AS a
-		) AS p;
+		) AS b;
 END;
 
 $$ LANGUAGE 'plpgsql';
