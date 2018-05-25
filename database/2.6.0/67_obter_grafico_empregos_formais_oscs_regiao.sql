ca313fe9cea9614e05f52194b0236d7118e115a6
vagnerpraia <vagnerpraia@gmail.com>
2018-05-25 19:01:49 -0300

    Issue: MOSC-1499 Subject: Criação da view para os dados de gráficos Detail: Atualização do gráfico Número de empregos formais nas OSCs por região

--- database/2.6.0/67_obter_grafico_empregos_formais_oscs_regiao.sql
+++ database/2.6.0/67_obter_grafico_empregos_formais_oscs_regiao.sql
@@ -16,22 +16,20 @@ BEGIN
 			b.fontes AS fontes 
 		FROM (
 			SELECT 
-				'{' || RTRIM(LTRIM(REPLACE(REPLACE(TRANSLATE(ARRAY_AGG('{"' || a.regiao::TEXT || '": ' || a.trabalhadores::TEXT || '}')::TEXT, '\', ''), '"{', '{'), '}"', '}'), '{'), '}') || '}' AS dados, 
+				('{' || RTRIM(LTRIM(REPLACE(REPLACE(REPLACE(REPLACE((TRANSLATE(ARRAY_AGG('"' || a.regiao::TEXT || '": ' || a.quantidade::TEXT)::TEXT, '\', '') || '}'), '""', '"'), '",', ','), '"}', '}'), '"{', '{'), '{'), '}') || '}')::JSONB AS dados, 
 				(SELECT ARRAY(SELECT DISTINCT UNNEST(('{' || BTRIM(REPLACE(REPLACE(RTRIM(LTRIM(TRANSLATE(ARRAY_AGG(a.fontes::TEXT)::TEXT, '"\', ''), '{'), '}'), '},{', ','), ',,', ','), ',') || '}')::TEXT[]))) AS fontes
 			FROM (
 				SELECT 
-					SUM(tb_relacoes_trabalho.nr_trabalhadores_vinculo) AS trabalhadores, 
-					(
-						SELECT edre_nm_regiao 
-						FROM spat.ed_regiao 
-						WHERE edre_cd_regiao = (SELECT SUBSTR(tb_localizacao.cd_municipio::TEXT, 1, 1))::NUMERIC(1, 0)
-					) AS regiao,
+					ed_regiao.edre_nm_regiao AS regiao,
+					SUM(tb_relacoes_trabalho.nr_trabalhadores_vinculo) AS quantidade, 
 					ARRAY_AGG(DISTINCT(COALESCE(tb_relacoes_trabalho.ft_trabalhadores_vinculo, '') || ',' || COALESCE(tb_localizacao.ft_municipio, ''))) AS fontes
 				FROM osc.tb_dados_gerais 
 				INNER JOIN osc.tb_relacoes_trabalho 
 				ON tb_dados_gerais.id_osc = tb_relacoes_trabalho.id_osc 
 				INNER JOIN osc.tb_localizacao 
 				ON tb_dados_gerais.id_osc = tb_localizacao.id_osc 
+				INNER JOIN spat.ed_regiao 
+				ON (SELECT SUBSTR(tb_localizacao.cd_municipio::TEXT, 1, 1))::NUMERIC(1, 0) = ed_regiao.edre_cd_regiao 
 				GROUP BY regiao
 			) AS a
 		) AS b;
