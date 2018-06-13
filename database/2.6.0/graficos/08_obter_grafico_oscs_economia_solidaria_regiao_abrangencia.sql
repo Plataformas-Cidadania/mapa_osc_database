d30c3cee1d85994e8febafc12baa10f97da9372b
vagnerpraia <vagnerpraia@gmail.com>
2018-06-13 18:42:43 -0300

    Issue: MOSC-1656 Subject: Criação de função para o gráfico "Distribuição de OSCs de economia solidária por região e tipo de vínculo com outras entidades" Detail: Criação de funções.

---
+++ database/2.6.0/graficos/08_obter_grafico_oscs_economia_solidaria_regiao_abrangencia.sql
@@ -0,0 +1,58 @@
+DROP FUNCTION IF EXISTS portal.obter_grafico_oscs_economia_solidaria_regiao_abrangencia() CASCADE;
+
+CREATE OR REPLACE FUNCTION portal.obter_grafico_oscs_economia_solidaria_regiao_abrangencia() RETURNS TABLE (
+	dados JSONB, 
+	fontes TEXT[]	
+) AS $$ 
+
+BEGIN 
+	RETURN QUERY 
+		SELECT 
+			('[{' || RTRIM(LTRIM(REPLACE(REPLACE(REPLACE(REPLACE((TRANSLATE(ARRAY_AGG(b.dados)::TEXT, '\', '') || '}'), '""', '"'), '}",', '},'), '"}', '}'), '"{', '{'), '{'), '}') || '}]')::JSONB AS dados, 
+			(
+				SELECT ARRAY_AGG(TRANSLATE(a::TEXT, '()\"', '')) FROM (SELECT DISTINCT UNNEST(
+					TRANSLATE(ARRAY_AGG(REPLACE(TRIM(TRANSLATE(b.fontes::TEXT, '"\{}', ''), ','), '","', ','))::TEXT, '"', '')::TEXT[]
+				)) AS a
+			) AS fontes 
+		FROM (
+			SELECT 
+				'{"rotulo": "' || a.rotulo_1 || '", "valor": ' || '[{' || RTRIM(LTRIM(REPLACE(REPLACE(REPLACE(REPLACE((TRANSLATE(ARRAY_AGG('{"rotulo": "' || a.rotulo_2::TEXT || '", "valor": ' || a.valor::TEXT || '}')::TEXT, '\', '') || '}'), '""', '"'), '}",', '},'), '"}', '}'), '"{', '{'), '{'), '}') || '}]}' AS dados, 
+				(
+					SELECT ARRAY_AGG(TRANSLATE(a::TEXT, '()\"', '')) FROM (SELECT DISTINCT UNNEST(
+						TRANSLATE(ARRAY_AGG(REPLACE(REPLACE(TRIM(TRANSLATE(a.fontes::TEXT, '"\{}', ''), ','), '","', ','), ',,', ','))::TEXT, '"', '')::TEXT[]
+					)) AS a
+				) AS fontes 
+			FROM (
+				SELECT 
+					COALESCE(ed_regiao.edre_nm_regiao, 'Sem informação') AS rotulo_1, 
+					COALESCE(tb_senaes.abr_13, 'Sem informação') AS rotulo_2, 
+					COUNT(*) AS valor, 
+					(
+						SELECT ARRAY_AGG(TRANSLATE(a::TEXT, '()', '')) FROM (SELECT DISTINCT UNNEST(
+							ARRAY_CAT(
+								ARRAY_CAT(
+									'{"MTE/SENAES"}'::TEXT[], 
+									ARRAY_AGG(DISTINCT REPLACE(COALESCE(tb_localizacao.ft_municipio, ''), '${ETL}', ''))
+								),
+								ARRAY_AGG(DISTINCT REPLACE(COALESCE(tb_osc.ft_identificador_osc, ''), '${ETL}', ''))
+							)
+						)) AS a
+					) AS fontes 
+				FROM osc.tb_osc 
+				INNER JOIN graph.tb_senaes 
+				ON tb_osc.cd_identificador_osc = TRANSLATE(tb_senaes.cnpj_8, '-', '')::NUMERIC 
+				LEFT JOIN osc.tb_localizacao 
+				ON tb_osc.id_osc = tb_localizacao.id_osc 
+				LEFT JOIN spat.ed_regiao 
+				ON ed_regiao.edre_cd_regiao::TEXT = SUBSTR(tb_localizacao.cd_municipio::TEXT, 1, 1) 
+				GROUP BY rotulo_1, rotulo_2 
+				ORDER BY rotulo_1, rotulo_2 
+			) AS a 
+			GROUP BY a.rotulo_1
+		) AS b;
+END;
+
+$$ LANGUAGE 'plpgsql';
+
+SELECT * FROM portal.obter_grafico_oscs_economia_solidaria_regiao_abrangencia();
+
