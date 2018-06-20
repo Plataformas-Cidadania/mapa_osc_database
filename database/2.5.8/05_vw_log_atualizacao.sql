cbdcb5c79df0429e21acbf14f65cac1456263fae
EricFerreiraS <eric_santos6@hotmail.com>
2018-06-20 17:10:38 -0300

    atualizacao de funcao

--- database/2.5.8/05_vw_log_atualizacao.sql
+++ database/2.5.8/05_vw_log_atualizacao.sql
@@ -1,25 +1,25 @@
 -- object: portal.vw_log_alteracao | type: MATERIALIZED VIEW --
+CREATE INDEX ix_tb_log_alteracao_2
+  ON log.tb_log_alteracao
+  USING btree
+  (id_carga);
+
 DROP MATERIALIZED VIEW IF EXISTS portal.vw_log_alteracao CASCADE;
 CREATE MATERIALIZED VIEW portal.vw_log_alteracao
 AS
-
 SELECT DISTINCT y.dt_alteracao,
     y.id_osc,
-    COALESCE(NULLIF(btrim(osc.tb_dados_gerais.tx_nome_fantasia_osc), ''::text), osc.tb_dados_gerais.tx_razao_social_osc) AS tx_nome_osc
+    COALESCE(NULLIF(btrim(tb_dados_gerais.tx_nome_fantasia_osc), ''::text), tb_dados_gerais.tx_razao_social_osc) AS tx_nome_osc
    FROM log.tb_log_alteracao y
      JOIN ( SELECT tb_log_alteracao.id_osc,
             max(tb_log_alteracao.dt_alteracao) AS currentts
            FROM log.tb_log_alteracao
           WHERE tb_log_alteracao.id_osc <> 789809
           GROUP BY tb_log_alteracao.id_osc) grp ON grp.id_osc = y.id_osc AND grp.currentts = y.dt_alteracao
-     JOIN osc.tb_dados_gerais ON y.id_osc = osc.tb_dados_gerais.id_osc
-   where y.tx_fonte_dados not like 'SALICWEB%'
-order by y.dt_alteracao DESC
+     JOIN osc.tb_dados_gerais ON y.id_osc = tb_dados_gerais.id_osc
+  WHERE y.id_carga is null
+  ORDER BY y.dt_alteracao DESC
  LIMIT 10;
-
--- ddl-end --
-ALTER MATERIALIZED VIEW portal.vw_log_alteracao OWNER TO postgres;
--- ddl-end --
 
 CREATE UNIQUE INDEX ix_vw_log_alteracao
     ON portal.vw_log_alteracao USING btree
