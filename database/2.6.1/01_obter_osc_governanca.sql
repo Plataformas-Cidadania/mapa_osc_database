ed3331187df6071f2687f7a95e0f1919b54ea9b9
vagnerpraia <vagnerpraia@gmail.com>
2018-06-29 05:49:56 -0300

    Acerto de função

---
+++ database/2.6.1/01_obter_osc_governanca.sql
@@ -0,0 +1,54 @@
+DROP FUNCTION IF EXISTS portal.obter_osc_governanca(TEXT);
+
+CREATE OR REPLACE FUNCTION portal.obter_osc_governanca(param TEXT) RETURNS TABLE (
+	resultado JSONB,
+	mensagem TEXT,
+	flag BOOLEAN
+) AS $$ 
+
+DECLARE
+	linha RECORD;
+	osc RECORD;
+
+BEGIN 
+	resultado := null;
+
+	SELECT INTO osc * FROM osc.tb_osc WHERE (id_osc = param::INTEGER OR tx_apelido_osc = param) AND bo_osc_ativa;
+	
+	IF osc.id_osc IS NOT null THEN 		
+		SELECT INTO linha
+			array_agg(a) AS array 
+		FROM (
+			SELECT 
+				id_dirigente,
+				tx_cargo_dirigente,
+				ft_cargo_dirigente,
+				tx_nome_dirigente,
+				ft_nome_dirigente
+			FROM 
+				osc.tb_governanca
+			WHERE 
+				id_osc = param::INTEGER
+		) a;
+
+		IF ARRAY_LENGTH(linha.array, 1) > 0 THEN 
+			resultado := to_jsonb(linha.array);
+		END IF;
+
+		flag := true;
+		mensagem := 'Governança retornada.';
+		
+	ELSE 
+		flag := false;
+		mensagem := 'OSC não encontrada.';
+	END IF;
+
+	RETURN NEXT;
+	
+EXCEPTION
+	WHEN others THEN 
+		flag := false;
+		SELECT INTO mensagem a.mensagem FROM portal.verificar_erro(SQLSTATE, SQLERRM, null, null, null, false, null) AS a;
+		RETURN NEXT;
+END;
+$$ LANGUAGE 'plpgsql';
