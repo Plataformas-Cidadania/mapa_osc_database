e43b1df36d9fc1d81f6361f5a2d402bf610fb065
Vagner Praia da Silva <vagnerpraia@gmail.com>
2018-07-21 21:21:45 -0300

    Refatoração do editar participação social

--- database/2.6.1/10_atualizar_participacao_social_conselho.sql
+++ database/2.6.1/10_atualizar_participacao_social_conselho.sql
@@ -14,6 +14,8 @@ DECLARE
 	dado_nao_delete INTEGER[];
 	flag_update BOOLEAN;
 	osc RECORD;
+	conselho RECORD;
+	representante RECORD;
 	nao_possui BOOLEAN;
 	representantes JSONB;
 	record_representantes RECORD;
@@ -49,28 +51,22 @@ BEGIN
 		RAISE EXCEPTION 'permissao_negada_usuario';
 	END IF;
 	
-	FOR objeto IN (SELECT * FROM jsonb_populate_recordset(null::osc.tb_participacao_social_conselho, json))
+	FOR objeto IN (SELECT * FROM json_to_recordset(json) AS x(conselho jsonb, representante jsonb))
 	LOOP
-		IF objeto.cd_conselho = (SELECT cd_conselho FROM syst.dc_conselho WHERE tx_nome_conselho = 'Não Possui') THEN 
-			nao_possui := true;
-			json := jsonb_build_array(objeto::JSONB);
-		END IF;
-	END LOOP;
-	
-	FOR objeto IN (SELECT * FROM jsonb_populate_recordset(null::osc.tb_participacao_social_conselho, json))
-	LOOP
+		conselho = (SELECT * FROM jsonb_populate_record(null::osc.tb_participacao_social_conselho, objeto.conselho);
+		
 		dado_anterior := null;
 		
 		IF tipo_busca = 1 THEN
 			SELECT INTO dado_anterior *
 			FROM osc.tb_participacao_social_conselho
-			WHERE id_conselho = objeto.id_conselho;
+			WHERE id_conselho = conselho.id_conselho;
 			
 		ELSIF tipo_busca = 2 THEN
 			SELECT INTO dado_anterior * 
 			FROM osc.tb_participacao_social_conselho
 			INNER JOIN syst.dc_conselho
-			WHERE tb_participacao_social_conselho.cd_conselho = objeto.cd_conselho
+			WHERE tb_participacao_social_conselho.cd_conselho = conselho.cd_conselho
 			AND dc_conselho.tx_nome_conselho <> 'Outra'
 			AND tb_participacao_social_conselho.id_osc = osc.id_osc;
 			
@@ -81,7 +77,7 @@ BEGIN
 		
 		IF dado_anterior.id_conselho IS null THEN
 			INSERT INTO osc.tb_participacao_social_conselho (
-				id_osc,				
+				id_osc,
 				cd_conselho,
 				ft_conselho,
 				cd_tipo_participacao,
@@ -94,164 +90,154 @@ BEGIN
 				ft_data_fim_conselho
 			) VALUES (
 				osc.id_osc,
-				objeto.cd_conselho,
+				conselho.cd_conselho,
 				fonte_dados.nome_fonte,
-				objeto.cd_tipo_participacao,
+				conselho.cd_tipo_participacao,
 				fonte_dados.nome_fonte,
-				objeto.cd_periodicidade_reuniao_conselho,
+				conselho.cd_periodicidade_reuniao_conselho,
 				fonte_dados.nome_fonte,
-				objeto.dt_data_inicio_conselho,
+				conselho.dt_data_inicio_conselho,
 				fonte_dados.nome_fonte,
-				objeto.dt_data_fim_conselho,
+				conselho.dt_data_fim_conselho,
 				fonte_dados.nome_fonte
 			) RETURNING * INTO dado_posterior;
 			
 			dado_nao_delete := array_append(dado_nao_delete, dado_posterior.id_conselho);
 			
-			PERFORM portal.inserir_log_atualizacao(nome_tabela, osc, fonte, data_atualizacao, null, row_to_json(dado_posterior),id_carga);
+			PERFORM portal.inserir_log_atualizacao(nome_tabela, osc, fonte, data_atualizacao, null, row_to_json(dado_posterior), id_carga);
 			
 		ELSE
 			dado_posterior := dado_anterior;
 			dado_nao_delete := array_append(dado_nao_delete, dado_posterior.id_projeto);
 			flag_update := false;
 			
-			IF (SELECT a.flag FROM portal.verificar_dado(dado_anterior.tx_identificador_projeto_externo::TEXT, dado_anterior.ft_identificador_projeto_externo, objeto.tx_identificador_projeto_externo::TEXT, fonte_dados.prioridade, null_valido) AS a) THEN
-				dado_posterior.tx_identificador_projeto_externo := objeto.tx_identificador_projeto_externo;
-				dado_posterior.ft_identificador_projeto_externo := fonte_dados.nome_fonte;
+			IF (SELECT a.flag FROM portal.verificar_dado(dado_anterior.cd_conselho::TEXT, dado_anterior.ft_conselho, conselho.cd_conselho::TEXT, fonte_dados.prioridade, null_valido) AS a) THEN
+				dado_posterior.cd_conselho := conselho.cd_conselho;
+				dado_posterior.ft_conselho := fonte_dados.nome_fonte;
 				flag_update := true;
 			END IF;
 			
-			IF (SELECT a.flag FROM portal.verificar_dado(dado_anterior.cd_municipio::TEXT, dado_anterior.ft_municipio, objeto.cd_municipio::TEXT, fonte_dados.prioridade, null_valido) AS a) THEN
-				dado_posterior.cd_municipio := objeto.cd_municipio;
-				dado_posterior.ft_municipio := fonte_dados.nome_fonte;
+			IF (SELECT a.flag FROM portal.verificar_dado(dado_anterior.cd_tipo_participacao::TEXT, dado_anterior.ft_tipo_participacao, conselho.cd_tipo_participacao::TEXT, fonte_dados.prioridade, null_valido) AS a) THEN
+				dado_posterior.cd_tipo_participacao := conselho.cd_tipo_participacao;
+				dado_posterior.ft_tipo_participacao := fonte_dados.nome_fonte;
 				flag_update := true;
 			END IF;
 			
-			IF (SELECT a.flag FROM portal.verificar_dado(dado_anterior.cd_uf::TEXT, dado_anterior.ft_uf, objeto.cd_uf::TEXT, fonte_dados.prioridade, null_valido) AS a) THEN
-				dado_posterior.cd_uf := objeto.cd_uf;
-				dado_posterior.ft_uf := fonte_dados.nome_fonte;
+			IF (SELECT a.flag FROM portal.verificar_dado(dado_anterior.cd_periodicidade_reuniao_conselho::TEXT, dado_anterior.ft_periodicidade_reuniao, objeto.cd_periodicidade_reuniao_conselho::TEXT, fonte_dados.prioridade, null_valido) AS a) THEN
+				dado_posterior.cd_periodicidade_reuniao_conselho := objeto.cd_periodicidade_reuniao_conselho;
+				dado_posterior.ft_periodicidade_reuniao := fonte_dados.nome_fonte;
 				flag_update := true;
 			END IF;
 			
-			IF (SELECT a.flag FROM portal.verificar_dado(dado_anterior.tx_nome_projeto::TEXT, dado_anterior.ft_nome_projeto, objeto.tx_nome_projeto::TEXT, fonte_dados.prioridade, null_valido) AS a) THEN
-				dado_posterior.tx_nome_projeto := objeto.tx_nome_projeto;
-				dado_posterior.ft_nome_projeto := fonte_dados.nome_fonte;
+			IF (SELECT a.flag FROM portal.verificar_dado(dado_anterior.dt_data_inicio_conselho::TEXT, dado_anterior.ft_data_inicio_conselho, conselho.dt_data_inicio_conselho::TEXT, fonte_dados.prioridade, null_valido) AS a) THEN
+				dado_posterior.dt_data_inicio_conselho := conselho.dt_data_inicio_conselho;
+				dado_posterior.ft_data_inicio_conselho := fonte_dados.nome_fonte;
 				flag_update := true;
 			END IF;
 			
-			IF (SELECT a.flag FROM portal.verificar_dado(dado_anterior.cd_status_projeto::TEXT, dado_anterior.ft_status_projeto, objeto.cd_status_projeto::TEXT, fonte_dados.prioridade, null_valido) AS a) THEN
-				dado_posterior.cd_status_projeto := objeto.cd_status_projeto;
-				dado_posterior.ft_status_projeto := fonte_dados.nome_fonte;
-				flag_update := true;
-			END IF;
-			
-			IF (SELECT a.flag FROM portal.verificar_dado(dado_anterior.dt_data_inicio_projeto::TEXT, dado_anterior.ft_data_inicio_projeto, objeto.dt_data_inicio_projeto::TEXT, fonte_dados.prioridade, null_valido) AS a) THEN
-				dado_posterior.dt_data_inicio_projeto := objeto.dt_data_inicio_projeto;
-				dado_posterior.ft_data_inicio_projeto := fonte_dados.nome_fonte;
-				flag_update := true;
-			END IF;
-			
-			IF (SELECT a.flag FROM portal.verificar_dado(dado_anterior.dt_data_fim_projeto::TEXT, dado_anterior.ft_data_fim_projeto, objeto.dt_data_fim_projeto::TEXT, fonte_dados.prioridade, null_valido) AS a) THEN
-				dado_posterior.dt_data_fim_projeto := objeto.dt_data_fim_projeto;
-				dado_posterior.ft_data_fim_projeto := fonte_dados.nome_fonte;
-				flag_update := true;
-			END IF;
-			
-			IF (SELECT a.flag FROM portal.verificar_dado(dado_anterior.nr_valor_total_projeto::TEXT, dado_anterior.ft_valor_total_projeto, objeto.nr_valor_total_projeto::TEXT, fonte_dados.prioridade, null_valido) AS a) THEN
-				dado_posterior.nr_valor_total_projeto := objeto.nr_valor_total_projeto;
-				dado_posterior.ft_valor_total_projeto := fonte_dados.nome_fonte;
-				flag_update := true;
-			END IF;
-			
-			IF (SELECT a.flag FROM portal.verificar_dado(dado_anterior.nr_valor_captado_projeto::TEXT, dado_anterior.ft_valor_captado_projeto, objeto.nr_valor_captado_projeto::TEXT, fonte_dados.prioridade, null_valido) AS a) THEN
-				dado_posterior.nr_valor_captado_projeto := objeto.nr_valor_captado_projeto;
-				dado_posterior.ft_valor_captado_projeto := fonte_dados.nome_fonte;
-				flag_update := true;
-			END IF;
-			
-			IF (SELECT a.flag FROM portal.verificar_dado(dado_anterior.nr_total_beneficiarios::TEXT, dado_anterior.ft_total_beneficiarios, objeto.nr_total_beneficiarios::TEXT, fonte_dados.prioridade, null_valido) AS a) THEN
-				dado_posterior.nr_total_beneficiarios := objeto.nr_total_beneficiarios;
-				dado_posterior.ft_total_beneficiarios := fonte_dados.nome_fonte;
-				flag_update := true;
-			END IF;
-			
-			IF (SELECT a.flag FROM portal.verificar_dado(dado_anterior.cd_abrangencia_projeto::TEXT, dado_anterior.ft_abrangencia_projeto, objeto.cd_abrangencia_projeto::TEXT, fonte_dados.prioridade, null_valido) AS a) THEN
-				dado_posterior.cd_abrangencia_projeto := objeto.cd_abrangencia_projeto;
-				dado_posterior.ft_abrangencia_projeto := fonte_dados.nome_fonte;
-				flag_update := true;
-			END IF;
-			
-			IF (SELECT a.flag FROM portal.verificar_dado(dado_anterior.cd_zona_atuacao_projeto::TEXT, dado_anterior.ft_zona_atuacao_projeto, objeto.cd_zona_atuacao_projeto::TEXT, fonte_dados.prioridade, null_valido) AS a) THEN
-				dado_posterior.cd_zona_atuacao_projeto := objeto.cd_zona_atuacao_projeto;
-				dado_posterior.ft_zona_atuacao_projeto := fonte_dados.nome_fonte;
-				flag_update := true;
-			END IF;
-			
-			IF (SELECT a.flag FROM portal.verificar_dado(dado_anterior.tx_descricao_projeto::TEXT, dado_anterior.ft_descricao_projeto, objeto.tx_descricao_projeto::TEXT, fonte_dados.prioridade, null_valido) AS a) THEN
-				dado_posterior.tx_descricao_projeto := objeto.tx_descricao_projeto;
-				dado_posterior.ft_descricao_projeto := fonte_dados.nome_fonte;
-				flag_update := true;
-			END IF;
-			
-			IF (SELECT a.flag FROM portal.verificar_dado(dado_anterior.tx_metodologia_monitoramento::TEXT, dado_anterior.ft_metodologia_monitoramento, objeto.tx_metodologia_monitoramento::TEXT, fonte_dados.prioridade, null_valido) AS a) THEN
-				dado_posterior.tx_metodologia_monitoramento := objeto.tx_metodologia_monitoramento;
-				dado_posterior.ft_metodologia_monitoramento := fonte_dados.nome_fonte;
-				flag_update := true;
-			END IF;
-			
-			IF (SELECT a.flag FROM portal.verificar_dado(dado_anterior.tx_link_projeto::TEXT, dado_anterior.ft_link_projeto, objeto.tx_link_projeto::TEXT, fonte_dados.prioridade, null_valido) AS a) THEN
-				dado_posterior.tx_link_projeto := objeto.tx_link_projeto;
-				dado_posterior.ft_link_projeto := fonte_dados.nome_fonte;
+			IF (SELECT a.flag FROM portal.verificar_dado(dado_anterior.dt_data_fim_conselho::TEXT, dado_anterior.ft_data_fim_conselho, conselho.dt_data_fim_conselho::TEXT, fonte_dados.prioridade, null_valido) AS a) THEN
+				dado_posterior.dt_data_fim_conselho := conselho.dt_data_fim_conselho;
+				dado_posterior.ft_data_fim_conselho := fonte_dados.nome_fonte;
 				flag_update := true;
 			END IF;
 			
 			IF flag_update THEN
 				UPDATE osc.tb_participacao_social_conselho
-				SET	tx_identificador_projeto_externo = dado_posterior.tx_identificador_projeto_externo,
-					ft_identificador_projeto_externo = dado_posterior.ft_identificador_projeto_externo,
-					cd_uf = dado_posterior.cd_uf,
-					ft_uf = dado_posterior.ft_uf,
-					cd_municipio = dado_posterior.cd_municipio,
-					ft_municipio = dado_posterior.ft_municipio,
-					tx_nome_projeto = dado_posterior.tx_nome_projeto,
-					ft_nome_projeto = dado_posterior.ft_nome_projeto,
-					cd_status_projeto = dado_posterior.cd_status_projeto,
-					ft_status_projeto = dado_posterior.ft_status_projeto,
-					dt_data_inicio_projeto = dado_posterior.dt_data_inicio_projeto,
-					ft_data_inicio_projeto = dado_posterior.ft_data_inicio_projeto,
-					dt_data_fim_projeto = dado_posterior.dt_data_fim_projeto,
-					ft_data_fim_projeto = dado_posterior.ft_data_fim_projeto,
-					nr_valor_total_projeto = dado_posterior.nr_valor_total_projeto,
-					ft_valor_total_projeto = dado_posterior.ft_valor_total_projeto,
-					nr_valor_captado_projeto = dado_posterior.nr_valor_captado_projeto,
-					ft_valor_captado_projeto = dado_posterior.ft_valor_captado_projeto,
-					nr_total_beneficiarios = dado_posterior.nr_total_beneficiarios,
-					ft_total_beneficiarios = dado_posterior.ft_total_beneficiarios,
-					cd_abrangencia_projeto = dado_posterior.cd_abrangencia_projeto,
-					ft_abrangencia_projeto = dado_posterior.ft_abrangencia_projeto,
-					cd_zona_atuacao_projeto = dado_posterior.cd_zona_atuacao_projeto,
-					ft_zona_atuacao_projeto = dado_posterior.ft_zona_atuacao_projeto,
-					tx_descricao_projeto = dado_posterior.tx_descricao_projeto,
-					ft_descricao_projeto = dado_posterior.ft_descricao_projeto,
-					tx_metodologia_monitoramento = dado_posterior.tx_metodologia_monitoramento,
-					ft_metodologia_monitoramento = dado_posterior.ft_metodologia_monitoramento,
-					tx_link_projeto = dado_posterior.tx_link_projeto,
-					ft_link_projeto = dado_posterior.ft_link_projeto
-				WHERE id_projeto = dado_posterior.id_projeto;
+				SET	cd_conselho = dado_posterior.cd_conselho,
+					ft_conselho = dado_posterior.ft_conselho,
+					cd_tipo_participacao = dado_posterior.cd_tipo_participacao,
+					ft_tipo_participacao = dado_posterior.ft_tipo_participacao,
+					cd_periodicidade_reuniao_conselho = dado_posterior.cd_periodicidade_reuniao_conselho,
+					ft_periodicidade_reuniao = dado_posterior.ft_periodicidade_reuniao,
+					dt_data_inicio_conselho = dado_posterior.dt_data_inicio_conselho,
+					ft_data_inicio_conselho = dado_posterior.ft_data_inicio_conselho,
+					dt_data_fim_conselho = dado_posterior.dt_data_fim_conselho,
+					ft_data_fim_conselho = dado_posterior.ft_data_fim_conselho
+				WHERE id_conselho = dado_posterior.id_conselho;
 				
-				PERFORM portal.inserir_log_atualizacao(nome_tabela, osc, fonte, data_atualizacao, row_to_json(dado_anterior), row_to_json(dado_posterior),id_carga);
+				PERFORM portal.inserir_log_atualizacao(nome_tabela, osc, fonte, data_atualizacao, row_to_json(dado_anterior), row_to_json(dado_posterior), id_carga);
 			END IF;
 		END IF;
+		
+		FOR representante IN (SELECT * FROM jsonb_populate_recordset(null::osc.tb_representante_conselho, objeto.representante))
+		LOOP
+			SELECT INTO dado_anterior *
+			FROM osc.tb_representante_conselho
+			WHERE id_representante_conselho = representante.id_representante_conselho;
+
+			IF dado_anterior.id_conselho IS null THEN
+				INSERT INTO osc.tb_representante_conselho (
+					id_osc,
+					id_participacao_social_conselho,
+					tx_nome_representante_conselho,
+					ft_nome_representante_conselho
+				) VALUES (
+					osc.id_osc,
+					representante.id_participacao_social_conselho,
+					representante.tx_nome_representante_conselho,
+					fonte_dados.nome_fonte
+				) RETURNING * INTO dado_posterior;
+				
+				dado_nao_delete := array_append(dado_nao_delete, dado_posterior.id_representante_conselho);
+				
+				PERFORM portal.inserir_log_atualizacao(nome_tabela, osc, fonte, data_atualizacao, null, row_to_json(dado_posterior), id_carga);
+				
+			ELSE
+				dado_posterior := dado_anterior;
+				dado_nao_delete := array_append(dado_nao_delete, dado_posterior.id_projeto);
+				flag_update := false;
+				
+				IF (SELECT a.flag FROM portal.verificar_dado(dado_anterior.cd_conselho::TEXT, dado_anterior.ft_conselho, conselho.cd_conselho::TEXT, fonte_dados.prioridade, null_valido) AS a) THEN
+					dado_posterior.cd_conselho := conselho.cd_conselho;
+					dado_posterior.ft_conselho := fonte_dados.nome_fonte;
+					flag_update := true;
+				END IF;
+				
+				IF (SELECT a.flag FROM portal.verificar_dado(dado_anterior.cd_tipo_participacao::TEXT, dado_anterior.ft_tipo_participacao, conselho.cd_tipo_participacao::TEXT, fonte_dados.prioridade, null_valido) AS a) THEN
+					dado_posterior.cd_tipo_participacao := conselho.cd_tipo_participacao;
+					dado_posterior.ft_tipo_participacao := fonte_dados.nome_fonte;
+					flag_update := true;
+				END IF;
+				
+				IF (SELECT a.flag FROM portal.verificar_dado(dado_anterior.cd_periodicidade_reuniao_conselho::TEXT, dado_anterior.ft_periodicidade_reuniao, objeto.cd_periodicidade_reuniao_conselho::TEXT, fonte_dados.prioridade, null_valido) AS a) THEN
+					dado_posterior.cd_periodicidade_reuniao_conselho := objeto.cd_periodicidade_reuniao_conselho;
+					dado_posterior.ft_periodicidade_reuniao := fonte_dados.nome_fonte;
+					flag_update := true;
+				END IF;
+				
+				IF (SELECT a.flag FROM portal.verificar_dado(dado_anterior.dt_data_inicio_conselho::TEXT, dado_anterior.ft_data_inicio_conselho, conselho.dt_data_inicio_conselho::TEXT, fonte_dados.prioridade, null_valido) AS a) THEN
+					dado_posterior.dt_data_inicio_conselho := conselho.dt_data_inicio_conselho;
+					dado_posterior.ft_data_inicio_conselho := fonte_dados.nome_fonte;
+					flag_update := true;
+				END IF;
+				
+				IF (SELECT a.flag FROM portal.verificar_dado(dado_anterior.dt_data_fim_conselho::TEXT, dado_anterior.ft_data_fim_conselho, conselho.dt_data_fim_conselho::TEXT, fonte_dados.prioridade, null_valido) AS a) THEN
+					dado_posterior.dt_data_fim_conselho := conselho.dt_data_fim_conselho;
+					dado_posterior.ft_data_fim_conselho := fonte_dados.nome_fonte;
+					flag_update := true;
+				END IF;
+				
+				IF flag_update THEN
+					UPDATE osc.tb_participacao_social_conselho
+					SET	cd_conselho = dado_posterior.cd_conselho,
+						ft_conselho = dado_posterior.ft_conselho,
+						cd_tipo_participacao = dado_posterior.cd_tipo_participacao,
+						ft_tipo_participacao = dado_posterior.ft_tipo_participacao,
+						cd_periodicidade_reuniao_conselho = dado_posterior.cd_periodicidade_reuniao_conselho,
+						ft_periodicidade_reuniao = dado_posterior.ft_periodicidade_reuniao,
+						dt_data_inicio_conselho = dado_posterior.dt_data_inicio_conselho,
+						ft_data_inicio_conselho = dado_posterior.ft_data_inicio_conselho,
+						dt_data_fim_conselho = dado_posterior.dt_data_fim_conselho,
+						ft_data_fim_conselho = dado_posterior.ft_data_fim_conselho
+					WHERE id_conselho = dado_posterior.id_conselho;
+					
+					PERFORM portal.inserir_log_atualizacao(nome_tabela, osc, fonte, data_atualizacao, row_to_json(dado_anterior), row_to_json(dado_posterior), id_carga);
+				END IF;
+			END IF;
+		END LOOP;
+
 	END LOOP;
 
-	/*
-	objetivos = COALESCE((json->>'objetivo_metas')::JSONB, '{}'::JSONB);
-	SELECT INTO record_objetivos * FROM portal.atualizar_objetivos_osc(fonte, identificador, tipo_identificador, data_atualizacao, objetivos, null_valido, delete_valido, erro_log, id_carga, tipo_busca);
-	IF record_objetivos.flag = false THEN 
-		mensagem := record_objetivos.mensagem;
-		RAISE EXCEPTION 'funcao_externa';
-	END IF;
-	*/
 	localizacao = COALESCE((json->>'localizacao')::JSONB, '{}'::JSONB);
 	SELECT INTO record_localizacao * FROM portal.atualizar_localizacao_projeto(fonte, identificador, data_atualizacao, objetivos, null_valido, delete_valido, erro_log, id_carga, tipo_busca);
 	IF record_localizacao.flag = false THEN 
