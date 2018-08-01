19f9cc2007c49eec46387e115caefc8d7a693d83
vagnerpraia <vagnerpraia@gmail.com>
2018-08-01 17:03:59 -0300

    Acerto no editar parceria de projeto

--- database/2.6.1/33_alter_tb_osc_parceira_projeto.sql
+++ database/2.6.1/33_alter_tb_osc_parceira_projeto.sql
@@ -1,3 +1,10 @@
 ALTER TABLE osc.tb_osc_parceira_projeto ALTER bo_oficial DROP NOT NULL;
 ALTER TABLE osc.tb_osc_parceira_projeto DROP CONSTRAINT pk_tb_osc_parceira_projeto;
 ALTER TABLE osc.tb_osc_parceira_projeto ADD COLUMN id_osc_parceira_projeto SERIAL PRIMARY KEY;
+
+DROP INDEX osc.ix_tb_osc_parceira_projeto;
+
+CREATE UNIQUE INDEX ix_tb_osc_parceira_projeto
+    ON osc.tb_osc_parceira_projeto USING btree
+    (id_osc_parceira_projeto ASC NULLS LAST)
+TABLESPACE pg_default;
