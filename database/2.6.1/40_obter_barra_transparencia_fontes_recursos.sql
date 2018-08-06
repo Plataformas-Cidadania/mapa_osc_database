d85897c39db5499c791d9eb68f3167f82794bec1
vagnerpraia <vagnerpraia@gmail.com>
2018-08-06 17:01:08 -0300

    Acerto na contagem de recursos de OSC  na barra de transparência

---
+++ database/2.6.1/40_obter_barra_transparencia_fontes_recursos.sql
@@ -0,0 +1,23 @@
+DROP FUNCTION IF EXISTS portal.obter_barra_transparencia_fontes_recursos(osc INTEGER);
+
+CREATE OR REPLACE FUNCTION portal.obter_barra_transparencia_fontes_recursos(osc INTEGER) RETURNS TABLE (
+	pontuacao DOUBLE PRECISION
+) AS $$ 
+
+DECLARE 
+	peso_campo DOUBLE PRECISION;
+
+BEGIN 
+	peso_campo := 100.0 / 4.0;
+	
+	RETURN QUERY 
+        SELECT 
+			SUM(
+				(CASE WHEN EXISTS(SELECT * FROM osc.tb_recursos_osc LEFT JOIN syst.dc_fonte_recursos_osc ON tb_recursos_osc.cd_fonte_recursos_osc = dc_fonte_recursos_osc.cd_fonte_recursos_osc WHERE tb_recursos_osc.id_osc = osc AND (tb_recursos_osc.cd_origem_fonte_recursos_osc = 1 OR dc_fonte_recursos_osc.cd_origem_fonte_recursos_osc = 1)) THEN peso_campo ELSE 0 END) + 
+				(CASE WHEN EXISTS(SELECT * FROM osc.tb_recursos_osc LEFT JOIN syst.dc_fonte_recursos_osc ON tb_recursos_osc.cd_fonte_recursos_osc = dc_fonte_recursos_osc.cd_fonte_recursos_osc WHERE tb_recursos_osc.id_osc = osc AND (tb_recursos_osc.cd_origem_fonte_recursos_osc = 2 OR dc_fonte_recursos_osc.cd_origem_fonte_recursos_osc = 2)) THEN peso_campo ELSE 0 END) + 
+				(CASE WHEN EXISTS(SELECT * FROM osc.tb_recursos_osc LEFT JOIN syst.dc_fonte_recursos_osc ON tb_recursos_osc.cd_fonte_recursos_osc = dc_fonte_recursos_osc.cd_fonte_recursos_osc WHERE tb_recursos_osc.id_osc = osc AND (tb_recursos_osc.cd_origem_fonte_recursos_osc = 3 OR dc_fonte_recursos_osc.cd_origem_fonte_recursos_osc = 3)) THEN peso_campo ELSE 0 END) + 
+				(CASE WHEN EXISTS(SELECT * FROM osc.tb_recursos_osc LEFT JOIN syst.dc_fonte_recursos_osc ON tb_recursos_osc.cd_fonte_recursos_osc = dc_fonte_recursos_osc.cd_fonte_recursos_osc WHERE tb_recursos_osc.id_osc = osc AND (tb_recursos_osc.cd_origem_fonte_recursos_osc = 4 OR dc_fonte_recursos_osc.cd_origem_fonte_recursos_osc = 4)) THEN peso_campo ELSE 0 END)
+			);
+END;
+
+$$ LANGUAGE 'plpgsql';
