6fc841b3dfe7cc60bf4aa095751b88b8259037eb
tmnascimento <tmnascimento@gmail.com>
2018-08-17 15:27:51 -0300

    Update 30_alter_tb_recursos_osc.sql
    SELECT corrigido. Vinha muitos dados.

--- database/2.6.0/30_alter_tb_recursos_osc.sql
+++ database/2.6.0/30_alter_tb_recursos_osc.sql
@@ -1,2 +1,2 @@
 UPDATE osc.tb_recursos_osc 
-SET cd_origem_fonte_recursos_osc = (SELECT cd_origem_fonte_recursos_osc FROM syst.dc_fonte_recursos_osc WHERE cd_fonte_recursos_osc = tb_recursos_osc.cd_fonte_recursos_osc);
+SET cd_origem_fonte_recursos_osc = (SELECT DISTINCT cd_origem_fonte_recursos_osc FROM syst.dc_fonte_recursos_osc WHERE cd_fonte_recursos_osc = osc.tb_recursos_osc.cd_fonte_recursos_osc);
