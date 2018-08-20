01ea46a6c54db15e5b52e8d4c9d737a71fa5b8cf
tmnascimento <tmnascimento@gmail.com>
2018-08-20 14:13:13 -0300

    Update 30_alter_tb_recursos_osc.sql
    Adição do DISABLE TRIGGER pois estava dando erro de memória.

--- database/2.6.0/30_alter_tb_recursos_osc.sql
+++ database/2.6.0/30_alter_tb_recursos_osc.sql
@@ -1,2 +1,3 @@
+ALTER TABLE osc.tb_recursos_osc DISABLE TRIGGER ALL;
 UPDATE osc.tb_recursos_osc 
 SET cd_origem_fonte_recursos_osc = (SELECT DISTINCT cd_origem_fonte_recursos_osc FROM syst.dc_fonte_recursos_osc WHERE cd_fonte_recursos_osc = osc.tb_recursos_osc.cd_fonte_recursos_osc);
