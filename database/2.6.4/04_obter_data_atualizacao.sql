c3305227e4618350e215b3c6aa436dc8482903b3
vagnerpraia <vagnerpraia@gmail.com>
2018-08-27 18:53:35 -0300

    Acerto no obter data de atualização

---
+++ database/2.6.4/04_obter_data_atualizacao.sql
@@ -0,0 +1,17 @@
+﻿DROP FUNCTION IF EXISTS portal.obter_data_atualizacao(TEXT);
+
+CREATE OR REPLACE FUNCTION portal.obter_data_atualizacao(osc TEXT) RETURNS TABLE(
+	dt_alteracao TEXT
+) AS $$ 
+
+BEGIN 
+	RETURN QUERY 
+		SELECT TO_CHAR(MAX(tb_log_alteracao.dt_alteracao), 'DD-MM-YYYY'::TEXT) 
+		FROM log.tb_log_alteracao 
+		LEFT JOIN osc.tb_osc 
+		ON tb_osc.id_osc = tb_log_alteracao.id_osc 
+		WHERE tb_log_alteracao.id_osc::TEXT = osc 
+		OR tb_osc.tx_apelido_osc = osc;
+
+END; 
+$$ LANGUAGE 'plpgsql';
