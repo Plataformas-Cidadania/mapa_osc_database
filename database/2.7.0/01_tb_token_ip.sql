c54cb5896a09e78056bf704f0a69f9dc24d07ddb
vagnerpraia <vagnerpraia@gmail.com>
2018-09-10 18:25:48 -0300

    Scripts para adição de restrição por IP

---
+++ database/2.7.0/01_tb_token_ip.sql
@@ -0,0 +1,11 @@
+-- DROP TABLE portal.tb_token_ip;
+
+CREATE TABLE portal.tb_token_ip
+(
+  id_token SERIAL NOT NULL, -- Identificador do token
+  tx_ip TEXT NOT NULL, -- IP de acesso
+  tx_token TEXT NOT NULL, -- Token do IP
+  dt_data_expiracao TIMESTAMP NOT NULL, -- Data de expiração do token
+  CONSTRAINT pk_tb_token_ip PRIMARY KEY (id_token), -- Chave primária
+  CONSTRAINT un_tx_ip UNIQUE (tx_ip) -- Restrição para IP único
+);
---
+++ database/2.7.0/02_obter_token_ip.sql
@@ -0,0 +1,53 @@
+DROP FUNCTION IF EXISTS portal.obter_token_ip(TEXT, TEXT, TIMESTAMP);
+
+CREATE OR REPLACE FUNCTION portal.obter_token_ip(ip TEXT, token TEXT, data_execucao TIMESTAMP) RETURNS TABLE (
+	resultado JSONB,
+	mensagem TEXT,
+	flag BOOLEAN
+) AS $$ 
+
+DECLARE 
+	data_expericao TIMESTAMP;
+	linha_token RECORD;
+
+BEGIN 
+	data_expericao := (data_execucao + interval '1' day)::TIMESTAMP;
+	resultado := null;
+	
+	SELECT INTO linha_token tx_token, dt_data_expiracao 
+		FROM portal.tb_token_ip 
+		WHERE tx_ip = ip;
+	
+	IF linha_token IS null THEN 
+		INSERT INTO portal.tb_token_ip (tx_ip, tx_token, dt_data_expiracao) 
+			VALUES (ip, token, data_expericao) 
+			RETURNING tx_token, dt_data_expiracao 
+			INTO linha_token;
+	
+	ELSIF linha_token.dt_data_expiracao < data_execucao THEN 
+		UPDATE portal.tb_token_ip 
+			SET tx_token = token, dt_data_expiracao = data_expericao
+			RETURNING tx_token, dt_data_expiracao 
+			INTO linha_token;
+	END IF;
+	
+	resultado := TO_JSONB(linha_token);
+	mensagem := 'Token retornado.';
+	flag := true;
+	
+	RETURN NEXT;
+	
+EXCEPTION
+	WHEN others THEN 
+		flag := false;
+		SELECT INTO mensagem a.mensagem FROM portal.verificar_erro(SQLSTATE, SQLERRM, null, null, null, false, null) AS a;
+		RETURN NEXT;
+END;
+
+$$ LANGUAGE 'plpgsql';
+
+SELECT * FROM portal.obter_token_ip(
+	1::TEXT, 
+	123::TEXT, 
+	now()::TIMESTAMP
+);
