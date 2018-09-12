caa300feac57f79c7fec062b9b1e3dbbe83792bc
vagnerpraia <vagnerpraia@gmail.com>
2018-09-12 19:23:05 -0300

    Scripts para restrição de acesso por IP

--- database/2.7.0/03_obter_quantidade_acessos_token_ip.sql
+++ database/2.7.0/03_obter_quantidade_acessos_token_ip.sql
@@ -1,17 +1,19 @@
-DROP FUNCTION IF EXISTS portal.obter_quantidade_acessos_token_ip(TEXT, TIMESTAMP);
+DROP FUNCTION IF EXISTS portal.obter_quantidade_acessos_token_ip(TEXT);
 
-CREATE OR REPLACE FUNCTION portal.obter_quantidade_acessos_token_ip(ip TEXT, data_execucao TIMESTAMP) RETURNS TABLE (
+CREATE OR REPLACE FUNCTION portal.obter_quantidade_acessos_token_ip(ip TEXT) RETURNS TABLE (
 	resultado JSONB,
 	mensagem TEXT,
 	flag BOOLEAN
 ) AS $$ 
 
 DECLARE
-	data_expericao TIMESTAMP;
+	data_execucao TIMESTAMP;
+	quantiade_limite_acesso INTEGER;
 	linha_token RECORD;
 
 BEGIN 
-	data_expericao := (data_execucao + interval '1' day)::TIMESTAMP;
+	data_execucao := now();
+	quantiade_limite_acesso := 100;
 	resultado := null;
 
 	SELECT INTO linha_token tx_token, dt_data_expiracao, nr_quantidade_acessos 
@@ -20,12 +22,18 @@ BEGIN
 
 	IF linha_token IS NOT null THEN 
         IF linha_token.dt_data_expiracao > data_execucao THEN 
-            UPDATE portal.tb_token_ip 
-                SET nr_quantidade_acessos = (nr_quantidade_acessos + 1);
+			IF quantiade_limite_acesso > linha_token.nr_quantidade_acessos THEN 
+				UPDATE portal.tb_token_ip 
+					SET nr_quantidade_acessos = (nr_quantidade_acessos + 1);
 
-			resultado := TO_JSONB(linha_token);
-			mensagem := 'Token retornado.';
-			flag := true;
+				resultado := TO_JSONB(linha_token);
+				mensagem := 'Token retornado.';
+				flag := true;
+
+			ELSE
+				mensagem := 'Este IP atingiu o limite de acessos permitidos.';
+				flag := false;
+			END IF;
 
 		ELSE
 			mensagem := 'O token deste IP expirou.';
