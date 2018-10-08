4d869557ee9f5738247bf6750a31925ca514abad
vagnerpraia <vagnerpraia@gmail.com>
2018-10-08 17:13:28 -0300

    Acerto do script de atualização dos dados dos gráficos

--- database/2.7.0/04_obter_grafico_oscs_assistencia_social_tipo_servico.sql
+++ database/2.7.0/04_obter_grafico_oscs_assistencia_social_tipo_servico.sql
@@ -5,18 +5,7 @@ CREATE OR REPLACE FUNCTION portal.obter_grafico_oscs_assistencia_social_tipo_ser
 	fontes TEXT[]
 ) AS $$ 
 
-DECLARE 
-	quantidade_oscs DOUBLE PRECISION;
-
 BEGIN 
-	quantidade_oscs := (
-		SELECT COUNT(*) 
-		FROM osc.tb_osc 
-		INNER JOIN graph.tb_cneas_mds 
-		ON tb_osc.cd_identificador_osc::TEXT = REGEXP_REPLACE(tb_cneas_mds.cnpj, '[^0-9]', '', 'g') 
-		WHERE bo_osc_ativa IS true
-	);
-
 	RETURN QUERY 
 		SELECT 
 			('[{' || RTRIM(LTRIM(REPLACE(REPLACE(REPLACE(REPLACE((TRANSLATE(ARRAY_AGG('{"label": "' || a.rotulo::TEXT || '", "value": ' || a.valor::TEXT || '}')::TEXT, '\', '') || '}'), '""', '"'), '}",', '},'), '"}', '}'), '"{', '{'), '{'), '}') || '}]')::JSONB AS dados, 
@@ -32,7 +21,7 @@ BEGIN
 					ELSE SUBSTRING(tb_cneas_mds.bloco_servico FROM 5 FOR LENGTH(tb_cneas_mds.bloco_servico)) 
 				END
 			) AS rotulo, 
-			ROUND(((COUNT(*) / quantidade_oscs) * 100.)::NUMERIC, 2) AS valor, 
+			COUNT(*) AS valor, 
 			(
 				SELECT ARRAY_AGG(TRANSLATE(a::TEXT, '()', '')) FROM (SELECT DISTINCT UNNEST(
 					ARRAY_CAT(
