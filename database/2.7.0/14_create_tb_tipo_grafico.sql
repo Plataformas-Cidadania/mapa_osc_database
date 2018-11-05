be37b78132bf52ec5e47a967a30662bf518ed6ac
vagnerpraia <vagnerpraia@gmail.com>
2018-11-05 17:49:57 -0200

    Acertos nas tabelas de gráficos

--- database/2.7.0/14_create_tb_tipo_grafico.sql
+++ database/2.7.0/14_create_tb_tipo_grafico.sql
@@ -1,6 +1,6 @@
 CREATE TABLE syst.tb_tipo_grafico(
-	id_tipo_grafico SERIAL, 
+	id_grafico SERIAL, 
 	nome_tipo_grafico TEXT, 
 	status INTEGER, 
-	CONSTRAINT pk_tb_tipo_grafico PRIMARY KEY (id_tipo_grafico)
+	CONSTRAINT pk_tb_tipo_grafico PRIMARY KEY (id_grafico)
 );
