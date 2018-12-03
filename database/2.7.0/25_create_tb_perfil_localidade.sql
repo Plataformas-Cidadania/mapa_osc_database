978b9259689003caec0b19af6e3a902afa3d08e0
vagnerpraia <vagnerpraia@gmail.com>
2018-12-03 17:33:18 -0200

    Atualização da tabela de perfil de localidade

--- database/2.7.0/25_create_tb_perfil_localidade.sql
+++ database/2.7.0/25_create_tb_perfil_localidade.sql
@@ -6,15 +6,18 @@ CREATE TABLE portal.tb_perfil_localidade
     id_localidade INTEGER UNIQUE NOT NULL, -- Identificador da localidade
     tx_localidade TEXT, -- Nome da localidade
 	tx_tipo_localidade TEXT, -- Nome do tipo da localidade
-    nr_quantidade_osc INTEGER, -- Quantidade de OSCs da localidade
-    nr_quantidade_trabalhadores INTEGER, -- Quantidade de trabalhadores em OSCs da localidade
-    nr_quantidade_recursos NUMERIC, -- Quantidade de recursos das OSCs da localidade
-    nr_quantidade_projetos INTEGER, -- Quantidade de projetos das OSCs da localidade
+    caracteristicas JSON, -- Dados sobre características gerais das OSCs na localidade
+    ft_caracteristicas TEXT[], -- Fontes sobre características gerais das OSCs na localidade
     evolucao_quantidade_osc_ano JSONB, -- Dados sobre a evolução da quantidade de OSCs por ano na localidade
+    ft_evolucao_quantidade_osc_ano TEXT[], -- Fontes sobre a evolução da quantidade de OSCs por ano na localidade
     natureza_juridica JSONB, -- Dados sobre a natureza jurídica das OSCs na localidade
-    repasse_recursos_federais JSONB, -- Dados sobre os repasses de recursos federais para as OSCs da localidade
+    ft_natureza_juridica TEXT[], -- Fontes sobre a natureza jurídica das OSCs na localidade
+    repasse_recursos JSONB, -- Dados sobre os repasses de recursos para as OSCs da localidade
+    ft_repasse_recursos TEXT[], -- Fontes sobre os repasses de recursos para as OSCs da localidade
     area_atuacao JSONB, -- Dados sobre a área de atuação das OSCs na localidade
-    trabalhadores JSONB -- Dados sobre os trabalhadores das OSCs na localidade
+    ft_area_atuacao TEXT[], -- Fontes sobre a área de atuação das OSCs na localidade
+    trabalhadores JSONB, -- Dados sobre os trabalhadores das OSCs na localidade
+    ft_trabalhadores TEXT[] -- Fontes sobre os trabalhadores das OSCs na localidade
 );
 
 CREATE UNIQUE INDEX ix_perfil_localidade
