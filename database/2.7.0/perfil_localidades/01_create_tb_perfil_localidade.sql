3bd8e20bc48e268132575f35081e9ffe0d3b1266
vagnerpraia <vagnerpraia@gmail.com>
2018-12-10 16:27:00 -0200

    Atualização dos scripts para o perfil de localidades

--- database/2.7.0/perfil_localidades/01_create_tb_perfil_localidade.sql
+++ database/2.7.0/perfil_localidades/01_create_tb_perfil_localidade.sql
@@ -6,6 +6,24 @@ CREATE TABLE portal.tb_perfil_localidade
     id_localidade INTEGER UNIQUE NOT NULL, -- Identificador da localidade
     tx_localidade TEXT, -- Nome da localidade
 	tx_tipo_localidade TEXT, -- Nome do tipo da localidade
+    nr_colocacao_nacional_quantidade_osc_regiao INTEGER, -- Colocação em nível nacional da quantidade de OSCs por região
+    nr_colocacao_nacional_quantidade_osc_estado INTEGER, -- Colocação em nível nacional da quantidade de OSCs por estado
+    nr_colocacao_nacional_quantidade_osc_municipio INTEGER, -- Colocação em nível nacional da quantidade de OSCs por município
+    nr_maior_concentracao_natureza_juridica_regiao NUMERIC, -- Maior concentração de OSCs de uma determinada natureza jurídica por região
+    nr_maior_concentracao_natureza_juridica_estado NUMERIC, -- Maior concentração de OSCs de uma determinada natureza jurídica por estado
+    nr_maior_concentracao_natureza_juridica_municipio NUMERIC, -- Maior concentração de OSCs de uma determinada natureza jurídica por município
+    tx_maior_concentracao_natureza_juridica_regiao NUMERIC, -- Natureza jurídica da maior concentração por região
+    tx_maior_concentracao_natureza_juridica_estado NUMERIC, -- Natureza jurídica da maior concentração por estado
+    tx_maior_concentracao_natureza_juridica_municipio NUMERIC, -- Natureza jurídica da maior concentração por município
+    nr_media_concentracao_natureza_juridica_regiao NUMERIC, -- Média concentração de OSCs de uma determinada natureza jurídica por região
+    nr_media_concentracao_natureza_juridica_estado NUMERIC, -- Média concentração de OSCs de uma determinada natureza jurídica por estado
+    nr_media_concentracao_natureza_juridica_municipio NUMERIC, -- Média concentração de OSCs de uma determinada natureza jurídica por município
+    tx_media_concentracao_natureza_juridica_regiao NUMERIC, -- Natureza jurídica da média concentração por região
+    tx_media_concentracao_natureza_juridica_estado NUMERIC, -- Natureza jurídica da média concentração por estado
+    tx_media_concentracao_natureza_juridica_municipio NUMERIC, -- Natureza jurídica da média concentração por município
+    nr_colocacao_nacional_repasses_regiao NUMERIC, -- Maior montante de repasses por região
+    nr_colocacao_nacional_repasses_estado NUMERIC, -- Maior montante de repasses por estado
+    nr_colocacao_nacional_repasses_municipio NUMERIC, -- Maior montante de repasses por município
     caracteristicas JSON, -- Dados sobre características gerais das OSCs na localidade
     ft_caracteristicas TEXT[], -- Fontes sobre características gerais das OSCs na localidade
     evolucao_quantidade_osc_ano JSONB, -- Dados sobre a evolução da quantidade de OSCs por ano na localidade
---
+++ database/2.7.0/perfil_localidades/04_obter_perfil_localidade_evolucao_quantidade_ano.sql
@@ -0,0 +1,99 @@
+DROP FUNCTION IF EXISTS portal.obter_perfil_localidade_evolucao_quantidade_ano(INTEGER) CASCADE;
+
+CREATE OR REPLACE FUNCTION portal.obter_perfil_localidade_evolucao_quantidade_ano(id_localidade INTEGER) RETURNS TABLE (
+	dados JSONB, 
+	fontes TEXT[]
+) AS $$ 
+
+BEGIN 
+	RETURN QUERY 
+		SELECT 
+			('{' || RTRIM(LTRIM(REPLACE(REPLACE((TRANSLATE(ARRAY_AGG(
+				'{' ||
+					'"nr_quantidade_oscs": "' || a.nr_quantidade_oscs::TEXT || '", ' || 
+					'"nr_quantidade_trabalhadores": "' || a.nr_quantidade_trabalhadores::TEXT ||  '", ' || 
+					'"nr_quantidade_recursos": "' || a.nr_quantidade_recursos::TEXT || '", ' || 
+					'"nr_quantidade_projetos": "' || a.nr_quantidade_projetos::TEXT ||  '"' || 
+				'}'
+			)::TEXT, '\', '') || '}'), '{"{"', '{"'), '"}"}', '"}'), '{'), '}') || '}')::JSONB AS dados, 
+			(
+				SELECT ARRAY_AGG(TRANSLATE(a::TEXT, '()\"', '')) FROM (SELECT DISTINCT UNNEST(
+					TRANSLATE(ARRAY_AGG(REPLACE(REPLACE(TRIM(TRANSLATE(a.fontes::TEXT, '"\{}', ''), ','), '","', ','), ',,', ','))::TEXT, '"', '')::TEXT[]
+				)) AS a
+			) AS fontes 
+		FROM (
+			SELECT 
+				COUNT(tb_osc) AS nr_quantidade_oscs, 
+				SUM(
+					COALESCE(tb_relacoes_trabalho.nr_trabalhadores_vinculo, 0) + 
+					COALESCE(tb_relacoes_trabalho.nr_trabalhadores_deficiencia, 0) + 
+					COALESCE(tb_relacoes_trabalho.nr_trabalhadores_voluntarios, 0) + 
+					COALESCE(tb_relacoes_trabalho_outra.nr_trabalhadores, 0)
+				) AS nr_quantidade_trabalhadores, 
+				SUM(
+					COALESCE(tb_recursos_osc.nr_valor_recursos_osc, 0) + 
+					COALESCE(tb_recursos_outro_osc.nr_valor_recursos_outro_osc, 0)
+				) AS nr_quantidade_recursos, 
+				COUNT(tb_projeto) AS nr_quantidade_projetos, 
+				(
+					SELECT ARRAY_AGG(TRANSLATE(a::TEXT, '()', '')) FROM (SELECT DISTINCT UNNEST(
+						ARRAY_CAT(
+							ARRAY_CAT(
+								ARRAY_CAT(
+									ARRAY_CAT(
+										ARRAY_CAT(
+											ARRAY_CAT(
+												ARRAY_CAT(
+													ARRAY_CAT(
+														ARRAY_CAT(
+															ARRAY_AGG(DISTINCT COALESCE(tb_osc.ft_identificador_osc, '')), 
+															ARRAY_AGG(DISTINCT COALESCE(tb_osc.ft_osc_ativa, ''))
+														),
+														ARRAY_AGG(DISTINCT COALESCE(tb_relacoes_trabalho.ft_trabalhadores_vinculo, ''))
+													),
+													ARRAY_AGG(DISTINCT COALESCE(tb_relacoes_trabalho.ft_trabalhadores_deficiencia, ''))
+												),
+												ARRAY_AGG(DISTINCT COALESCE(tb_relacoes_trabalho.ft_trabalhadores_voluntarios, ''))
+											),
+											ARRAY_AGG(DISTINCT COALESCE(tb_relacoes_trabalho_outra.ft_trabalhadores, ''))
+										),
+										ARRAY_AGG(DISTINCT COALESCE(tb_recursos_osc.ft_valor_recursos_osc, ''))
+									),
+									ARRAY_AGG(DISTINCT COALESCE(tb_recursos_outro_osc.ft_valor_recursos_outro_osc, ''))
+								),
+								ARRAY_AGG(DISTINCT COALESCE(tb_projeto.ft_nome_projeto, ''))
+							),
+							ARRAY_AGG(DISTINCT COALESCE(tb_projeto.ft_identificador_projeto_externo, ''))
+						)
+					)) AS a
+				) AS fontes 
+			FROM osc.tb_osc 
+			LEFT JOIN osc.tb_relacoes_trabalho 
+			ON tb_osc.id_osc = tb_relacoes_trabalho.id_osc 
+			LEFT JOIN osc.tb_relacoes_trabalho_outra 
+			ON tb_osc.id_osc = tb_relacoes_trabalho_outra.id_osc 
+			LEFT JOIN osc.tb_recursos_osc 
+			ON tb_osc.id_osc = tb_recursos_osc.id_osc 
+			LEFT JOIN osc.tb_recursos_outro_osc 
+			ON tb_osc.id_osc = tb_recursos_outro_osc.id_osc 
+			LEFT JOIN osc.tb_projeto 
+			ON tb_osc.id_osc = tb_projeto.id_osc 
+			LEFT JOIN osc.tb_localizacao 
+			ON tb_osc.id_osc = tb_localizacao.id_osc 
+			LEFT JOIN spat.ed_regiao 
+			ON (SELECT SUBSTR(tb_localizacao.cd_municipio::TEXT, 1, 1))::NUMERIC(1, 0) = ed_regiao.edre_cd_regiao 
+			WHERE tb_osc.bo_osc_ativa 
+			AND tb_osc.id_osc <> 789809 
+			AND (
+				SUBSTR(tb_localizacao.cd_municipio::TEXT, 1, 1) = id_localidade::TEXT 
+				OR 
+				SUBSTR(tb_localizacao.cd_municipio::TEXT, 1, 2) = id_localidade::TEXT 
+				OR 
+				tb_localizacao.cd_municipio = id_localidade 
+			)
+		) AS a;
+END;
+
+$$ LANGUAGE 'plpgsql';
+
+SELECT * FROM portal.obter_perfil_localidade_caracteristicas(11);
