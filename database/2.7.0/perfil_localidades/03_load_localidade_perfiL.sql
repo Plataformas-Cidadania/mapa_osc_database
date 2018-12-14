3510467225bba24e926d6e46881e1deb593a6257
vagnerpraia <vagnerpraia@gmail.com>
2018-12-14 02:08:00 -0200

    Atualização da maior porcentagem de natureza jurídica

--- database/2.7.0/perfil_localidades/03_load_localidade_perfiL.sql
+++ database/2.7.0/perfil_localidades/03_load_localidade_perfiL.sql
@@ -11,12 +11,29 @@ BEGIN
         	RETURN QUERY
 				SELECT
 					('{"localidades": [' || REPLACE(REPLACE(RTRIM(LTRIM(ARRAY_AGG(b.nm_localidade)::TEXT, '{'), '}'), '\', ''), '""', '"') || '], "maior_media": ' || b.media::TEXT || '}')::JSONB AS dados,
-					null::TEXT[] AS fontes
+					b.fontes::TEXT[] AS fontes
 				FROM 
 				(
-					SELECT '"' || a.nm_localidade || '"' AS nm_localidade, MAX(a.quantidade) / SUM(a.quantidade) * 100 AS media
+					SELECT
+						'"' || a.nm_localidade || '"' AS nm_localidade,
+						MAX(a.quantidade) / SUM(a.quantidade) * 100 AS media,
+						(
+							SELECT ARRAY_AGG(TRANSLATE(a::TEXT, '()\"', '')) FROM (SELECT DISTINCT UNNEST(
+								TRANSLATE(ARRAY_AGG(REPLACE(REPLACE(TRIM(TRANSLATE(a.fontes::TEXT, '"\{}', ''), ','), '","', ','), ',,', ','))::TEXT, '"', '')::TEXT[]
+							)) AS a
+						) AS fontes
 					FROM (
-						SELECT edre_nm_regiao AS nm_localidade, cd_natureza_juridica_osc, COUNT(*) AS quantidade
+						SELECT
+							edre_nm_regiao AS nm_localidade,
+							cd_natureza_juridica_osc, COUNT(*) AS quantidade,
+							(
+								SELECT ARRAY_AGG(TRANSLATE(a::TEXT, '()', '')) FROM (SELECT DISTINCT UNNEST(
+									ARRAY_CAT(
+										ARRAY_AGG(DISTINCT COALESCE(ft_natureza_juridica_osc, '')), 
+										ARRAY_AGG(DISTINCT COALESCE(ft_municipio, ''))
+									)
+								)) AS a
+							) AS fontes
 						FROM osc.tb_dados_gerais
 						LEFT JOIN osc.tb_localizacao
 						ON tb_dados_gerais.id_osc = tb_localizacao.id_osc
@@ -28,19 +45,37 @@ BEGIN
 					) AS a
 					GROUP BY a.nm_localidade
 				) AS b
-				GROUP BY b.media
+				GROUP BY b.media, b.fontes
 				ORDER BY b.media DESC
 				LIMIT 1;
+
 		WHEN tipo_localidade = 'estado' THEN
         	RETURN QUERY
 				SELECT
 					('{"localidades": [' || REPLACE(REPLACE(RTRIM(LTRIM(ARRAY_AGG(b.nm_localidade)::TEXT, '{'), '}'), '\', ''), '""', '"') || '], "maior_media": ' || b.media::TEXT || '}')::JSONB AS dados,
-					null::TEXT[] AS fontes
+					b.fontes::TEXT[] AS fontes
 				FROM 
 				(
-					SELECT '"' || a.nm_localidade || '"' AS nm_localidade, MAX(a.quantidade) / SUM(a.quantidade) * 100 AS media
+					SELECT
+						'"' || a.nm_localidade || '"' AS nm_localidade,
+						MAX(a.quantidade) / SUM(a.quantidade) * 100 AS media,
+						(
+							SELECT ARRAY_AGG(TRANSLATE(a::TEXT, '()\"', '')) FROM (SELECT DISTINCT UNNEST(
+								TRANSLATE(ARRAY_AGG(REPLACE(REPLACE(TRIM(TRANSLATE(a.fontes::TEXT, '"\{}', ''), ','), '","', ','), ',,', ','))::TEXT, '"', '')::TEXT[]
+							)) AS a
+						) AS fontes
 					FROM (
-						SELECT eduf_nm_uf AS nm_localidade, cd_natureza_juridica_osc, COUNT(*) AS quantidade
+						SELECT
+							eduf_nm_uf AS nm_localidade,
+							cd_natureza_juridica_osc, COUNT(*) AS quantidade,
+							(
+								SELECT ARRAY_AGG(TRANSLATE(a::TEXT, '()', '')) FROM (SELECT DISTINCT UNNEST(
+									ARRAY_CAT(
+										ARRAY_AGG(DISTINCT COALESCE(ft_natureza_juridica_osc, '')), 
+										ARRAY_AGG(DISTINCT COALESCE(ft_municipio, ''))
+									)
+								)) AS a
+							) AS fontes
 						FROM osc.tb_dados_gerais
 						LEFT JOIN osc.tb_localizacao
 						ON tb_dados_gerais.id_osc = tb_localizacao.id_osc
@@ -52,7 +87,7 @@ BEGIN
 					) AS a
 					GROUP BY a.nm_localidade
 				) AS b
-				GROUP BY b.media
+				GROUP BY b.media, b.fontes
 				ORDER BY b.media DESC
 				LIMIT 1;
 
@@ -60,12 +95,29 @@ BEGIN
         	RETURN QUERY
 				SELECT
 					('{"localidades": [' || REPLACE(REPLACE(RTRIM(LTRIM(ARRAY_AGG(b.nm_localidade)::TEXT, '{'), '}'), '\', ''), '""', '"') || '], "maior_media": ' || b.media::TEXT || '}')::JSONB AS dados,
-					null::TEXT[] AS fontes
+					b.fontes::TEXT[] AS fontes
 				FROM 
 				(
-					SELECT '"' || a.nm_localidade || '"' AS nm_localidade, MAX(a.quantidade) / SUM(a.quantidade) * 100 AS media
+					SELECT
+						'"' || a.nm_localidade || '"' AS nm_localidade,
+						MAX(a.quantidade) / SUM(a.quantidade) * 100 AS media,
+						(
+							SELECT ARRAY_AGG(TRANSLATE(a::TEXT, '()\"', '')) FROM (SELECT DISTINCT UNNEST(
+								TRANSLATE(ARRAY_AGG(REPLACE(REPLACE(TRIM(TRANSLATE(a.fontes::TEXT, '"\{}', ''), ','), '","', ','), ',,', ','))::TEXT, '"', '')::TEXT[]
+							)) AS a
+						) AS fontes
 					FROM (
-						SELECT edmu_nm_municipio || ' - ' || eduf_sg_uf AS nm_localidade, cd_natureza_juridica_osc, COUNT(*) AS quantidade
+						SELECT
+							edmu_nm_municipio || ' - ' || eduf_sg_uf AS nm_localidade,
+							cd_natureza_juridica_osc, COUNT(*) AS quantidade,
+							(
+								SELECT ARRAY_AGG(TRANSLATE(a::TEXT, '()', '')) FROM (SELECT DISTINCT UNNEST(
+									ARRAY_CAT(
+										ARRAY_AGG(DISTINCT COALESCE(ft_natureza_juridica_osc, '')), 
+										ARRAY_AGG(DISTINCT COALESCE(ft_municipio, ''))
+									)
+								)) AS a
+							) AS fontes
 						FROM osc.tb_dados_gerais
 						LEFT JOIN osc.tb_localizacao
 						ON tb_dados_gerais.id_osc = tb_localizacao.id_osc
@@ -79,7 +131,7 @@ BEGIN
 					) AS a
 					GROUP BY a.nm_localidade
 				) AS b
-				GROUP BY b.media
+				GROUP BY b.media, b.fontes
 				ORDER BY b.media DESC
 				LIMIT 1;
 
