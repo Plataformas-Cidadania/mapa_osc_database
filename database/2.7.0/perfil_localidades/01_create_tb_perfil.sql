120149b3a65e89feb3de857cb227d05e3bef4b72
vagnerpraia <vagnerpraia@gmail.com>
2018-12-14 15:17:07 -0200

    Adição de função para atualizar perfil nacional

--- database/2.7.0/perfil_localidades/01_create_tb_perfil.sql
+++ database/2.7.0/perfil_localidades/01_create_tb_perfil.sql
@@ -2,32 +2,10 @@ DROP TABLE IF EXISTS portal.tb_perfil;
 
 CREATE TABLE portal.tb_perfil
 (
-    id SERIAL,
-    nome_perfil TEXT,
+    id INTEGER NOT NULL,
+    nome TEXT,
     tipo_localidade TEXT,
     series JSONB,
-    fonte TEXT[]
-    /*
-    tx_maior_media_nacional_natureza_juridica_regiao TEXT[],
-    nr_porcentagem_maior_media_natureza_juridica_regiao NUMERIC,
-    tx_maior_media_nacional_natureza_juridica_estado TEXT[],
-    nr_porcentagem_maior_media_natureza_juridica_estado NUMERIC,
-    tx_maior_media_nacional_natureza_juridica_municipio TEXT[],
-    nr_porcentagem_maior_media_natureza_juridica_municipio NUMERIC,
-
-    nr_media_nacional_repasse_regiao NUMERIC,
-    nr_media_nacional_repasse_estado NUMERIC,
-    nr_media_nacional_repasse_municipio NUMERIC,
-
-    tx_maior_media_nacional_area_atuacao_regiao TEXT[],
-    nr_porcentagem_maior_media_nacional_area_atuacao_regiao NUMERIC,
-    tx_maior_media_nacional_area_atuacao_estado TEXT[],
-    nr_porcentagem_maior_media_nacional_area_atuacao_estado NUMERIC,
-    tx_maior_media_nacional_area_atuacao_municipio TEXT[],
-    nr_porcentagem_maior_media_nacional_area_atuacao_municipio NUMERIC,
-
-    nr_porcentagem_maior_media_nacional_trabalhadores_regiao NUMERIC,
-    nr_porcentagem_maior_media_nacional_trabalhadores_estado NUMERIC,
-    nr_porcentagem_maior_media_nacional_trabalhadores_municipio NUMERIC
-    */
+    fontes TEXT[],
+    CONSTRAINT pk_tb_perfil PRIMARY KEY (id_perfil)
 );
---
+++ database/2.7.0/perfil_localidades/07_atualizar_perfil.sql
@@ -0,0 +1,263 @@
+CREATE OR REPLACE FUNCTION portal.atualizar_perfil() RETURNS VOID AS $$ 
+
+DECLARE
+    nome_tipo_localidade_regiao TEXT;
+    nome_tipo_localidade_estado TEXT;
+    nome_tipo_localidade_municipio TEXT;
+    id_perfil INTEGER;
+    nome_perfil TEXT;
+    series_perfil JSONB;
+    fontes_perfil TEXT[];
+
+BEGIN
+    DROP TABLE IF EXISTS portal.tb_perfil;
+
+    nome_tipo_localidade_regiao := 'regiao';
+    nome_tipo_localidade_estado := 'estado';
+    nome_tipo_localidade_municipio := 'municipio';
+
+    /* ==================== Maior média de natureza juridica ==================== */
+    nome_perfil := 'maior_media_natureza_juridica';
+
+    /* Maior média de natureza juridica por região */
+    id_perfil := 1;
+    SELECT INTO series_perfil, fontes_perfil dados, fontes 
+        FROM portal.obter_perfil_maior_media_natureza_juridica(nome_tipo_localidade_regiao);
+
+    INSERT INTO portal.tb_perfil (
+        id,
+        nome,
+        tipo_localidade,
+        series,
+        fontes
+    ) VALUES (
+        id_perfil,
+        nome_perfil,
+        nome_tipo_localidade_regiao,
+        series_perfil,
+        fontes_perfil
+    );
+
+    /* Maior média de natureza juridica por estado */
+    id_perfil := 2;
+    SELECT INTO series_perfil, fontes_perfil dados, fontes 
+        FROM portal.obter_perfil_maior_media_natureza_juridica(nome_tipo_localidade_estado);
+
+    INSERT INTO portal.tb_perfil (
+        id,
+        nome,
+        tipo_localidade,
+        series,
+        fontes
+    ) VALUES (
+        id_perfil,
+        nome_perfil,
+        nome_tipo_localidade_estado,
+        series_perfil,
+        fontes_perfil
+    );
+
+    /* Maior média de natureza juridica por município */
+    id_perfil := 3;
+    SELECT INTO series_perfil, fontes_perfil dados, fontes 
+        FROM portal.obter_perfil_maior_media_natureza_juridica(nome_tipo_localidade_municipio);
+
+    INSERT INTO portal.tb_perfil (
+        id,
+        nome,
+        tipo_localidade,
+        series,
+        fontes
+    ) VALUES (
+        id_perfil,
+        nome_perfil,
+        nome_tipo_localidade_municipio,
+        series_perfil,
+        fontes_perfil
+    );
+
+    /* ==================== Maior média de repasses ==================== */
+    nome_perfil := 'maior_media_repasses';
+
+    /* Maior média de repasses por região */
+    id_perfil := 4;
+    SELECT INTO series_perfil, fontes_perfil dados, fontes 
+        FROM portal.obter_perfil_maior_media_repasse(nome_tipo_localidade_regiao);
+
+    INSERT INTO portal.tb_perfil (
+        id,
+        nome,
+        tipo_localidade,
+        series,
+        fontes
+    ) VALUES (
+        id_perfil,
+        nome_perfil,
+        nome_tipo_localidade_regiao,
+        series_perfil,
+        fontes_perfil
+    );
+
+    /* Maior média de repasses por estado */
+    id_perfil := 5;
+    SELECT INTO series_perfil, fontes_perfil dados, fontes 
+        FROM portal.obter_perfil_maior_media_repasse(nome_tipo_localidade_estado);
+
+    INSERT INTO portal.tb_perfil (
+        id,
+        nome,
+        tipo_localidade,
+        series,
+        fontes
+    ) VALUES (
+        id_perfil,
+        nome_perfil,
+        nome_tipo_localidade_estado,
+        series_perfil,
+        fontes_perfil
+    );
+
+    /* Maior média de repasses por município */
+    id_perfil := 6;
+    SELECT INTO series_perfil, fontes_perfil dados, fontes 
+        FROM portal.obter_perfil_maior_media_repasse(nome_tipo_localidade_municipio);
+
+    INSERT INTO portal.tb_perfil (
+        id,
+        nome,
+        tipo_localidade,
+        series,
+        fontes
+    ) VALUES (
+        id_perfil,
+        nome_perfil,
+        nome_tipo_localidade_municipio,
+        series_perfil,
+        fontes_perfil
+    );
+
+    /* ==================== Maior média de área de atuação ==================== */
+    nome_perfil := 'maior_media_area_atuacao';
+
+    /* Maior média de área de atuação por região */
+    id_perfil := 7;
+    SELECT INTO series_perfil, fontes_perfil dados, fontes 
+        FROM portal.obter_perfil_maior_media_area_atuacao(nome_tipo_localidade_regiao);
+
+    INSERT INTO portal.tb_perfil (
+        id,
+        nome,
+        tipo_localidade,
+        series,
+        fontes
+    ) VALUES (
+        id_perfil,
+        nome_perfil,
+        nome_tipo_localidade_regiao,
+        series_perfil,
+        fontes_perfil
+    );
+
+    /* Maior média de área de atuação por estado */
+    id_perfil := 8;
+    SELECT INTO series_perfil, fontes_perfil dados, fontes 
+        FROM portal.obter_perfil_maior_media_area_atuacao(nome_tipo_localidade_estado);
+
+    INSERT INTO portal.tb_perfil (
+        id,
+        nome,
+        tipo_localidade,
+        series,
+        fontes
+    ) VALUES (
+        id_perfil,
+        nome_perfil,
+        nome_tipo_localidade_estado,
+        series_perfil,
+        fontes_perfil
+    );
+
+    /* Maior média de área de atuação por município */
+    id_perfil := 9;
+    SELECT INTO series_perfil, fontes_perfil dados, fontes 
+        FROM portal.obter_perfil_maior_media_area_atuacao(nome_tipo_localidade_municipio);
+
+    INSERT INTO portal.tb_perfil (
+        id,
+        nome,
+        tipo_localidade,
+        series,
+        fontes
+    ) VALUES (
+        id_perfil,
+        nome_perfil,
+        nome_tipo_localidade_municipio,
+        series_perfil,
+        fontes_perfil
+    );
+
+    /* ==================== Maior média de trabalhadores ==================== */
+    nome_perfil := 'maior_media_trabalhadores';
+
+    /* Maior média de trabalhadores por região */
+    id_perfil := 10;
+    SELECT INTO series_perfil, fontes_perfil dados, fontes 
+        FROM portal.obter_perfil_maior_media_trabalhadores(nome_tipo_localidade_regiao);
+
+    INSERT INTO portal.tb_perfil (
+        id,
+        nome,
+        tipo_localidade,
+        series,
+        fontes
+    ) VALUES (
+        id_perfil,
+        nome_perfil,
+        nome_tipo_localidade_regiao,
+        series_perfil,
+        fontes_perfil
+    );
+
+    /* Maior média de trabalhadores por estado */
+    id_perfil := 11;
+    SELECT INTO series_perfil, fontes_perfil dados, fontes 
+        FROM portal.obter_perfil_maior_media_trabalhadores(nome_tipo_localidade_estado);
+
+    INSERT INTO portal.tb_perfil (
+        id,
+        nome,
+        tipo_localidade,
+        series,
+        fontes
+    ) VALUES (
+        id_perfil,
+        nome_perfil,
+        nome_tipo_localidade_estado,
+        series_perfil,
+        fontes_perfil
+    );
+
+    /* Maior média de trabalhadores por município */
+    id_perfil := 12;
+    SELECT INTO series_perfil, fontes_perfil dados, fontes 
+        FROM portal.obter_perfil_maior_media_trabalhadores(nome_tipo_localidade_municipio);
+
+    INSERT INTO portal.tb_perfil (
+        id,
+        nome,
+        tipo_localidade,
+        series,
+        fontes
+    ) VALUES (
+        id_perfil,
+        nome_perfil,
+        nome_tipo_localidade_municipio,
+        series_perfil,
+        fontes_perfil
+    );
+
+END;
+
+$$ LANGUAGE 'plpgsql';
+
+SELECT * FROM portal.obter_perfil_maior_media_trabalhadores('regiao'::TEXT);
