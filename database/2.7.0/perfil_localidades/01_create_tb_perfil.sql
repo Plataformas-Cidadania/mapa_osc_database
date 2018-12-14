17e4f36da10690c925fab325de6d3a327ddf22ed
vagnerpraia <vagnerpraia@gmail.com>
2018-12-14 03:52:05 -0200

    Atualização da maior média de área de atuação

--- database/2.7.0/perfil_localidades/01_create_tb_perfil.sql
+++ database/2.7.0/perfil_localidades/01_create_tb_perfil.sql
@@ -14,23 +14,20 @@ CREATE TABLE portal.tb_perfil
     nr_porcentagem_maior_media_natureza_juridica_estado NUMERIC,
     tx_maior_media_nacional_natureza_juridica_municipio TEXT[],
     nr_porcentagem_maior_media_natureza_juridica_municipio NUMERIC,
+
     nr_media_nacional_repasse_regiao NUMERIC,
     nr_media_nacional_repasse_estado NUMERIC,
     nr_media_nacional_repasse_municipio NUMERIC,
+
     tx_maior_media_nacional_area_atuacao_regiao TEXT[],
     nr_porcentagem_maior_media_nacional_area_atuacao_regiao NUMERIC,
     tx_maior_media_nacional_area_atuacao_estado TEXT[],
     nr_porcentagem_maior_media_nacional_area_atuacao_estado NUMERIC,
     tx_maior_media_nacional_area_atuacao_municipio TEXT[],
     nr_porcentagem_maior_media_nacional_area_atuacao_municipio NUMERIC,
-    tx_porcentagem_maior_media_nacional_area_atuacao_regiao NUMERIC,
-    tx_porcentagem_maior_media_nacional_area_atuacao_estado NUMERIC,
-    tx_porcentagem_maior_media_nacional_area_atuacao_municipio NUMERIC,
+
     nr_porcentagem_maior_media_nacional_trabalhadores_regiao NUMERIC,
     nr_porcentagem_maior_media_nacional_trabalhadores_estado NUMERIC,
-    nr_porcentagem_maior_media_nacional_trabalhadores_municipio NUMERIC,
-    tx_porcentagem_maior_media_nacional_trabalhadores_regiao NUMERIC,
-    tx_porcentagem_maior_media_nacional_trabalhadores_estado NUMERIC,
-    tx_porcentagem_maior_media_nacional_trabalhadores_municipio NUMERIC
+    nr_porcentagem_maior_media_nacional_trabalhadores_municipio NUMERIC
     */
 );
--- database/2.7.0/perfil_localidades/03_obter_perfil_maior_media_natureza_juridica.sql
+++ database/2.7.0/perfil_localidades/03_obter_perfil_maior_media_natureza_juridica.sql
@@ -10,12 +10,13 @@ BEGIN
 		WHEN tipo_localidade = 'regiao' THEN
         	RETURN QUERY
 				SELECT
-					('{"localidades": [' || REPLACE(REPLACE(RTRIM(LTRIM(ARRAY_AGG(b.nm_localidade)::TEXT, '{'), '}'), '\', ''), '""', '"') || '], "maior_media": ' || b.media::TEXT || '}')::JSONB AS dados,
+					('{"localidades": [' || REPLACE(REPLACE(RTRIM(LTRIM(ARRAY_AGG(b.nm_localidade)::TEXT, '{'), '}'), '\', ''), '""', '"') || '], "natureza_juridica": ' || b.natureza_juridica::TEXT || ', "maior_media": ' || b.media::TEXT || '}')::JSONB AS dados,
 					b.fontes::TEXT[] AS fontes
 				FROM 
 				(
 					SELECT
 						'"' || a.nm_localidade || '"' AS nm_localidade,
+						'"' || a.natureza_juridica || '"' AS natureza_juridica,
 						MAX(a.quantidade) / SUM(a.quantidade) * 100 AS media,
 						(
 							SELECT ARRAY_AGG(TRANSLATE(a::TEXT, '()\"', '')) FROM (SELECT DISTINCT UNNEST(
@@ -25,7 +26,7 @@ BEGIN
 					FROM (
 						SELECT
 							edre_nm_regiao AS nm_localidade,
-							cd_natureza_juridica_osc,
+							tx_nome_natureza_juridica AS natureza_juridica,
 							COUNT(*) AS quantidade,
 							(
 								SELECT ARRAY_AGG(TRANSLATE(a::TEXT, '()', '')) FROM (SELECT DISTINCT UNNEST(
@@ -40,25 +41,27 @@ BEGIN
 						ON tb_dados_gerais.id_osc = tb_localizacao.id_osc
 						LEFT JOIN spat.ed_regiao
 						ON ed_regiao.edre_cd_regiao = SUBSTRING(tb_localizacao.cd_municipio::TEXT from 1 for 1)::NUMERIC(1, 0)
+						LEFT JOIN syst.dc_natureza_juridica
+						ON tb_dados_gerais.cd_natureza_juridica_osc = dc_natureza_juridica.cd_natureza_juridica
 						WHERE cd_natureza_juridica_osc IS NOT null
-						GROUP BY cd_natureza_juridica_osc, edre_nm_regiao
-						ORDER BY quantidade DESC
+						GROUP BY tx_nome_natureza_juridica, edre_nm_regiao
 					) AS a
-					GROUP BY a.nm_localidade
+					GROUP BY a.nm_localidade, a.natureza_juridica
 				) AS b
-				GROUP BY b.media, b.fontes
+				GROUP BY b.natureza_juridica, b.media, b.fontes
 				ORDER BY b.media DESC
 				LIMIT 1;
 
 		WHEN tipo_localidade = 'estado' THEN
         	RETURN QUERY
 				SELECT
-					('{"localidades": [' || REPLACE(REPLACE(RTRIM(LTRIM(ARRAY_AGG(b.nm_localidade)::TEXT, '{'), '}'), '\', ''), '""', '"') || '], "maior_media": ' || b.media::TEXT || '}')::JSONB AS dados,
+					('{"localidades": [' || REPLACE(REPLACE(RTRIM(LTRIM(ARRAY_AGG(b.nm_localidade)::TEXT, '{'), '}'), '\', ''), '""', '"') || '], "natureza_juridica": ' || b.natureza_juridica::TEXT || ', "maior_media": ' || b.media::TEXT || '}')::JSONB AS dados,
 					b.fontes::TEXT[] AS fontes
 				FROM 
 				(
 					SELECT
 						'"' || a.nm_localidade || '"' AS nm_localidade,
+						'"' || a.natureza_juridica || '"' AS natureza_juridica,
 						MAX(a.quantidade) / SUM(a.quantidade) * 100 AS media,
 						(
 							SELECT ARRAY_AGG(TRANSLATE(a::TEXT, '()\"', '')) FROM (SELECT DISTINCT UNNEST(
@@ -68,7 +71,7 @@ BEGIN
 					FROM (
 						SELECT
 							eduf_nm_uf AS nm_localidade,
-							cd_natureza_juridica_osc,
+							tx_nome_natureza_juridica AS natureza_juridica,
 							COUNT(*) AS quantidade,
 							(
 								SELECT ARRAY_AGG(TRANSLATE(a::TEXT, '()', '')) FROM (SELECT DISTINCT UNNEST(
@@ -83,25 +86,28 @@ BEGIN
 						ON tb_dados_gerais.id_osc = tb_localizacao.id_osc
 						LEFT JOIN spat.ed_uf
 						ON ed_uf.eduf_cd_uf = SUBSTRING(tb_localizacao.cd_municipio::TEXT from 1 for 2)::NUMERIC(2, 0)
+						LEFT JOIN syst.dc_natureza_juridica
+						ON tb_dados_gerais.cd_natureza_juridica_osc = dc_natureza_juridica.cd_natureza_juridica
 						WHERE cd_natureza_juridica_osc IS NOT null
-						GROUP BY cd_natureza_juridica_osc, eduf_nm_uf
+						GROUP BY tx_nome_natureza_juridica, eduf_nm_uf
 						ORDER BY quantidade DESC
 					) AS a
-					GROUP BY a.nm_localidade
+					GROUP BY a.nm_localidade, a.natureza_juridica
 				) AS b
-				GROUP BY b.media, b.fontes
+				GROUP BY b.natureza_juridica, b.media, b.fontes
 				ORDER BY b.media DESC
 				LIMIT 1;
 
 		WHEN tipo_localidade = 'municipio' THEN
         	RETURN QUERY
 				SELECT
-					('{"localidades": [' || REPLACE(REPLACE(RTRIM(LTRIM(ARRAY_AGG(b.nm_localidade)::TEXT, '{'), '}'), '\', ''), '""', '"') || '], "maior_media": ' || b.media::TEXT || '}')::JSONB AS dados,
+					('{"localidades": [' || REPLACE(REPLACE(RTRIM(LTRIM(ARRAY_AGG(b.nm_localidade)::TEXT, '{'), '}'), '\', ''), '""', '"') || '], "natureza_juridica": ' || b.natureza_juridica::TEXT || ', "maior_media": ' || b.media::TEXT || '}')::JSONB AS dados,
 					b.fontes::TEXT[] AS fontes
 				FROM 
 				(
 					SELECT
 						'"' || a.nm_localidade || '"' AS nm_localidade,
+						'"' || a.natureza_juridica || '"' AS natureza_juridica,
 						MAX(a.quantidade) / SUM(a.quantidade) * 100 AS media,
 						(
 							SELECT ARRAY_AGG(TRANSLATE(a::TEXT, '()\"', '')) FROM (SELECT DISTINCT UNNEST(
@@ -111,7 +117,7 @@ BEGIN
 					FROM (
 						SELECT
 							edmu_nm_municipio || ' - ' || eduf_sg_uf AS nm_localidade,
-							cd_natureza_juridica_osc,
+							tx_nome_natureza_juridica AS natureza_juridica,
 							COUNT(*) AS quantidade,
 							(
 								SELECT ARRAY_AGG(TRANSLATE(a::TEXT, '()', '')) FROM (SELECT DISTINCT UNNEST(
@@ -128,13 +134,15 @@ BEGIN
 						ON tb_localizacao.cd_municipio = ed_municipio.edmu_cd_municipio
 						LEFT JOIN spat.ed_uf
 						ON ed_municipio.eduf_cd_uf = ed_uf.eduf_cd_uf
+						LEFT JOIN syst.dc_natureza_juridica
+						ON tb_dados_gerais.cd_natureza_juridica_osc = dc_natureza_juridica.cd_natureza_juridica
 						WHERE cd_natureza_juridica_osc IS NOT null
-						GROUP BY cd_natureza_juridica_osc, edmu_nm_municipio, eduf_sg_uf
+						GROUP BY tx_nome_natureza_juridica, edmu_nm_municipio, eduf_sg_uf
 						ORDER BY quantidade DESC
 					) AS a
-					GROUP BY a.nm_localidade
+					GROUP BY a.nm_localidade, a.natureza_juridica
 				) AS b
-				GROUP BY b.media, b.fontes
+				GROUP BY b.natureza_juridica, b.media, b.fontes
 				ORDER BY b.media DESC
 				LIMIT 1;
 
