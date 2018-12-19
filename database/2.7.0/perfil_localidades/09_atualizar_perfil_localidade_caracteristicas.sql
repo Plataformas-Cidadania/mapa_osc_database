3e74e45edb4e7300c6268263ca3466c265a69012
vagnerpraia <vagnerpraia@gmail.com>
2018-12-19 17:34:21 -0200

    Adição do carregar características

--- database/2.7.0/perfil_localidades/09_atualizar_perfil_localidade_caracteristicas.sql
+++ database/2.7.0/perfil_localidades/09_atualizar_perfil_localidade_caracteristicas.sql
@@ -1,65 +1,26 @@
-/*
-DELETE FROM portal.tb_perfil_localidade;
-
-INSERT INTO portal.tb_perfil_localidade (id_osc)
-VALUES (
-    SELECT id_osc
-    FROM osc.tb_osc
-    WHERE tb_osc.bo_osc_ativa
-    AND id_osc <> 789809;
-);
-*/
-
 DROP FUNCTION IF EXISTS portal.atualizar_perfil_localidade_caracteristicas() CASCADE;
 
 CREATE OR REPLACE FUNCTION portal.atualizar_perfil_localidade_caracteristicas() RETURNS VOID AS $$ 
 
 DECLARE
-    id_perfil := 1;
+    id_perfil INTEGER := 1;
+    localidade RECORD;
+    caracteristicas_localidade JSONB;
+    fontes_caracteristicas_localidade TEXT[];
 
 BEGIN
     FOR localidade IN
-        SELECT * FROM tb_perfil_localidade ORDER BY id
+        SELECT * 
+            FROM portal.tb_perfil_localidade 
+            ORDER BY id
     LOOP
-        INSERT INTO 
-            portal.tb_perfil_localidade(
-                id,
-                id_localidade,
-                tx_localidade,
-                tx_tipo_localidade,
-                nr_colocacao_quantidade_osc_regiao,
-                nr_colocacao_quantidade_osc_estado,
-                nr_colocacao_quantidade_osc_municipio,
-                nr_maior_media_natureza_juridica_regiao,
-                nr_maior_media_natureza_juridica_estado,
-                nr_maior_media_natureza_juridica_municipio,
-                tx_maior_media_natureza_juridica_regiao,
-                tx_maior_media_natureza_juridica_estado,
-                tx_maior_media_natureza_juridica_municipio,
-                nr_colocacao_repasses_regiao,
-                nr_colocacao_repasses_estado,
-                nr_colocacao_repasses_municipio,
-                nr_media_repasse_regiao,
-                nr_media_repasse_estado,
-                nr_media_repasse_municipio,
-                nr_maior_media_area_atuacao_regiao,
-                nr_maior_media_area_atuacao_estado,
-                nr_maior_media_area_atuacao_municipio,
-                tx_maior_media_area_atuacao_regiao,
-                tx_maior_media_area_atuacao_estado,
-                tx_maior_media_area_atuacao_municipio,
-                nr_maior_media_trabalhadores_regiao,
-                nr_maior_media_trabalhadores_estado,
-                nr_maior_media_trabalhadores_municipio,
-                tx_maior_media_trabalhadores_regiao,
-                tx_maior_media_trabalhadores_estado,
-                tx_maior_media_trabalhadores_municipio,
-            ) VALUES (
-                id_perfil,
-                localidade.id_localidade,
-                localidade.nome_localidade,
-                localidade.tipo_localidade,
-            );
+        SELECT INTO caracteristicas_localidade, fontes_caracteristicas_localidade dados, fontes
+            FROM portal.obter_perfil_localidade_caracteristicas(localidade.id_localidade);
+
+        UPDATE portal.tb_perfil_localidade
+            SET caracteristicas = caracteristicas_localidade,
+                ft_caracteristicas = fontes_caracteristicas_localidade
+            WHERE id = localidade.id;
 
         id_perfil := id_perfil + 1;
     END LOOP;
@@ -68,4 +29,4 @@ END;
 
 $$ LANGUAGE 'plpgsql';
 
-SELECT * FROM portal.atualizar_localidade_perfil_localidade();
+SELECT * FROM portal.atualizar_perfil_localidade_caracteristicas();
