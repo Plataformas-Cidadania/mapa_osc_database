a672fbf2ae1d6a56f631b134cc821fd9d3603aca
vagnerpraia <vagnerpraia@gmail.com>
2019-01-09 16:58:29 -0200

    Adição da média de natureza jurídica

--- database/2.7.0/perfil_localidades/20_obter_media_natureza_juridica.sql
+++ database/2.7.0/perfil_localidades/20_obter_media_natureza_juridica.sql
@@ -4,12 +4,13 @@ CREATE OR REPLACE FUNCTION portal.atualizar_perfil_localidade_medias_natureza_ju
 
 DECLARE
 	localidade RECORD;
+	series JSONB;
 	dados JSONB;
-	atualizado JSONB := '[]'::JSONB;
-	quantidade_osc INTEGER := 0;
-	media DOUBLE PRECISION := 0;
-	natureza_juridica_maior_media TEXT := '';
-	maior_media DOUBLE PRECISION := 0;
+	atualizado JSONB;
+	quantidade_osc_localidade INTEGER;
+	media_localidade DOUBLE PRECISION;
+	natureza_juridica_maior_media_localidade TEXT;
+	maior_media_localidade DOUBLE PRECISION;
 
 BEGIN
 	FOR localidade IN
@@ -18,45 +19,51 @@ BEGIN
 		WHERE tx_tipo_localidade = 'regiao'
 		--OR tx_tipo_localidade = 'estado'
 	LOOP
+		atualizado := '[]'::JSONB;
+		quantidade_osc_localidade := 0;
+		media_localidade := 0;
+		natureza_juridica_maior_media_localidade := '';
+		maior_media_localidade := 0;
+		
+		IF (localidade.natureza_juridica->>'series_1') IS null THEN
+			series := localidade.natureza_juridica;
+		ELSE
+			series := localidade.natureza_juridica->>'series_1';
+		END IF;
+		
 		FOR dados IN 
 			SELECT *
-			FROM jsonb_array_elements(localidade.natureza_juridica)
+			FROM jsonb_array_elements(series)
 		LOOP
-			quantidade_osc := quantidade_osc + (dados->>'nr_quantidade_oscs')::INTEGER;
+			quantidade_osc_localidade := quantidade_osc_localidade + (dados->>'nr_quantidade_oscs')::INTEGER;
 		END LOOP;
 
 		FOR dados IN 
 			SELECT *
-			FROM jsonb_array_elements(localidade.natureza_juridica)
+			FROM jsonb_array_elements(series)
 		LOOP
-			media := (dados->>'nr_quantidade_oscs')::DOUBLE PRECISION / quantidade_osc * 100;
-			dados := dados || ('{"media":' || media::TEXT || '}')::JSONB;
+			media_localidade := (dados->>'nr_quantidade_oscs')::DOUBLE PRECISION / quantidade_osc_localidade::DOUBLE PRECISION * 100;
+			dados := dados || ('{"media":' || media_localidade::TEXT || '}')::JSONB;
 			atualizado := atualizado || dados;
 			
-			IF media >= maior_media THEN
-				natureza_juridica_maior_media := (dados->>'tx_nome_natureza_juridica')::TEXT;
-				maior_media := media;
+			IF media_localidade >= maior_media_localidade THEN
+				natureza_juridica_maior_media_localidade := (dados->>'tx_nome_natureza_juridica')::TEXT;
+				maior_media_localidade := media_localidade;
 			END IF;
 			
 		END LOOP;
 		
-		atualizado := ('{"nr_porcentagem_maior": "' || maior_media::TEXT || '", "tx_porcentagem_maior": "' || natureza_juridica_maior_media::TEXT || '", "series_1": ' || atualizado::TEXT || '}')::JSONB;
+		atualizado := ('{"nr_porcentagem_maior": "' || maior_media_localidade::TEXT || '", "tx_porcentagem_maior": "' || natureza_juridica_maior_media_localidade::TEXT || '", "series_1": ' || atualizado::TEXT || '}')::JSONB;
 		
 		RAISE NOTICE 'Localidade: %', localidade.id_localidade;
 		RAISE NOTICE 'Atualizado: %', atualizado;
-		RAISE NOTICE 'Maior Média Natureza Jurdica: %', natureza_juridica_maior_media;
-		RAISE NOTICE 'Maior Média: %', maior_media;
+		RAISE NOTICE 'Maior Média Natureza Jurdica: %', natureza_juridica_maior_media_localidade;
+		RAISE NOTICE 'Maior Média: %', maior_media_localidade;
 		RAISE NOTICE '';
 		
 		UPDATE portal.tb_perfil_localidade
 		SET natureza_juridica = atualizado
 		WHERE id_localidade = localidade.id_localidade;
-		
-		atualizado := '[]'::JSONB;
-		quantidade_osc := 0;
-		media := 0;
-		natureza_juridica_maior_media := '';
-		maior_media := 0;
 	END LOOP;
 END;
 
