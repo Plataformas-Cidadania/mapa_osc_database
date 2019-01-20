a2a21f9426de4cb42229535ce6f893438b55939b
vagnerpraia <vagnerpraia@gmail.com>
2019-01-20 21:22:20 -0200

    Criação da view com a maior área de atuação

---
+++ database/2.7.0/perfil_localidades/2.0/12_vw_perfil_localidade_maior_area_atuacao.sql
@@ -0,0 +1,93 @@
+DROP MATERIALIZED VIEW IF EXISTS analysis.vw_perfil_localidade_maior_area_atuacao CASCADE;
+CREATE MATERIALIZED VIEW analysis.vw_perfil_localidade_maior_area_atuacao AS 
+
+SELECT
+	a.localidade,
+	ARRAY_AGG(a.area_atuacao),
+	(
+		MAX(a.quantidade_oscs)::DOUBLE PRECISION / 
+		(SELECT SUM(quantidade_oscs) FROM analysis.vw_perfil_localidade_area_atuacao WHERE localidade = a.localidade)::DOUBLE PRECISION 
+		* 100
+	) AS porcertagem_maior,
+	REPLACE(('{' || TRIM(TRANSLATE(
+		ARRAY_AGG(fontes_caracteristicas::TEXT)::TEXT
+	, '"\{}', ''), ',') || '}'), ',,', ',')::TEXT[] AS fontes
+FROM analysis.vw_perfil_localidade_area_atuacao AS a
+RIGHT JOIN (
+	SELECT
+		localidade,
+		MAX(quantidade_oscs) AS quantidade_oscs
+	FROM analysis.vw_perfil_localidade_area_atuacao
+	WHERE (
+		CASE 
+			WHEN SUBSTRING(localidade FROM '[0-9]*') = '' THEN '0'
+			ELSE SUBSTRING(localidade FROM '[0-9]*')
+		END
+	)::INTEGER BETWEEN 1 AND 9
+	GROUP BY localidade
+) AS b
+ON a.localidade = b.localidade
+AND a.quantidade_oscs = b.quantidade_oscs
+GROUP BY a.localidade
+
+UNION
+
+SELECT
+	a.localidade,
+	ARRAY_AGG(a.area_atuacao),
+	(
+		MAX(a.quantidade_oscs)::DOUBLE PRECISION / 
+		(SELECT SUM(quantidade_oscs) FROM analysis.vw_perfil_localidade_area_atuacao WHERE localidade = a.localidade)::DOUBLE PRECISION 
+		* 100
+	) AS porcertagem_maior,
+	REPLACE(('{' || TRIM(TRANSLATE(
+		ARRAY_AGG(fontes_caracteristicas::TEXT)::TEXT
+	, '"\{}', ''), ',') || '}'), ',,', ',')::TEXT[] AS fontes
+FROM analysis.vw_perfil_localidade_area_atuacao AS a
+RIGHT JOIN (
+	SELECT
+		localidade,
+		MAX(quantidade_oscs) AS quantidade_oscs
+	FROM analysis.vw_perfil_localidade_area_atuacao
+	WHERE (
+		CASE 
+			WHEN SUBSTRING(localidade FROM '[0-9]*') = '' THEN '0'
+			ELSE SUBSTRING(localidade FROM '[0-9]*')
+		END
+	)::INTEGER BETWEEN 10 AND 99
+	GROUP BY localidade
+) AS b
+ON a.localidade = b.localidade
+AND a.quantidade_oscs = b.quantidade_oscs
+GROUP BY a.localidade
+
+UNION
+
+SELECT
+	a.localidade,
+	ARRAY_AGG(a.area_atuacao),
+	(
+		MAX(a.quantidade_oscs)::DOUBLE PRECISION / 
+		(SELECT SUM(quantidade_oscs) FROM analysis.vw_perfil_localidade_area_atuacao WHERE localidade = a.localidade)::DOUBLE PRECISION 
+		* 100
+	) AS porcertagem_maior,
+	REPLACE(('{' || TRIM(TRANSLATE(
+		ARRAY_AGG(fontes_caracteristicas::TEXT)::TEXT
+	, '"\{}', ''), ',') || '}'), ',,', ',')::TEXT[] AS fontes
+FROM analysis.vw_perfil_localidade_area_atuacao AS a
+RIGHT JOIN (
+	SELECT
+		localidade,
+		MAX(quantidade_oscs) AS quantidade_oscs
+	FROM analysis.vw_perfil_localidade_area_atuacao
+	WHERE (
+		CASE 
+			WHEN SUBSTRING(localidade FROM '[0-9]*') = '' THEN '0'
+			ELSE SUBSTRING(localidade FROM '[0-9]*')
+		END
+	)::INTEGER > 99
+	GROUP BY localidade
+) AS b
+ON a.localidade = b.localidade
+AND a.quantidade_oscs = b.quantidade_oscs
+GROUP BY a.localidade;
