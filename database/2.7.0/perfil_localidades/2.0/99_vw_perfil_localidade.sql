ec760ddfdf4acbc71a2ddf81ea35986ca63e1f03
vagnerpraia <vagnerpraia@gmail.com>
2019-01-21 19:40:11 -0200

    Atualização da view do perfil das localidades

--- database/2.7.0/perfil_localidades/2.0/99_vw_perfil_localidade.sql
+++ database/2.7.0/perfil_localidades/2.0/99_vw_perfil_localidade.sql
@@ -1,7 +1,10 @@
 --DROP MATERIALIZED VIEW IF EXISTS analysis.vw_perfil_localidade CASCADE;
 --CREATE MATERIALIZED VIEW analysis.vw_perfil_localidade AS 
 
-SELECT 
+SELECT
+	a.localidade AS id_localidade,
+	a.nome_localidade AS tx_localidade,
+	a.tipo_localidade AS tx_tipo_localidade,
 	(
 		SELECT row_to_json(a)
 		FROM (
@@ -61,6 +64,23 @@ SELECT
 			WHERE localidade = a.localidade
 		) AS a
 	) AS area_atuacao,
+	/*
+	(
+		SELECT json_agg(row_to_json(a))
+		FROM (
+			SELECT
+				b.area_atuacao AS tx_porcentagem_maior,
+				b.porcertagem_maior AS nr_porcentagem_maior,
+				a.area_atuacao,
+				a.quantidade_oscs,
+				a.fontes
+			FROM analysis.vw_perfil_localidade_area_atuacao AS a
+			INNER JOIN analysis.vw_perfil_localidade_maior_area_atuacao AS b
+			ON a.localidade = b.localidade
+			WHERE localidade = a.localidade
+		) AS a
+	) AS area_atuacao,
+	*/
 	(
 		SELECT json_agg(row_to_json(a))
 		FROM (
