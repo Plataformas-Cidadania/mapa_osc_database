4350a631e8bc7e0a1d4092947e6cc42fb47c745e
vagnerpraia <vagnerpraia@gmail.com>
2019-01-20 23:40:09 -0200

    Ajuste na view de trabalhadores

--- database/2.7.0/perfil_localidades/2.0/07_vw_perfil_localidade_trabalhadores.sql
+++ database/2.7.0/perfil_localidades/2.0/07_vw_perfil_localidade_trabalhadores.sql
@@ -2,7 +2,7 @@ DROP MATERIALIZED VIEW IF EXISTS analysis.vw_perfil_localidade_trabalhadores CAS
 CREATE MATERIALIZED VIEW analysis.vw_perfil_localidade_trabalhadores AS 
 
 SELECT 
-	COALESCE(SUBSTR(tb_localizacao.cd_municipio::TEXT, 1, 1), 'Sem informação') AS localidade,
+	SUBSTR(tb_localizacao.cd_municipio::TEXT, 1, 1) AS localidade,
 	SUM(COALESCE(tb_relacoes_trabalho.nr_trabalhadores_vinculo, 0)) AS vinculos,
 	SUM(COALESCE(tb_relacoes_trabalho.nr_trabalhadores_deficiencia, 0)) AS deficiencia,
 	SUM(COALESCE(tb_relacoes_trabalho.nr_trabalhadores_voluntarios, 0)) AS voluntarios,
@@ -36,6 +36,7 @@ LEFT JOIN osc.tb_localizacao
 ON tb_osc.id_osc = tb_localizacao.id_osc
 WHERE tb_osc.bo_osc_ativa
 AND tb_osc.id_osc <> 789809
+AND tb_localizacao.cd_municipio IS NOT NULL
 GROUP BY localidade
 
 UNION
@@ -75,6 +76,7 @@ LEFT JOIN osc.tb_localizacao
 ON tb_osc.id_osc = tb_localizacao.id_osc
 WHERE tb_osc.bo_osc_ativa
 AND tb_osc.id_osc <> 789809
+AND tb_localizacao.cd_municipio IS NOT NULL
 GROUP BY localidade
 
 UNION
@@ -114,4 +116,5 @@ LEFT JOIN osc.tb_localizacao
 ON tb_osc.id_osc = tb_localizacao.id_osc
 WHERE tb_osc.bo_osc_ativa
 AND tb_osc.id_osc <> 789809
+AND tb_localizacao.cd_municipio IS NOT NULL
 GROUP BY localidade;
