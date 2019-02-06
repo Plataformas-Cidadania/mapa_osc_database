643665606f156e69b05e45440ea79a9732cdbea9
vagnerpraia <vagnerpraia@gmail.com>
2019-02-06 18:59:07 -0200

    Serapação dos rank por tipo de região

--- database/2.7.0/perfil_localidades/08_vw_perfil_localidade_ranking_quantidade_osc.sql
+++ database/2.7.0/perfil_localidades/08_vw_perfil_localidade_ranking_quantidade_osc.sql
@@ -1,11 +1,48 @@
 DROP MATERIALIZED VIEW IF EXISTS analysis.vw_perfil_localidade_ranking_quantidade_osc CASCADE;
 CREATE MATERIALIZED VIEW analysis.vw_perfil_localidade_ranking_quantidade_osc AS 
 
-SELECT 
+SELECT
 	localidade,
 	quantidade_oscs,
-	RANK() over (ORDER BY quantidade_oscs DESC) as rank
-FROM analysis.vw_perfil_localidade_caracteristicas;
+	RANK() over (ORDER BY quantidade_oscs DESC) as rank,
+	'regiao' AS tipo_rank
+FROM analysis.vw_perfil_localidade_caracteristicas
+WHERE (
+	CASE 
+		WHEN SUBSTRING(localidade FROM '[0-9]*') = '' THEN '0'
+		ELSE SUBSTRING(localidade FROM '[0-9]*')
+	END
+)::INTEGER BETWEEN 1 AND 9
+
+UNION
+
+SELECT
+	localidade,
+	quantidade_oscs,
+	RANK() over (ORDER BY quantidade_oscs DESC) as rank,
+	'estado' AS tipo_rank
+FROM analysis.vw_perfil_localidade_caracteristicas
+WHERE (
+	CASE 
+		WHEN SUBSTRING(localidade FROM '[0-9]*') = '' THEN '0'
+		ELSE SUBSTRING(localidade FROM '[0-9]*')
+	END
+)::INTEGER BETWEEN 10 AND 99
+
+UNION
+
+SELECT
+	localidade,
+	quantidade_oscs,
+	RANK() over (ORDER BY quantidade_oscs DESC) as rank,
+	'municipio' AS tipo_rank
+FROM analysis.vw_perfil_localidade_caracteristicas
+WHERE (
+	CASE 
+		WHEN SUBSTRING(localidade FROM '[0-9]*') = '' THEN '0'
+		ELSE SUBSTRING(localidade FROM '[0-9]*')
+	END
+)::INTEGER > 99;
 
 CREATE INDEX ix_localidade_vw_perfil_localidade_ranking_quantidade_osc
     ON analysis.vw_perfil_localidade_ranking_quantidade_osc USING btree
