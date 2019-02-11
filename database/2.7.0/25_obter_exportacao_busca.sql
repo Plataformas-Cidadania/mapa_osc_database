3ab9191d5071bf6d43fe58d7bc929cc8e0b19620
vagnerpraia <vagnerpraia@gmail.com>
2019-02-11 16:14:00 -0200

    Acerto na função de exportação de busca

--- database/2.7.0/25_obter_exportacao_busca.sql
+++ database/2.7.0/25_obter_exportacao_busca.sql
@@ -16,8 +16,7 @@ BEGIN
 
 	FOREACH indice IN ARRAY variaveis_adicionais
 	LOOP
-		SELECT INTO apelido
-			regexp_replace(reverse(split_part(reverse(tx_nome_indice), ' - ', 1)), '^[0-9]*', '')
+		SELECT INTO apelido tx_sigla
 		FROM ipeadata.tb_indice
 		WHERE cd_indice = indice;
 
@@ -26,9 +25,38 @@ BEGIN
 				SELECT nr_valor AS ' || apelido || '
 				FROM ipeadata.tb_ipeadata
 				WHERE cd_indice = ' || indice::TEXT || '
+				AND cd_municipio = d.cd_municipio
 			)';
 		END IF;
 	END LOOP;
+	
+	RAISE NOTICE '%', '
+		SELECT to_json(array_agg(row_to_json(a)))
+		FROM (
+			SELECT
+				a.id_osc AS id_osc,
+				a.tx_razao_social_osc AS tx_razao_social,
+				b.tx_nome_natureza_juridica AS tx_natureza_juridica,
+				c.tx_nome_classe_atividade_economica AS tx_classe_atividade_economica,
+				d.edmu_nm_municipio AS tx_municipio,
+				d.eduf_nm_uf AS tx_estado
+				' || colunas_adicionais || '
+			FROM osc.tb_dados_gerais AS a
+			LEFT JOIN syst.dc_natureza_juridica AS b
+			ON a.cd_natureza_juridica_osc = b.cd_natureza_juridica
+			LEFT JOIN syst.dc_classe_atividade_economica AS c
+			ON a.cd_classe_atividade_economica_osc = c.cd_classe_atividade_economica
+			LEFT JOIN (
+				osc.tb_localizacao AS a
+				INNER JOIN spat.ed_municipio AS b
+				ON a.cd_municipio = b.edmu_cd_municipio
+				INNER JOIN spat.ed_uf AS c
+				ON b.eduf_cd_uf = c.eduf_cd_uf
+			) AS d
+			ON a.id_osc = d.id_osc
+			WHERE a.id_osc = ANY(''{789809, 394905}''::INTEGER[])
+		) AS a
+	';
 	
 	EXECUTE '
 		SELECT to_json(array_agg(row_to_json(a)))
@@ -72,10 +100,9 @@ BEGIN
 	
 EXCEPTION
 	WHEN others THEN
+		RAISE NOTICE '%', SQLERRM;
 		codigo := 400;
 		SELECT INTO mensagem a.mensagem FROM portal.verificar_erro(SQLSTATE, SQLERRM, null, null, null, false, null) AS a;
 		RETURN NEXT;
 END;
 $$ LANGUAGE 'plpgsql';
-
-SELECT * FROM portal.obter_exportacao_busca('{1221321, 987654}'::INTEGER[], '{1, 8}'::INTEGER[]);
