0b28a27a5fcdeeda66f9bf723b4fb5e342193266
vagnerpraia <vagnerpraia@gmail.com>
2019-03-25 17:14:08 -0300

    Acerto no obter gráfico 12

--- database/2.7.0/12_obter_grafico_osc_titulos_certificados.sql
+++ database/2.7.0/12_obter_grafico_osc_titulos_certificados.sql
@@ -9,11 +9,14 @@ BEGIN
 	RETURN QUERY 
 		SELECT 
 			('[{' || RTRIM(LTRIM(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE((TRANSLATE(ARRAY_AGG('{"label": "' || a.rotulo::TEXT || '", "value": ' || a.valor::TEXT || '}')::TEXT, '\', '') || '}'), '""', '"'), '}",', '},'), '"}', '}'), '"{', '{'), ',,', ','), '{'), '}') || '}]')::JSONB AS dados, 
-			(
-				SELECT ARRAY_AGG(TRANSLATE(a::TEXT, '()\"', '')) FROM (SELECT DISTINCT UNNEST(
-					TRANSLATE(ARRAY_AGG(REPLACE(REPLACE(REPLACE(TRIM(TRIM(TRANSLATE(a.fontes::TEXT, '"\{}', ''), ' '), ','), '","', ','), ', ,', ','), ',,', ','))::TEXT, '"', '')::TEXT[]
-				)) AS a
-			) AS fontes 
+			('{' || TRIM(REPLACE(TRANSLATE((
+				SELECT ARRAY_AGG(TRANSLATE(a::TEXT, '()', '')) 
+				FROM (
+					SELECT DISTINCT UNNEST( 
+						('{' || TRIM(REPLACE(TRANSLATE(ARRAY_AGG(a.fontes)::TEXT, '\"', ''), ',,', ','), ',{}') || '}')::TEXT[] 
+					)
+				) AS a
+			)::TEXT, '\"', ''), ',,', ','), ',{}') || '}')::TEXT[] AS fontes
 		FROM (
 			SELECT 
 				CASE 
@@ -27,13 +30,9 @@ BEGIN
 				END AS rotulo, 
 				COUNT(*) AS valor, 
 				(
-						SELECT ARRAY_AGG(TRANSLATE(a::TEXT, '()', '')) FROM (SELECT DISTINCT UNNEST(
-							ARRAY_CAT(
-								ARRAY_AGG(DISTINCT REPLACE(COALESCE(tb_certificado.ft_certificado, ''), '${ETL}', '')), 
-								ARRAY_AGG(DISTINCT REPLACE(COALESCE(tb_osc.ft_identificador_osc, ''), '${ETL}', ''))
-							)
-						)) AS a
-					) AS fontes 
+					TRIM(ARRAY_AGG(DISTINCT TRIM(tb_certificado.ft_certificado)) FILTER (WHERE (TRIM(tb_certificado.ft_certificado) = '') IS false)::TEXT, '{}') || ',' || 
+					TRIM(ARRAY_AGG(DISTINCT TRIM(tb_osc.ft_identificador_osc)) FILTER (WHERE (TRIM(tb_osc.ft_identificador_osc) = '') IS false)::TEXT, '{}')
+				) AS fontes 
 			FROM osc.tb_osc 
 			LEFT JOIN osc.tb_certificado 
 			ON tb_osc.id_osc = tb_certificado.id_osc 
