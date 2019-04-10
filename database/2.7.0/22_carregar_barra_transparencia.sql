8fdd5a03056f6150c526fa57b52062c2679aca48
vagnerpraia <vagnerpraia@gmail.com>
2019-04-10 16:34:30 -0300

    Otimização do script de carregamento da barra de transparência

--- database/2.7.0/22_carregar_barra_transparencia.sql
+++ database/2.7.0/22_carregar_barra_transparencia.sql
@@ -21,8 +21,6 @@ DECLARE
 	pontuacao_fontes_recursos DOUBLE PRECISION;
 
 BEGIN 
-	DELETE FROM portal.tb_barra_transparencia;
-
 	peso_dados_gerais := (SELECT peso_secao FROM portal.tb_peso_barra_transparencia WHERE id_peso_barra_transparencia = 1);
 	peso_area_atuacao := (SELECT peso_secao FROM portal.tb_peso_barra_transparencia WHERE id_peso_barra_transparencia = 2);
 	peso_descricao := (SELECT peso_secao FROM portal.tb_peso_barra_transparencia WHERE id_peso_barra_transparencia = 3);
@@ -35,87 +33,89 @@ BEGIN
 	FOR osc IN
         SELECT * FROM osc.tb_osc 
     LOOP
-		pontuacao_dados_gerais := (SELECT pontuacao FROM portal.obter_barra_transparencia_dados_gerais(osc.id_osc));
-		pontuacao_area_atuacao := (SELECT pontuacao FROM portal.obter_barra_transparencia_area_atuacao(osc.id_osc));
-		pontuacao_descricao := (SELECT pontuacao FROM portal.obter_barra_transparencia_descricao(osc.id_osc));
-		pontuacao_titulos_certificacoes := (SELECT pontuacao FROM portal.obter_barra_transparencia_titulos_certificacoes(osc.id_osc));
-		pontuacao_relacoes_trabalho_governanca := (SELECT pontuacao FROM portal.obter_barra_transparencia_relacoes_trabalho_governanca(osc.id_osc));
-		pontuacao_espacos_participacao_social := (SELECT pontuacao FROM portal.obter_barra_transparencia_espacos_participacao_social(osc.id_osc));
-		pontuacao_projetos_atividades_programas := (SELECT pontuacao FROM portal.obter_barra_transparencia_projetos_atividades_programas(osc.id_osc));
-		pontuacao_fontes_recursos := (SELECT pontuacao FROM portal.obter_barra_transparencia_fontes_recursos(osc.id_osc));
+		IF (SELECT NOT EXISTS(SELECT 1 FROM portal.tb_barra_transparencia WHERE id_osc = osc.id_osc)) THEN
+			pontuacao_dados_gerais := (SELECT pontuacao FROM portal.obter_barra_transparencia_dados_gerais(osc.id_osc));
+			pontuacao_area_atuacao := (SELECT pontuacao FROM portal.obter_barra_transparencia_area_atuacao(osc.id_osc));
+			pontuacao_descricao := (SELECT pontuacao FROM portal.obter_barra_transparencia_descricao(osc.id_osc));
+			pontuacao_titulos_certificacoes := (SELECT pontuacao FROM portal.obter_barra_transparencia_titulos_certificacoes(osc.id_osc));
+			pontuacao_relacoes_trabalho_governanca := (SELECT pontuacao FROM portal.obter_barra_transparencia_relacoes_trabalho_governanca(osc.id_osc));
+			pontuacao_espacos_participacao_social := (SELECT pontuacao FROM portal.obter_barra_transparencia_espacos_participacao_social(osc.id_osc));
+			pontuacao_projetos_atividades_programas := (SELECT pontuacao FROM portal.obter_barra_transparencia_projetos_atividades_programas(osc.id_osc));
+			pontuacao_fontes_recursos := (SELECT pontuacao FROM portal.obter_barra_transparencia_fontes_recursos(osc.id_osc));
 
-		FOR transparencia IN
-        	SELECT 
-				osc.id_osc AS id_osc, 
-				CAST(pontuacao_dados_gerais AS NUMERIC(7,2)) AS transparencia_dados_gerais, 
-				peso_dados_gerais, 
-				CAST(pontuacao_area_atuacao AS NUMERIC(7,2)) AS transparencia_area_atuacao, 
-				peso_area_atuacao, 
-				CAST(pontuacao_descricao AS NUMERIC(7,2)) AS transparencia_descricao, 
-				peso_descricao, 
-				CAST(pontuacao_titulos_certificacoes AS NUMERIC(7,2)) AS transparencia_titulos_certificacoes, 
-				peso_titulos_certificacoes, 
-				CAST(pontuacao_relacoes_trabalho_governanca AS NUMERIC(7,2)) AS transparencia_relacoes_trabalho_governanca, 
-				peso_relacoes_trabalho_governanca, 
-				CAST(pontuacao_espacos_participacao_social AS NUMERIC(7,2)) AS transparencia_espacos_participacao_social, 
-				peso_espacos_participacao_social, 
-				CAST(pontuacao_projetos_atividades_programas AS NUMERIC(7,2)) AS transparencia_projetos_atividades_programas, 
-				peso_projetos_atividades_programas, 
-				CAST(pontuacao_fontes_recursos AS NUMERIC(7,2)) AS transparencia_fontes_recursos, 
-				peso_fontes_recursos, 
-				CAST((
-					((pontuacao_dados_gerais * peso_dados_gerais) / 100) + 
-					((pontuacao_area_atuacao * peso_area_atuacao) / 100) + 
-					((pontuacao_descricao * peso_descricao) / 100) + 
-					((pontuacao_titulos_certificacoes * peso_titulos_certificacoes) / 100) + 
-					((pontuacao_relacoes_trabalho_governanca * peso_relacoes_trabalho_governanca) / 100) + 
-					((pontuacao_espacos_participacao_social * peso_espacos_participacao_social) / 100) + 
-					((pontuacao_projetos_atividades_programas * peso_projetos_atividades_programas) / 100) + 
-					((pontuacao_fontes_recursos * peso_fontes_recursos) / 100)
-				) AS NUMERIC(7,2)) AS transparencia_osc
-    	LOOP
-			INSERT INTO 
-				portal.tb_barra_transparencia(
-					id_osc, 
-					transparencia_dados_gerais, 
+			FOR transparencia IN
+				SELECT 
+					osc.id_osc AS id_osc, 
+					CAST(pontuacao_dados_gerais AS NUMERIC(7,2)) AS transparencia_dados_gerais, 
 					peso_dados_gerais, 
-					transparencia_area_atuacao, 
+					CAST(pontuacao_area_atuacao AS NUMERIC(7,2)) AS transparencia_area_atuacao, 
 					peso_area_atuacao, 
-					transparencia_descricao, 
+					CAST(pontuacao_descricao AS NUMERIC(7,2)) AS transparencia_descricao, 
 					peso_descricao, 
-					transparencia_titulos_certificacoes, 
+					CAST(pontuacao_titulos_certificacoes AS NUMERIC(7,2)) AS transparencia_titulos_certificacoes, 
 					peso_titulos_certificacoes, 
-					transparencia_relacoes_trabalho_governanca, 
+					CAST(pontuacao_relacoes_trabalho_governanca AS NUMERIC(7,2)) AS transparencia_relacoes_trabalho_governanca, 
 					peso_relacoes_trabalho_governanca, 
-					transparencia_espacos_participacao_social, 
+					CAST(pontuacao_espacos_participacao_social AS NUMERIC(7,2)) AS transparencia_espacos_participacao_social, 
 					peso_espacos_participacao_social, 
-					transparencia_projetos_atividades_programas, 
+					CAST(pontuacao_projetos_atividades_programas AS NUMERIC(7,2)) AS transparencia_projetos_atividades_programas, 
 					peso_projetos_atividades_programas, 
-					transparencia_fontes_recursos, 
+					CAST(pontuacao_fontes_recursos AS NUMERIC(7,2)) AS transparencia_fontes_recursos, 
 					peso_fontes_recursos, 
-					transparencia_osc
-				) 
-				VALUES (
-					transparencia.id_osc, 
-					transparencia.transparencia_dados_gerais, 
-					transparencia.peso_dados_gerais, 
-					transparencia.transparencia_area_atuacao, 
-					transparencia.peso_area_atuacao, 
-					transparencia.transparencia_descricao, 
-					transparencia.peso_descricao, 
-					transparencia.transparencia_titulos_certificacoes, 
-					transparencia.peso_titulos_certificacoes, 
-					transparencia.transparencia_relacoes_trabalho_governanca, 
-					transparencia.peso_relacoes_trabalho_governanca, 
-					transparencia.transparencia_espacos_participacao_social, 
-					transparencia.peso_espacos_participacao_social, 
-					transparencia.transparencia_projetos_atividades_programas, 
-					transparencia.peso_projetos_atividades_programas, 
-					transparencia.transparencia_fontes_recursos, 
-					transparencia.peso_fontes_recursos, 
-					transparencia.transparencia_osc
-				);
-		END LOOP;
+					CAST((
+						((pontuacao_dados_gerais * peso_dados_gerais) / 100) + 
+						((pontuacao_area_atuacao * peso_area_atuacao) / 100) + 
+						((pontuacao_descricao * peso_descricao) / 100) + 
+						((pontuacao_titulos_certificacoes * peso_titulos_certificacoes) / 100) + 
+						((pontuacao_relacoes_trabalho_governanca * peso_relacoes_trabalho_governanca) / 100) + 
+						((pontuacao_espacos_participacao_social * peso_espacos_participacao_social) / 100) + 
+						((pontuacao_projetos_atividades_programas * peso_projetos_atividades_programas) / 100) + 
+						((pontuacao_fontes_recursos * peso_fontes_recursos) / 100)
+					) AS NUMERIC(7,2)) AS transparencia_osc
+			LOOP
+				INSERT INTO 
+					portal.tb_barra_transparencia(
+						id_osc, 
+						transparencia_dados_gerais, 
+						peso_dados_gerais, 
+						transparencia_area_atuacao, 
+						peso_area_atuacao, 
+						transparencia_descricao, 
+						peso_descricao, 
+						transparencia_titulos_certificacoes, 
+						peso_titulos_certificacoes, 
+						transparencia_relacoes_trabalho_governanca, 
+						peso_relacoes_trabalho_governanca, 
+						transparencia_espacos_participacao_social, 
+						peso_espacos_participacao_social, 
+						transparencia_projetos_atividades_programas, 
+						peso_projetos_atividades_programas, 
+						transparencia_fontes_recursos, 
+						peso_fontes_recursos, 
+						transparencia_osc
+					) 
+					VALUES (
+						transparencia.id_osc, 
+						transparencia.transparencia_dados_gerais, 
+						transparencia.peso_dados_gerais, 
+						transparencia.transparencia_area_atuacao, 
+						transparencia.peso_area_atuacao, 
+						transparencia.transparencia_descricao, 
+						transparencia.peso_descricao, 
+						transparencia.transparencia_titulos_certificacoes, 
+						transparencia.peso_titulos_certificacoes, 
+						transparencia.transparencia_relacoes_trabalho_governanca, 
+						transparencia.peso_relacoes_trabalho_governanca, 
+						transparencia.transparencia_espacos_participacao_social, 
+						transparencia.peso_espacos_participacao_social, 
+						transparencia.transparencia_projetos_atividades_programas, 
+						transparencia.peso_projetos_atividades_programas, 
+						transparencia.transparencia_fontes_recursos, 
+						transparencia.peso_fontes_recursos, 
+						transparencia.transparencia_osc
+					);
+			END LOOP;
+		END IF;
 	END LOOP;
 
 END $$;
