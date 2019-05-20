e9cf03338b87587bdcd42399fe302a36a00cba5b
vagnerpraia <vagnerpraia@gmail.com>
2019-05-20 19:45:14 -0300

    Alteração da área de atualização nacional do perfil de localidade

--- database/2.8.0/05_vw_perfil_area_atuacao_nacional.sql
+++ database/2.8.0/05_vw_perfil_area_atuacao_nacional.sql
@@ -1,13 +1,18 @@
-DROP MATERIALIZED VIEW IF EXISTS analysis.vw_perfil_area_atuacao_nacional CASCADE;
-CREATE MATERIALIZED VIEW analysis.vw_perfil_area_atuacao_nacional AS 
+DROP MATERIALIZED VIEW IF EXISTS analysis.vw_perfil_localidade_area_atuacao_nacional CASCADE;
+CREATE MATERIALIZED VIEW analysis.vw_perfil_localidade_area_atuacao_nacional AS 
 
 SELECT 
 	area_atuacao,
-	SUM(quantidade_oscs) AS quantidade_osc
+	SUM(quantidade_oscs) AS quantidade_osc,
+    (
+		MAX(quantidade_oscs)
+		/ (SELECT SUM(quantidade_oscs) FROM analysis.vw_perfil_localidade_area_atuacao AS c)
+		* 100
+	) AS media
 FROM analysis.vw_perfil_localidade_area_atuacao
 GROUP BY area_atuacao;
 
-CREATE INDEX ix_area_atuacao_vw_perfil_area_atuacao_nacional
-    ON analysis.vw_perfil_area_atuacao_nacional USING btree
+CREATE INDEX ix_area_atuacao_vw_perfil_localidade_area_atuacao_nacional
+    ON analysis.vw_perfil_localidade_area_atuacao_nacional USING btree
     (area_atuacao ASC NULLS LAST)
     TABLESPACE pg_default;
