605ec6a6e2042ebe3db4124aa0b287c09ec24d22
EricFerreiraS <eric_santos6@hotmail.com>
2019-06-17 19:03:13 -0300

    update_fontes RAIS

---
+++ database/2.8.0/15_update_fontes_rais.sql
@@ -0,0 +1,5 @@
+﻿alter table osc.tb_relacoes_trabalho DISABLE TRIGGER ALL;
+
+update osc.tb_relacoes_trabalho set ft_trabalhadores_vinculo = 'RAIS/MTE 2015' where ft_trabalhadores_vinculo = 'RAIS/MTE';
+
+alter table osc.tb_relacoes_trabalho ENABLE TRIGGER ALL;
