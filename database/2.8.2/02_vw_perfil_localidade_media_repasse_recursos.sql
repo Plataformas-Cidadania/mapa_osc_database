f8acafa0b9c0171c21be372195afcea4a134039a
Thiago Giannini Ramos <b116908948@erj5110.ipea.gov.br>
2019-08-19 16:28:21 -0300

    Erro na média de repasse

---
+++ database/2.8.2/02_vw_perfil_localidade_media_repasse_recursos.sql
@@ -0,0 +1,15 @@
+DROP materialized view IF EXISTS analysis.vw_perfil_localidade_media_repasse_recursos;
+
+create materialized view analysis.vw_perfil_localidade_media_repasse_recursos as
+SELECT a.localidade,
+       count(*)                                               AS quantidade_repasses,
+       sum(a.valor_recursos)                                  AS valor_repasses,
+       (sum(a.valor_recursos) / COALESCE(1, count(*))::double precision) AS media
+FROM analysis.vw_perfil_localidade_repasse_recursos a
+    --WHERE a.localidade = 2925006::text
+GROUP BY a.localidade;
+
+alter materialized view analysis.vw_perfil_localidade_media_repasse_recursos owner to b116908948;
+
+create index ix_localidade_vw_perfil_localidade_media_repasse_recursos
+    on analysis.vw_perfil_localidade_media_repasse_recursos (localidade);
