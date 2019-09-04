f14ca09354f22b17c28edb8475e4eab6436746f3
tmnascimento <tmnascimento@gmail.com>
2019-09-04 16:48:50 -0300

    Update 01-altera_classe_economica_dados_gerais.sql

--- database/2.8.3/01-altera_classe_economica_dados_gerais.sql
+++ database/2.8.3/01-altera_classe_economica_dados_gerais.sql
@@ -1,10 +1,10 @@
-﻿ALTER TABLE osc.tb_dados_gerais  DISABLE TRIGGER ALL
+ALTER TABLE osc.tb_dados_gerais  DISABLE TRIGGER ALL ;
 
 update osc.tb_dados_gerais  set
 cd_classe_atividade_economica_OSC = '0' || cd_classe_atividade_economica_OSC
-where length(cd_classe_atividade_economica_OSC)=4 
+where length(cd_classe_atividade_economica_OSC)=4 ;
 
-ALTER TABLE osc.tb_dados_gerais  ENABLE TRIGGER ALL
+ALTER TABLE osc.tb_dados_gerais  ENABLE TRIGGER ALL ;
 
 select refreshallmaterializedviewsc('osc');
 select refreshallmaterializedviewsc('portal');
