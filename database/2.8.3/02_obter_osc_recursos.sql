e1a2dc3cfb9d2401d5e9b6de470cad633b6d4da9
Thiago <gianninimail@gmail.com>
2019-09-11 15:10:58 -0300

    #MOSC-1913 Ordenar recursos por ano

---
+++ database/2.8.3/02_obter_osc_recursos.sql
@@ -0,0 +1,83 @@
+DROP FUNCTION IF EXISTS portal.obter_osc_recursos(param text);
+
+CREATE function portal.obter_osc_recursos(param text)
+    returns TABLE
+            (
+                resultado jsonb,
+                mensagem  text,
+                flag      boolean
+            )
+    language plpgsql
+as
+$$
+DECLARE
+    osc RECORD;
+
+BEGIN
+    resultado := null;
+
+    SELECT INTO osc * FROM osc.tb_osc WHERE (id_osc = param::INTEGER OR tx_apelido_osc = param) AND bo_osc_ativa;
+
+    IF osc.id_osc IS NOT null THEN
+        resultado := (
+            jsonb_build_object(
+                    'recursos', (
+                SELECT jsonb_agg(l)
+                FROM (SELECT jsonb_build_object(
+                                     'id_recursos_osc', id_recursos_osc,
+                                     'cd_origem_fonte_recursos_osc', cd_origem_fonte_recursos_osc,
+                                     'cd_fonte_recursos_osc', cd_fonte_recursos_osc,
+                                     'ft_fonte_recursos_osc', ft_fonte_recursos_osc,
+                                     'dt_ano_recursos_osc', SUBSTRING(dt_ano_recursos_osc::text FROM 1 FOR 4),
+                                     'ft_ano_recursos_osc', ft_ano_recursos_osc,
+                                     'nr_valor_recursos_osc',
+                                     TRIM(TO_CHAR(nr_valor_recursos_osc, '99999999999999999999D99')),
+                                     'ft_valor_recursos_osc', ft_valor_recursos_osc,
+                                     'bo_nao_possui', bo_nao_possui,
+                                     'ft_nao_possui', ft_nao_possui
+                                 ) as l
+                      FROM osc.tb_recursos_osc
+                      WHERE id_osc = 784138
+                      ORDER BY dt_ano_recursos_osc) as t
+            ),
+                    'recursos_outros', (
+                        SELECT jsonb_agg(l)
+                        FROM (
+                                 SELECT jsonb_build_object(
+                                                'tx_nome_fonte_recursos_outro_osc', tx_nome_fonte_recursos_outro_osc,
+                                                'ft_nome_fonte_recursos_outro_osc', ft_nome_fonte_recursos_outro_osc,
+                                                'dt_ano_recursos_outro_osc',
+                                                SUBSTRING(dt_ano_recursos_outro_osc::text FROM 1 FOR 4),
+                                                'ft_ano_recursos_outro_osc', ft_ano_recursos_outro_osc,
+                                                'nr_valor_recursos_outro_osc',
+                                                TRIM(TO_CHAR(nr_valor_recursos_outro_osc, '99999999999999999999D99')),
+                                                'ft_valor_recursos_outro_osc', ft_valor_recursos_outro_osc
+                                            ) as l
+
+                                 FROM osc.tb_recursos_outro_osc
+                                 WHERE id_osc = param::INTEGER
+                                 ORDER BY dt_ano_recursos_outro_osc) as x
+                    )
+                )
+            );
+
+        flag := true;
+        mensagem := 'Recursos de OSC retornadas.';
+
+    ELSE
+        flag := false;
+        mensagem := 'OSC não encontrada.';
+    END IF;
+
+    RETURN NEXT;
+
+EXCEPTION
+    WHEN others THEN
+        flag := false;
+        SELECT INTO mensagem a.mensagem
+        FROM portal.verificar_erro(SQLSTATE, SQLERRM, null, null, null, false, null) AS a;
+        RETURN NEXT;
+END;
+$$;
+
+alter function portal.obter_osc_recursos(text) owner to i3geo;
---
+++ database/2.9.0/14_Script_Consulta_Dados.sql
@@ -0,0 +1,53 @@
+SELECT os.id_osc AS ID,
+       loc.cd_municipio AS MUNICIPIO,
+       uf.eduf_nm_uf AS UF,
+       nj.cd_natureza_juridica AS CODIGO_NATUREZA_JURIDICA,
+       nj.tx_nome_natureza_juridica AS NATUREZA_JURIDICA,
+       dg.tx_razao_social_osc AS RAZAO_SOCIAL,
+       COALESCE(dg.tx_nome_fantasia_osc, 'Não informado') AS NOME_FANTASIA,
+       SUM(COALESCE(rt.nr_trabalhadores_vinculo, 0) + COALESCE(rt.nr_trabalhadores_voluntarios, 0) + COALESCE(rt.nr_trabalhadores_deficiencia, 0)) AS TRABALHADORES_ATIVOS,
+       rt.nr_trabalhadores_vinculo AS TRABALHADORES_COM_VINCULO,
+       --loc.tx_endereco,   --NÃO AUTO
+       --loc.nr_localizacao,    --NÃO AUTO
+       --loc.tx_bairro, --NÃO AUTO
+       --loc.nr_cep --NÃO AUTO
+       dg.dt_fundacao_osc AS DATA_FUNDACAO,
+       --dt_encerramentos NÃO EXISTE
+       --CNPJ (ID OSC) --NÃO AUTO
+       atec.cd_classe_atividade_economica AS COD_ATIV_ECONOMICA,
+       atec.tx_nome_classe_atividade_economica AS ATIVIDADE_ECONOMICA,
+       arat.tx_nome_area_atuacao AS AREA_ATUACAO
+FROM osc.tb_osc os,
+     osc.tb_dados_gerais dg,
+     syst.dc_natureza_juridica nj,
+     osc.tb_localizacao loc,
+     spat.ed_municipio mun,
+     spat.ed_uf uf,
+     osc.tb_relacoes_trabalho rt,
+     osc.tb_area_atuacao tbarea,
+     syst.dc_area_atuacao arat,
+     syst.dc_classe_atividade_economica atec
+WHERE os.id_osc = dg.id_osc
+  AND os.id_osc = loc.id_osc
+  AND dg.cd_natureza_juridica_osc = nj.cd_natureza_juridica
+  AND loc.cd_municipio = mun.edmu_cd_municipio
+  AND mun.eduf_cd_uf = uf.eduf_cd_uf
+  AND os.id_osc = rt.id_osc
+  AND os.id_osc = tbarea.id_osc
+  AND tbarea.cd_area_atuacao = arat.cd_area_atuacao
+  AND dg.cd_classe_atividade_economica_osc = atec.cd_classe_atividade_economica
+GROUP BY os.id_osc, loc.cd_municipio,
+         uf.eduf_nm_uf,
+         nj.cd_natureza_juridica,
+         nj.tx_nome_natureza_juridica,
+         dg.tx_nome_fantasia_osc,
+         dg.tx_razao_social_osc,
+         rt.nr_trabalhadores_vinculo,
+         --loc.tx_endereco,   --NÃO AUTO
+         --loc.nr_localizacao,   --NÃO AUTO
+         --loc.tx_bairro,   --NÃO AUTO
+         --loc.nr_cep   --NÃO AUTO
+         dg.dt_fundacao_osc,
+         atec.cd_classe_atividade_economica,
+         atec.tx_nome_classe_atividade_economica,
+         arat.tx_nome_area_atuacao;
