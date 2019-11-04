c45e404e67ab9d9b3a6364e32bd8d996e1402f73
tmnascimento <tmnascimento@gmail.com>
2019-11-04 19:09:56 -0300

    Update 06-delete_area_atuacao.sql

--- database/2.8.5/06-delete_area_atuacao.sql
+++ database/2.8.5/06-delete_area_atuacao.sql
@@ -3,8 +3,7 @@ delete FROM osc.tb_area_atuacao where id_osc in
 (SELECT id_osc
 FROM ( SELECT id_osc, cd_area_atuacao, cd_subarea_atuacao,
 row_number() OVER(PARTITION BY id_osc,cd_area_atuacao, cd_subarea_atuacao) N
-FROM portal.vw_osc_area_atuacao
-where cd_subarea_atuacao is null ) as A
+FROM portal.vw_osc_area_atuacao ) as A
 WHERE N > 1
 order by id_osc);
 ALTER TABLE osc.tb_area_atuacao ENABLE TRIGGER ALL ;
