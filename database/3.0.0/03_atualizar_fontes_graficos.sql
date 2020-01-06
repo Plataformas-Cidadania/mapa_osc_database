1a40625cab18fe322bf87dd2cc662ab5de680d98
Thiago <gianninimail@gmail.com>
2020-01-06 13:05:54 -0300

    #MOSC-1971 - Alterar legendas do mapa

---
+++ database/3.0.0/03_atualizar_fontes_graficos.sql
@@ -0,0 +1,19 @@
+UPDATE portal.tb_analise SET fontes = string_to_array('RAIS,CNPJ/SRF/MF', ',') WHERE id_analise = 9;
+
+UPDATE portal.tb_analise SET fontes = string_to_array('CNPJ/SRF/MF 2018, RAIS, OSCIP/MJ', ',') WHERE id_analise = 10;
+
+UPDATE portal.tb_analise SET fontes = string_to_array('Representante de OSC, RAIS, CNPJ/SRF/MF 2018, CNEA/MMA, OSCIP/MJ 2016, CEBAS/MS 09/2019, CEBAS/MDS 2017, CEBAS/MEC 10/2017', ',') WHERE id_analise = 12;
+
+UPDATE portal.tb_analise SET fontes = string_to_array('CNPJ/SRF/MF 2018, OSCIP/MJ, RAIS', ',') WHERE id_analise = 1;
+
+UPDATE portal.tb_analise SET fontes = string_to_array('OSCIP/MJ, RAIS, CNPJ/SRF/MF 2018', ',') WHERE id_analise = 2;
+
+UPDATE portal.tb_analise SET fontes = string_to_array('Representante de OSC, LIE/MESP 2017, RAIS, CNEAS/MDS, CNPJ/SRF/MF 2018, CEBAS/MS 09/2019, CEBAS/MDS 2017, CNES/MS 2017, CADSOL/MTE 2017, CEBAS/MEC 10/2017, CNEA/MMA 08/2019, OSCIP/MJ, Censo SUAS 08/2019', ',') WHERE id_analise = 3;
+
+UPDATE portal.tb_analise SET fontes = string_to_array('CNPJ/SRF/MF 2018, CNEAS/MDS, OSCIP/MJ, RAIS', ',') WHERE id_analise = 4;
+
+UPDATE portal.tb_analise SET fontes = string_to_array('CNPJ/SRF/MF 2018, CNES/MS, OSCIP/MJ, RAIS', ',') WHERE id_analise = 5;
+
+UPDATE portal.tb_analise SET fontes = string_to_array('MS/CNES, CNPJ/SRF/MF 2018, OSCIP/MJ, RAIS', ',') WHERE id_analise = 6;
+
+UPDATE portal.tb_analise SET fontes = string_to_array('SENAES/MTE, CNPJ/SRF/MF 2018, OSCIP/MJ, RAIS', ',') WHERE id_analise = 7;
