1c2f3ad7638d67c36ad46323cc9691abdac06053
Thiago Giannini Ramos <gianninimail@gmail.com>
2020-09-08 11:31:01 -0300

    Atualização de consulta de dados certificados

--- database/3.0.0/05_atualizar_data_fim_certificado_oscip4.sql
+++ database/3.0.0/05_atualizar_data_fim_certificado_oscip4.sql
@@ -1,1 +1,3 @@
+
+
 SELECT * FROM portal.atualizar_certificado_osc('Representante de OSC'::TEXT, 762859::NUMERIC, 'id_osc'::TEXT, now()::TIMESTAMP, '[{"cd_certificado":4, "dt_inicio_certificado":"2012-10-26","dt_fim_certificado":"2020-06-05","cd_municipio":null,"cd_uf":null}]'::JSONB, TRUE::BOOLEAN, TRUE::BOOLEAN, TRUE::BOOLEAN, NULL::INTEGER, 3::INTEGER);
