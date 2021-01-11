94d87f6dddd3de3a1bdd9ad4562655bf53fbb8e0
Thiago Giannini Ramos <gianninimail@gmail.com>
2021-01-11 14:33:16 -0300

    #MOSC-2112 - Criar nova estrutura para a regra de negócio do módulo Espaços de Participação Social

---
+++ database/3.0.0/05_alter_table_osc_modulo_participacao_social.sql
@@ -0,0 +1,17 @@
+alter table osc.tb_osc
+	add bo_nao_possui_ps_conselhos boolean;
+
+alter table osc.tb_osc
+	add ft_nao_possui_ps_conselhos text;
+
+alter table osc.tb_osc
+	add bo_nao_possui_ps_conferencias boolean;
+
+alter table osc.tb_osc
+	add ft_nao_possui_ps_conferencias text;
+
+alter table osc.tb_osc
+	add bo_nao_possui_ps_outros_espacos boolean;
+
+alter table osc.tb_osc
+	add ft_nao_possui_ps_outros_espacos text;
