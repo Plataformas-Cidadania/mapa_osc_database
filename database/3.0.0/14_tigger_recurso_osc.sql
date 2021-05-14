d8b9ad39136599c0e2fa1e0ba9e74638e652b618
nome <email>
2021-05-14 13:07:45 -0300

    Tigger para apagar os recurso de uma osc_ano_origem da tabela de recurso quando o não recurso no ano for cadastrado

---
+++ database/3.0.0/14_tigger_recurso_osc.sql
@@ -0,0 +1,20 @@
+CREATE OR REPLACE FUNCTION limpa_recursos_osc_ano_origem() RETURNS trigger
+    language plpgsql
+as
+$$
+BEGIN
+    DELETE FROM osc.tb_recursos_osc WHERE tb_recursos_osc.id_osc = new.id_osc AND
+       "substring"(tb_recursos_osc.dt_ano_recursos_osc::text, 1, 4)::integer  = new.ano AND
+       (SELECT fonte.cd_origem_fonte_recursos_osc FROM syst.dc_fonte_recursos_osc
+           as fonte WHERE tb_recursos_osc.cd_fonte_recursos_osc = fonte.cd_fonte_recursos_osc
+        ) = new.cd_origem_fonte_recursos_osc;
+    RETURN NULL;
+END;
+$$;
+
+alter function limpa_recursos_osc_ano_origem() owner to postgres;
+
+CREATE TRIGGER limpa_recursos_trigger
+AFTER INSERT ON osc.tb_n_recurso_osc_ano
+FOR EACH ROW
+EXECUTE PROCEDURE limpa_recursos_osc_ano_origem();
