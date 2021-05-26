7ae4f9def76bc392544e73118af7845781f90f30
Thiago Giannini Ramos <gianninimail@gmail.com>
2021-05-25 21:33:37 -0300

    alteração script

--- database/3.0.0/00_criar_novo_usuario_para_teste.sql
+++ database/3.0.0/00_criar_novo_usuario_para_teste.sql
@@ -15,10 +15,94 @@
         )
   values (
       2,
-      'teste3@gmail.com',
+      'camila.escudero@ipea.gov.br',
       'dd5fef9c1c1da1394d6d34b248c51be2ad740840',
-      'Usuário Teste 3',
-      46857256322,
+      'Camila',
+      11111111111,
+      true,
+      true,
+      '2017-01-09 15:16:46.846736',
+      '2019-11-18 15:54:11.971385',
+      true,
+      false,
+      false
+    ),
+     (
+      2,
+      'felix.lopez@ipea.gov.br',
+      'dd5fef9c1c1da1394d6d34b248c51be2ad740840',
+      'Felix',
+      22222222222,
+      true,
+      true,
+      '2017-01-09 15:16:46.846736',
+      '2019-11-18 15:54:11.971385',
+      true,
+      false,
+      false
+    ),
+         (
+      2,
+      'erivelton.guedes@ipea.gov.br',
+      'dd5fef9c1c1da1394d6d34b248c51be2ad740840',
+      'Erivelton',
+      33333333333,
+      true,
+      true,
+      '2017-01-09 15:16:46.846736',
+      '2019-11-18 15:54:11.971385',
+      true,
+      false,
+      false
+    ),
+    (
+      2,
+      'pedro.andrade@ipea.gov.br',
+      'dd5fef9c1c1da1394d6d34b248c51be2ad740840',
+      'Pedro',
+      44444444444,
+      true,
+      true,
+      '2017-01-09 15:16:46.846736',
+      '2019-11-18 15:54:11.971385',
+      true,
+      false,
+      false
+    ),
+    (
+      2,
+      'janine.mello@ipea.gov.br',
+      'dd5fef9c1c1da1394d6d34b248c51be2ad740840',
+      'Janine',
+      55555555555,
+      true,
+      true,
+      '2017-01-09 15:16:46.846736',
+      '2019-11-18 15:54:11.971385',
+      true,
+      false,
+      false
+    ),
+          (
+      2,
+      'ana.ribeiro@ipea.gov.br',
+      'dd5fef9c1c1da1394d6d34b248c51be2ad740840',
+      'Ana Camila',
+      66666666666,
+      true,
+      true,
+      '2017-01-09 15:16:46.846736',
+      '2019-11-18 15:54:11.971385',
+      true,
+      false,
+      false
+    ),
+     (
+      2,
+      'thiago.ramos@ipea.gov.br',
+      'dd5fef9c1c1da1394d6d34b248c51be2ad740840',
+      'Thiago',
+      77777777777,
       true,
       true,
       '2017-01-09 15:16:46.846736',
@@ -27,5 +111,11 @@
       false,
       false
     );
-SELECT u.id_usuario FROM portal.tb_usuario u WHERE u.nr_cpf_usuario = 46857256322;
-insert into portal.tb_representacao (id_osc, id_usuario) values (789809, );
+
+SELECT 789809, u.id_usuario FROM portal.tb_usuario u WHERE tx_email_usuario LIKE '%@ipea.gov.br';
+
+
+insert into portal.tb_representacao (id_osc, id_usuario)
+    SELECT 789809, u.id_usuario
+    FROM portal.tb_usuario u
+WHERE tx_email_usuario LIKE '%@ipea.gov.br';;
