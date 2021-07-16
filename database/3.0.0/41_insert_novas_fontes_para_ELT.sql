025b837bb3c0a010589dd888e394abddb04eade1
Thiago Giannini Ramos <gianninimail@gmail.com>
2021-07-16 17:02:52 -0300

    add arquivo

--- database/3.0.0/41_insert_novas_fontes_para_ELT.sql
+++ database/3.0.0/41_insert_novas_fontes_para_ELT.sql
@@ -1,3 +1,3 @@
-insert into dc_fonte_dados (cd_sigla_fonte_dados, tx_nome_fonte_dados, tx_descricao_fonte_dados,
+insert into portal.dc_fonte_dados (cd_sigla_fonte_dados, tx_nome_fonte_dados, tx_descricao_fonte_dados,
                             tx_referencia_fonte_dados, nr_prioridade)
 values ('CNPJ/RFB/MF', 'Cadastro Nacional de Pessoas Jurídicas da Secretaria da Receita Federal', 'Cadastro Nacional de Pessoas Jurídicas da Secretaria da Receita Federal', 'www.receita.gov.br', 2);
