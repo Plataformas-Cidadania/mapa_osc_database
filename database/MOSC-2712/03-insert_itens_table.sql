3cd19d8943f2200bd0fe9128c79362f5b60c1a7b
Thiago Ramos <thiago.ramos@impa.br>
2025-03-28 15:00:44 -0300

    MOSC-2712 / Criar tabela auxiliar para Sistuação Cadastral

---
+++ database/MOSC-2712/03-insert_itens_table.sql
@@ -0,0 +1,14 @@
+INSERT INTO syst.dc_situacao_cadastral (cd_situacao_cadastral, tx_nome_situacao_cadastral)
+VALUES (DEFAULT, 'NULA');
+
+INSERT INTO syst.dc_situacao_cadastral (cd_situacao_cadastral, tx_nome_situacao_cadastral)
+VALUES (DEFAULT, 'ATIVA');
+
+INSERT INTO syst.dc_situacao_cadastral (cd_situacao_cadastral, tx_nome_situacao_cadastral)
+VALUES (DEFAULT, 'SUSPENSA');
+
+INSERT INTO syst.dc_situacao_cadastral (cd_situacao_cadastral, tx_nome_situacao_cadastral)
+VALUES (DEFAULT, 'INAPTA');
+
+INSERT INTO syst.dc_situacao_cadastral (cd_situacao_cadastral, tx_nome_situacao_cadastral)
+VALUES (DEFAULT, 'BAIXADA');
