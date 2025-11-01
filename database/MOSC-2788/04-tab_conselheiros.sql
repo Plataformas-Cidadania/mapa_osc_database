1a7d17a59656f8f3a96e0e014f6fe8b837458edb
b116908948 <thiago.ramos@ipea.gov.br>
2025-11-01 11:24:18 -0300

    MOSC-2682

--- database/MOSC-2788/04-tab_conselheiros.sql
+++ database/MOSC-2788/04-tab_conselheiros.sql
@@ -28,3 +28,20 @@ INSERT INTO confocos.tb_conselheiro (id_conselheiro, tx_nome_conselheiro, tx_org
                                      dt_data_vinculo, dt_data_final_vinculo, bo_conselheiro_ativo, bo_eh_governamental,
                                      id_conselho)
 VALUES (DEFAULT, 'Teste', 'IPEA', null, '2025-10-18 10:38:06.000000', null, true, true, 1);
+
+
+grant select on confocos.tb_conselheiro to "ASTEC-BD";
+
+grant delete, insert, select, trigger, update on confocos.tb_conselheiro to role_mapaosc;
+
+grant delete, insert, select, trigger, update on confocos.tb_conselheiro to grupo_mapaosc with grant option;
+
+grant select on confocos.tb_conselheiro to role_mapaosc_rj_consulta;
+
+grant delete, insert, references, select, trigger, truncate, update on confocos.tb_conselheiro to "thiago.ramos";
+
+grant select on confocos.tb_conselheiro to "murilo.junqueira";
+
+grant delete, insert, select, update on confocos.tb_conselheiro to usr_manutencao_mapa;
+
+grant delete, insert, references, select, trigger, truncate, update on confocos.tb_conselheiro to usr_etl;
