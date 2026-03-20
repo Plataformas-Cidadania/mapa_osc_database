e62177d116931f7fd32d7c47bff565b34e45c891
Thiago Giannini Ramos <thiagogiannini@tecgraf.puc-rio.br>
2026-03-20 14:52:14 -0300

    MOSC-2819 / Novas colunas

--- database/MOSC-2819/add-nova-colunas.sql
+++ database/MOSC-2819/add-nova-colunas.sql
@@ -3,3 +3,10 @@ alter table osc.tb_osc
 
 alter table osc.tb_osc
     add ft_nao_possui_certificacoes text;
+
+
+alter table osc.tb_osc
+    add bo_nao_possui_link_estatuto boolean;
+
+alter table osc.tb_osc
+    add ft_nao_possui_link_estatuto text;
