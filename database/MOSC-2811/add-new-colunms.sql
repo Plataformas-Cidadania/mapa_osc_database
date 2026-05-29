18f3fefbdab62ec94bf4d6ccd3f55b893fde3a84
Thiago Giannini Ramos <thiagogiannini@tecgraf.puc-rio.br>
2026-05-29 17:44:09 -0300

    MOSC-2811 / Novas colunas

--- database/MOSC-2811/add-new-colunms.sql
+++ database/MOSC-2811/add-new-colunms.sql
@@ -5,16 +5,10 @@ alter table osc.tb_projeto
     add ft_possui_emenda text;
 
 alter table osc.tb_projeto
-    add nr_numero_proposta integer;
+    add nr_numero_proposta text;
 
 alter table osc.tb_projeto
     add ft_numero_proposta text;
-
-alter table osc.tb_projeto
-    add tx_nome_proposta text;
-
-alter table osc.tb_projeto
-    add ft_nome_proposta text;
 
 alter table osc.tb_projeto
     add nr_numero_instrumento integer;
